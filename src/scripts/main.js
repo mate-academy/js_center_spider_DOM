a31a5f42e7ecb814476428ca5b074598ef3c6e78
Anatoliy Pokhylko <pokhilko.av@gmail.com>
2020-07-06 13:53:29 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const wallStyle = window.getComputedStyle(spider.offsetParent);
+const spiderStyle = window.getComputedStyle(spider);
+const halfWidth = parseFloat(wallStyle.width) / 2
+- parseFloat(spiderStyle.width) / 2;
+const halfHeight = parseFloat(wallStyle.height) / 2
+- parseFloat(spiderStyle.height) / 2;
+
+spider.style.cssText = `top: ${halfHeight}px; left: ${halfWidth}px;`;
