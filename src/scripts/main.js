6e696d0eaa1eb3419ff76e968d38058993058b80
Liudmyla Ilchenko <liudmyla.ilchenko7@gmail.com>
2020-07-15 12:58:55 +0300

    Center the spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const style = window.getComputedStyle(spider);
+
+spider.style.left = '50%';
+spider.style.marginLeft = `-${parseFloat(style.width) / 2}px`;
+spider.style.top = '50%';
+spider.style.marginTop = `-${parseFloat(style.height) / 2}px`;
