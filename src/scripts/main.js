5a38e355d6b193644ce61f4da7e6b8ede48ce175
Anastasiia Bortnichuk <feilanheid@gmail.com>
2020-08-25 19:34:47 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const wall = document.querySelector('.wall');
-const wallHight = wall.clientHeight;
+const wallHeight = wall.clientHeight;
 const wallWidth = wall.clientWidth;
 
 const spider = document.querySelector('.spider');
-const spiderHight = spider.offsetHeight;
+const spiderHeight = spider.offsetHeight;
 const spiderWidth = spider.offsetWidth;
 
-spider.style.top = `${wallHight / 2 - spiderHight / 2}px`;
+spider.style.top = `${wallHeight / 2 - spiderHeight / 2}px`;
 spider.style.left = `${wallWidth / 2 - spiderWidth / 2}px`;
