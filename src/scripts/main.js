5d64c3be2b829a343371d9bb36657cef066c8d44
JuliaNastenko <juli.nastenko@gmail.com>
2020-08-25 21:26:51 +0300

    solution with cssText

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,6 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-const wallHeight = wall.clientHeight;
-const wallWidth = wall.clientWidth;
-
-const spiderHeight = spider.clientHeight;
-const spiderWidth = spider.clientWidth;
-
-spider.style.top = `${wallHeight / 2 - spiderHeight / 2}px`;
-spider.style.left = `${wallWidth / 2 - spiderWidth / 2}px`;
+spider.style.cssText = `
+  top: ${wall.clientHeight / 2 - spider.clientHeight / 2}px;
+  left: ${wall.clientWidth / 2 - spider.clientWidth / 2}px`;
