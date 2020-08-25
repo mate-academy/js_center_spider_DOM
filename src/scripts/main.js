7784d6e0e27c175d8a69f6de9665fb1d0ffbba7a
martakupka <marta-k2008@ukr.net>
2020-08-25 23:02:48 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+const topValue = (wall.clientHeight - spider.offsetHeight) / 2;
+const leftValue = (wall.clientWidth - spider.offsetWidth) / 2;
+
+spider.setAttribute('style', `top: ${topValue}px; left: ${leftValue}px`);
