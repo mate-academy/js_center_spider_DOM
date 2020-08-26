7b8c4eab3f2a504686edf64eb3961df33373dcbd
Oleg <mvp.westbrook4k@gmail.com>
2020-08-26 11:33:16 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 // write code here
+const spiderToCenter = function() {
+  const spider = document.querySelector('.spider');
+
+  spider.style.top = '0';
+  spider.style.bottom = '0';
+  spider.style.left = '0';
+  spider.style.right = '0';
+  spider.style.margin = 'auto';
+};
+
+spiderToCenter();
