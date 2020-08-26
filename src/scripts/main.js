cda417a40dad7a9203f694b149fdad24f8066fef
Alisa <cheshyr.alisa@gmail.com>
2020-08-26 09:59:21 +0300

    Sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,5 +4,5 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-spider.style.top = `${(wall.clientHeight / 2) - (spider.offsetHeight / 2)}`;
-spider.style.left = `${(wall.clientWidth / 2) - (spider.offsetWidth / 2)}`;
+spider.style.top = `${(wall.clientHeight / 2) - (spider.offsetHeight / 2)}px`;
+spider.style.left = `${(wall.clientWidth / 2) - (spider.offsetWidth / 2)}px`;
