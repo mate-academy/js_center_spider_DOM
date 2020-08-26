1192c0ea74d3d9a134ed7ae4a82fade82d011fd2
Kostya Sabada <kostyasabada@gmail.com>
2020-08-26 11:58:21 +0300

    centered

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,5 +3,5 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-spider.style.top = `${((wall.offsetHeight - spider.offsetHeight) / 2)}px`;
-spider.style.left = `${((wall.offsetWidth - spider.offsetWidth) / 2)}px`;
+spider.style.top = `${((wall.clientHeight - spider.offsetHeight) / 2)}px`;
+spider.style.left = `${((wall.clientWidth - spider.offsetWidth) / 2)}px`;
