469da5f93b48021ab5b7c3df6095b4d7d0a27911
oggyrock <steelo.sun@gmail.com>
2020-08-26 12:51:34 +0300

    a "more js" solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-spider.style.cssText = 'left: 50%; top: 50%; transform: translate(-50%, -50%)';
+spider.style.top = `${wall.clientHeight / 2 - spider.offsetHeight / 2}px`;
+spider.style.left = `${wall.clientWidth / 2 - spider.offsetWidth / 2}px`;
