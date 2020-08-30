e88050bdf7a76a7ebc540dcacb0f54f584e8cfde
Vasyl Tykhan <tykhan@MacBook-Pro-Vasyl.local>
2020-08-30 10:57:47 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+const wallSize = wall.getBoundingClientRect().width;
+const spiderSize = spider.getBoundingClientRect().width;
+
+wall.style.boxSizing = 'border-box';
+spider.style.left = `${wallSize / 2 - spiderSize}px`;
+spider.style.top = `${wallSize / 2 - spiderSize}px`;
