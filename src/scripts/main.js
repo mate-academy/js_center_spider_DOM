292e4c225348767fd0dc116e11ea1ba760f05eac
Dima Diachenko <zarg129@gmail.com>
2020-09-09 17:56:25 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,7 @@
 'use strict';
 
-// write code here
+const myWall = document.querySelector('.wall');
+const mySpider = document.querySelector('img');
+
+mySpider.style.top = `${(myWall.clientHeight - mySpider.clientHeight) / 2}px`;
+mySpider.style.left = `${(myWall.clientWidth - mySpider.clientWidth) / 2}px`;
