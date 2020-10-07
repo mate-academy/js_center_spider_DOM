85a39d510dd7bba39007021cdb0173fa53a76626
Alona <aionovaionova@gmail.com>
2020-10-07 12:43:25 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
+
+spider.style.top = `${(wall.clientHeight - spiderHeight) / 2}px`;
+spider.style.left = `${(wall.clientWidth - spiderWidth) / 2}px`;
