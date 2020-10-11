5f111744f74346d01506117cda6f98ed7bacdc99
OleksandrMykoliuk <69759907+OleksandrMykoliuk@users.noreply.github.com>
2020-10-11 22:28:38 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const wallItem = document.querySelector('.wall');
+const spiderItem = document.querySelector('.spider');
+const wallHeight = wallItem.clientHeight;
+const wallWidth = wallItem.clientWidth;
+const spiderHeight = spiderItem.offsetHeight;
+const spiderWidth = spiderItem.offsetWidth;
+const centerHorizontal = wallWidth / 2 - spiderWidth / 2;
+const centerVertical = wallHeight / 2 - spiderHeight / 2;
+
+spiderItem.style.transform = `
+translate(${centerHorizontal}px, ${centerVertical}px) rotate(360deg)
+`;
