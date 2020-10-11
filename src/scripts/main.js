2f94e65612ff7f4e3354f5700639936f76f210b9
Artem Lavreniuk <artemlavrenyuk@gmail.com>
2020-10-11 21:33:01 +0300

    position removed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,7 @@
 'use strict';
 
 const spider = document.querySelector('.spider');
-const wall = document.querySelector('.wall');
 
-wall.style.position = 'relative';
-spider.style.position = 'absolute';
 spider.style.left = '50%';
 spider.style.top = '50%';
 spider.style.transform = 'translate(-50%, -50%)';
