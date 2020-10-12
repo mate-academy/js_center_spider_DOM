c28f24e22b81ae4398a81d4d9f908e861b18ec03
Alina <alina90565@gmail.com>
2020-10-12 14:04:44 +0300

    done center spider task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,7 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+spider.style.top = ((wall.clientHeight - spider.clientHeight) / 2) + 'px';
+spider.style.left = ((wall.clientWidth - spider.clientWidth) / 2) + 'px';
