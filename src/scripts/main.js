5b7ec3adc0446946e2733b6debf4c5e8585ef678
andlap24 <andrew.laptev24@gmail.com>
2020-10-15 19:58:15 +0300

    add spider sizes vars

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,8 @@ const spider = document.querySelector('.spider');
 
 const wallWidth = wall.clientWidth;
 const wallHeight = wall.clientHeight;
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
 
-spider.style.top = `${(wallHeight / 2) - 25}px`;
-spider.style.left = `${(wallWidth / 2) - 25}px`;
+spider.style.top = `${(wallHeight / 2) - (spiderHeight / 2)}px`;
+spider.style.left = `${(wallWidth / 2) - (spiderWidth / 2)}px`;
