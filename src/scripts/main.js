6005293e7ea44e9171b990e959ed17a3245deaf2
goncharukop <goncharukop@gmail.com>
2020-10-16 15:11:43 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.style.boxSizing = 'border-box';
+
+spider.style.left = (wall.offsetWidth - spider.offsetWidth) / 2 + 'px';
+spider.style.top = (wall.offsetHeight - spider.offsetHeight) / 2 + 'px';
