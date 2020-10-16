15dc59be3d2f9e4581b8380f107711e6931f3907
KhrystynaYelyseyeva <play1409@gmail.com>
2020-10-16 21:44:36 +0300

    shift spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+const wallWidth = wall.offsetWidth;
+const wallHeight = wall.offsetHeight;
+const convertWallWidth = parseFloat(wallWidth);
+const convertWallHeight = parseFloat(wallHeight);
+const shiftX = `calc(${convertWallWidth / 2}px - 50%)`;
+const shiftY = `calc(${convertWallHeight / 2}px - 50%)`;
+
+spider.style.transform = `translate(${shiftX}, ${shiftY})`;
