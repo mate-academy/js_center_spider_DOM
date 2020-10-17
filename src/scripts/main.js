7474e198a2ee0557752a40d57ac949138db8f04b
KhrystynaYelyseyeva <play1409@gmail.com>
2020-10-17 14:15:45 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,7 @@
 'use strict';
 
 const spider = document.querySelector('.spider');
-const wall = document.querySelector('.wall');
-const wallWidth = wall.offsetWidth;
-const wallHeight = wall.offsetHeight;
-const convertWallWidth = parseFloat(wallWidth);
-const convertWallHeight = parseFloat(wallHeight);
-const shiftX = `calc(${convertWallWidth / 2}px - 50%)`;
-const shiftY = `calc(${convertWallHeight / 2}px - 50%)`;
 
-spider.style.transform = `translate(${shiftX}, ${shiftY})`;
+spider.style.top = '50%';
+spider.style.left = '50%';
+spider.style.transform = `translate( -50%, -50%)`;
