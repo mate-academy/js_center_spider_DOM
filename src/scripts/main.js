1e2d5787755b8df5aafeb96180fd11df6f01b5e1
dmitry <akulinind@gmail.com>
2020-10-17 12:33:07 +0300

    move spider to center

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+function centerSpider() {
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  spider.style.left = Math.round(wall.clientWidth / 2
+    - spider.offsetWidth / 2) + 'px';
+
+  spider.style.top = Math.round(wall.clientHeight / 2
+    - spider.offsetHeight / 2) + 'px';
+}
+
+centerSpider();
