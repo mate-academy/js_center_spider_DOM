aebeacc25b6b3b1038796fde4098405a0930e87d
milla-romankova <milla-romankova@gmail.com>
2020-10-19 00:19:01 +0300

    fix spaces

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,5 +3,5 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-spider.style.left = `${(wall.clientWidth - spider.offsetWidth)/2}px`;
-spider.style.top = `${(wall.clientHeight - spider.offsetHeight)/2}px`;
+spider.style.left = `${(wall.clientWidth - spider.offsetWidth) / 2}px`;
+spider.style.top = `${(wall.clientHeight - spider.offsetHeight) / 2}px`;
