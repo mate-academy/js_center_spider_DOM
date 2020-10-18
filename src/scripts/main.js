0a2899cb373c9fc3be5aa2e7607137cc31e4d4f8
38063 <xeilen1112@gmail.com>
2020-10-18 16:24:17 +0300

    -solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const wallHeighht = wall.clientHeight;
+const wallWidth = wall.clientWidth;
+
+spider.style.top = wallHeighht / 2 + 'px';
+spider.style.left = wallWidth / 2 + 'px';
+spider.style.transform = 'translate(-50%, -50%)';
