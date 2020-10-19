776b62ac81bd01deefc98e731b0eb6afc23d86bc
Marina <m.gerasymova29@gmail.com>
2020-10-20 00:28:17 +0300

    fix bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,8 @@
 
 const wall = document.querySelector('.wall');
 const spider = wall.querySelector('.spider');
-const spiderSize = spider.clientWidth;
-const wallSize = wall.clientWidth;
+const spiderSize = spider.offsetWidth;
+const wallSize = wall.offsetWidth;
 
 spider.style.top = wallSize / 2 - spiderSize / 2 + 'px';
 spider.style.left = wallSize / 2 - spiderSize / 2 + 'px';
