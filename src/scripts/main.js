d7d9eb51712a192629be5ee5c458546aa5df35f4
Anna Uska <annauska95@gmail.com>
2020-11-09 03:28:21 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+// eslint-disable-next-line no-undef
+const styleOfWall = getComputedStyle(wall);
+// eslint-disable-next-line no-undef
+const styleOfSpider = getComputedStyle(spider);
+
+const wallWidth = parseFloat(styleOfWall.width) / 2;
+const wallHeight = parseFloat(styleOfWall.height) / 2;
+const spiderWidth = parseFloat(styleOfSpider.width) / 2;
+const spiderHeight = parseFloat(styleOfSpider.height) / 2;
+
+const midWidth = wallWidth - spiderWidth;
+const midHeight = wallHeight - spiderHeight;
+
+wall.style.display = 'relative';
+
+spider.style.position = 'absolute';
+spider.style.top = `${midHeight}px`;
+spider.style.left = `${midWidth}px`;
