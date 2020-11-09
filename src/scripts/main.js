944c1ad64884ae09475807c437850dc981be2f35
Anna Uska <annauska95@gmail.com>
2020-11-09 14:56:10 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,21 +3,8 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-// eslint-disable-next-line no-undef
-const styleOfWall = getComputedStyle(wall);
-// eslint-disable-next-line no-undef
-const styleOfSpider = getComputedStyle(spider);
+const midWidth = (wall.clientWidth - spider.offsetWidth) / 2;
+const midHeight = (wall.clientHeight - spider.offsetHeight) / 2;
 
-const wallWidth = parseFloat(styleOfWall.width) / 2;
-const wallHeight = parseFloat(styleOfWall.height) / 2;
-const spiderWidth = parseFloat(styleOfSpider.width) / 2;
-const spiderHeight = parseFloat(styleOfSpider.height) / 2;
-
-const midWidth = wallWidth - spiderWidth;
-const midHeight = wallHeight - spiderHeight;
-
-wall.style.display = 'relative';
-
-spider.style.position = 'absolute';
 spider.style.top = `${midHeight}px`;
 spider.style.left = `${midWidth}px`;
