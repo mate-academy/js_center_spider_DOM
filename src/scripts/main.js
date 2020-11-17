31428307b2f66b54746b9edeb504c2af0acedd47
Olga Rubalska <olga.anikeieva@gmail.com>
2020-11-17 19:13:17 +0200

    Spider is centered

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const wallHalfWidth = (parseInt(wall.clientWidth) / 2);
+const wallHalfHeight = (parseInt(wall.clientHeight) / 2);
+
+const spiderHalfWidth = (parseInt(spider.clientWidth) / 2);
+const spiderHalfHeight = (parseInt(spider.clientHeight) / 2);
+
+spider.style.top = `${wallHalfWidth - spiderHalfWidth}px`;
+spider.style.left = `${wallHalfHeight - spiderHalfHeight}px`;
