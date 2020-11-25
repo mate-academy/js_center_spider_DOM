f00548f5c6d391dd7de988a016f2210f7a1bd447
y.kostenko <y.kostenko@mate.academy>
2020-11-25 17:46:53 +0200

    now cypress work with jquery

--- cypress/integration/centerSpider.spec.js
+++ cypress/integration/centerSpider.spec.js
@@ -8,12 +8,9 @@ describe('Spider application', () => {
   it('should center the spider by left border', () => {
     cy.get('.wall').then(($wall) => {
       cy.get('.spider').should(($spider) => {
-        const spider = $spider.get(0);
-        const wall = $wall.get(0);
-
-        const spiderLeft = spider.getBoundingClientRect().left;
-        const spiderLeftAbs = (wall.clientWidth / 2) + (wall.offsetLeft + 10)
-          - (spider.width / 2);
+        const spiderLeft = $spider.offset().left;
+        const spiderLeftAbs = ($wall.innerWidth() / 2)
+          + ($wall.offset().left + 10) - ($spider.width() / 2);
 
         expect(spiderLeft).to.equal(spiderLeftAbs);
       });
@@ -23,12 +20,9 @@ describe('Spider application', () => {
   it('should center the spider by top border', () => {
     cy.get('.wall').then(($wall) => {
       cy.get('.spider').should(($spider) => {
-        const spider = $spider.get(0);
-        const wall = $wall.get(0);
-
-        const spiderTop = spider.getBoundingClientRect().top;
-        const spiderTopAbs = (wall.clientHeight / 2) + (wall.offsetTop + 10)
-          - (spider.height / 2);
+        const spiderTop = $spider.offset().top;
+        const spiderTopAbs = ($wall.innerHeight() / 2)
+          + ($wall.offset().top + 10) - ($spider.height() / 2);
 
         expect(spiderTop).to.equal(spiderTopAbs);
       });
