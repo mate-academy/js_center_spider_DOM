5346382ec60feb5d7d6930a72a164f451ead99ca
Lora Levitska <larakad94@gmail.com>
2021-01-05 16:26:11 +0200

    maybe this solution will be righter

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,3 @@
 'use strict';
 
-document.querySelector('.spider').style.cssText = 'top: 45%; left: 45%';
+document.querySelector('.spider').style.cssText = 'top: 50%; left: 50%; transform: translate(-50%, -50%)';
