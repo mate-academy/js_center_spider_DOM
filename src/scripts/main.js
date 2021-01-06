bfe73c9a530da603df29ae47650f686cff490fca
Vitalii Siedykh <siedykh.v@gmail.com>
2021-01-06 10:38:15 +0200

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,5 +5,5 @@ const spider = wall.querySelector('.spider');
 const wallSize = wall.offsetWidth;
 const spiderSize = spider.offsetWidth;
 
-spider.style.left = wallSize / 2 - spiderSize / 2 + 'px';
-spider.style.top = wallSize / 2 - spiderSize / 2 + 'px';
+spider.style.left = ((wallSize / 2) - 10) - spiderSize / 2 + 'px';
+spider.style.top = ((wallSize / 2) - 10) - spiderSize / 2 + 'px';
