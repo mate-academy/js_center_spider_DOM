b41636d68fe9f34f306c27ca166eb1515a08253b
Iryna Kozhura <ivladimirovna713@gmail.com>
2021-01-06 18:36:32 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,7 @@
 'use strict';
 
-// write code here
+const image = document.querySelector('.spider');
+
+image.style.top = '50%';
+image.style.left = '50%';
+image.style.transform = 'translate(-50%, -50%)';
