86f44f38efedab36e3f4ed488cccbbb6ae2c16f0
Nazar Valchyshyn <nvalja@gmail.com>
2021-01-07 16:08:52 +0200

    made task about spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+function position(valueSpider, valueWall) {
+  return `${valueWall / 2 - valueSpider / 2}px`;
+}
+
+spider.style.top = position(spider.offsetHeight, wall.clientHeight);
+spider.style.left = position(spider.offsetWidth, wall.clientWidth);
