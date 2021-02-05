203653599da947e98ffc48e98d07f38b86208119
DominoSl <artemenko.mik@gmail.com>
2021-02-05 11:32:10 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const wallCenter = {
+  x: wall.clientWidth / 2,
+  y: wall.clientHeight / 2,
+};
+
+const spiderCenter = {
+  x: spider.clientWidth / 2,
+  y: spider.clientHeight / 2,
+};
+
+spider.style.top = wallCenter.y - spiderCenter.y + 'px';
+spider.style.left = wallCenter.x - spiderCenter.x + 'px';
