6230dffb84eb348fa606f2c41b94e05d01cabd64
Andriy <andriy@Air-Andriy.lan>
2021-02-14 19:44:16 +0000

    Solution Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,8 +5,8 @@ const spider = document.querySelector('.spider');
 
 const midleSpiderSize = parseInt(getComputedStyle(spider).width) / 2;
 
-const topCoord = parseInt(getComputedStyle(field).width) / 2 - midleSpiderSize;
-const leftCoord = parseInt(getComputedStyle(field).width) / 2 - midleSpiderSize;
+const topCoord = parseFloat(getComputedStyle(field).width) / 2 - midleSpiderSize;
+const leftCoord = parseFloat(getComputedStyle(field).width) / 2 - midleSpiderSize;
 
 spider.style.top = topCoord + 'px';
 spider.style.left = leftCoord + 'px';
