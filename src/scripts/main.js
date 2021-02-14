56e15431bab1ff380c33043adab2cbf3e69f0987
Andriy <andriy@Air-Andriy.lan>
2021-02-14 19:17:42 +0000

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+const field = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const midleSpiderSize = parseInt(getComputedStyle(spider).width) / 2;
+
+const topCoord = parseInt(getComputedStyle(field).width) / 2 - midleSpiderSize;
+const leftCoord = parseInt(getComputedStyle(field).width) / 2 - midleSpiderSize;
+
+spider.style.top = topCoord + 'px';
+spider.style.left = leftCoord + 'px';
