55ad4ff5270ea51c32b6cc8ab09ec5366f053969
Bohdan Dobak <chudolipton@gmail.com>
2021-02-25 22:46:15 +0200

    made task center spider and for this task i find height and width by using mathematical operation and that all

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const wallStyles = getComputedStyle(wall);
+
+const heightToCenter = (wall.getBoundingClientRect().height
+  - (parseInt(wallStyles.borderTopWidth) * 2)) / 2
+  - spider.getBoundingClientRect().height / 2;
+const widthToCenter = (wall.getBoundingClientRect().width
+  - (parseInt(wallStyles.borderLeftWidth) * 2)) / 2
+  - spider.getBoundingClientRect().width / 2;
+
+spider.style.top = heightToCenter + 'px';
+spider.style.left = widthToCenter + 'px';
