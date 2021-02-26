db52e80ee93f7309adabf4bf063391ce0792911a
Bohdan Dobak <chudolipton@gmail.com>
2021-02-26 11:36:18 +0200

    made this task again because getBoundingClientRect are very hard function for performance and i done this task by using offsetWidth and clientWidth and same with height

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,9 @@
 
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
-const wallStyles = getComputedStyle(wall);
 
-const heightToCenter = (wall.getBoundingClientRect().height
-  - (parseInt(wallStyles.borderTopWidth) * 2)) / 2
-  - spider.getBoundingClientRect().height / 2;
-const widthToCenter = (wall.getBoundingClientRect().width
-  - (parseInt(wallStyles.borderLeftWidth) * 2)) / 2
-  - spider.getBoundingClientRect().width / 2;
+const heightToCenter = (wall.clientHeight - spider.offsetHeight) / 2;
+const widthToCenter = (wall.clientWidth - spider.offsetWidth) / 2;
 
 spider.style.top = heightToCenter + 'px';
 spider.style.left = widthToCenter + 'px';
