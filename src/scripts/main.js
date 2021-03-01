52000232d291aaff569d52eb0cc6aa8c02e782b5
Ruslan Shmadchenko <ruslanwpua@gmail.com>
2021-03-01 17:49:24 +0200

    solution fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,14 +6,14 @@ const spider = document.querySelector('.spider');
 const wallHeight = wall.clientHeight;
 const wallWidth = wall.clientWidth;
 
-const halfWallWidth = wall.clientWidth / 2;
-const halfWallHeight = wall.clientHeight / 2;
+const halfWallWidth = wallWidth / 2;
+const halfWallHeight = wallHeight / 2;
 
 const spiderWidth = spider.clientWidth;
 const spiderHeight = spider.clientHeight;
 
-const halfSpiderWidth = spider.clientWidth / 2;
-const halfSpiderHeight = spider.clientHeight / 2;
+const halfSpiderWidth = spiderWidth / 2;
+const halfSpiderHeight = spiderHeight / 2;
 
 spider.style.left = `${halfWallWidth - halfSpiderWidth}px`;
 spider.style.top = `${halfWallHeight - halfSpiderHeight}px`;
