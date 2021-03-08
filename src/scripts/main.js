3b258d53a47e245ab3b18fd771645e6f0213a770
Sofiia Asieieva <sofiia.asieieva@gmail.com>
2021-03-08 16:29:37 +0200

    centered the element

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,6 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+
+spider.style.left = `calc(50% - ${spider.offsetWidth / 2}px)`;
+spider.style.top = `calc(50% - ${spider.offsetHeight / 2}px)`;
