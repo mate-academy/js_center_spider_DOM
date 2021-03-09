772b2d88ce143a0c327af55ab5f789d263b84cb9
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-03-09 22:19:33 +0200

    Fixes after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,13 +2,9 @@
 
 const spider = document.querySelector('.spider');
 const wall = document.querySelector('.wall');
-const wallStyle = getComputedStyle(wall);
-const spiderStyle = getComputedStyle(spider);
 
-spider.style.position = 'relative';
+spider.style.top = parseFloat(wall.clientHeight - spider.clientHeight) / 2
+  + 'px';
 
-spider.style.top = parseFloat(wallStyle.height) / 2
-  - parseFloat(spiderStyle.height) / 2 + 'px';
-
-spider.style.left = parseFloat(wallStyle.width) / 2
-  - parseFloat(spiderStyle.width) / 2 + 'px';
+spider.style.left = parseFloat(wall.clientWidth - spider.clientWidth) / 2
+  + 'px';
