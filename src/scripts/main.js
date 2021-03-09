3130f88dd308f012fbb0fe02483672e5274e3b38
Ivan Panchenko <ivanpanchenkorulit@gmail.com>
2021-03-09 11:59:38 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const spiderImg = document.querySelector('.spider');
+
+const wall = document.querySelector('.wall');
+
+wall.style.display = 'flex';
+
+wall.style.justifyContent = 'center';
+
+wall.style.alignItems = 'center';
+
+spiderImg.style.position = 'static';
