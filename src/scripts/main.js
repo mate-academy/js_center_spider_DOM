bdfbf27487e19dc8ed5e620ba409a254ce38ab34
Olexandr Bieliaiev <Shurasava17@gmail.com>
2021-03-09 20:42:46 +0200

    optimizated solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,12 @@
 
 const spider = document.querySelector('.spider');
 const wall = document.querySelector('.wall');
-const spiderWidth = parseInt(getComputedStyle(spider).width);
-const spiderHeight = parseInt(getComputedStyle(spider).height);
+const spiderWidth = parseInt(spider.clientWidth);
+const spiderHeight = parseInt(spider.clientHeight);
 
-const wallWidth = parseInt(getComputedStyle(wall).width);
-const wallHeight = parseInt(getComputedStyle(wall).height);
+const wallWidth = parseInt(wall.clientWidth);
+const wallHeight = parseInt(wall.clientHeight);
 
-document.querySelector('.spider').style.top = wallHeight / 2
-  - spiderHeight / 2 + 'px';
+spider.style.top = wallHeight / 2 - spiderHeight / 2 + 'px';
 
-document.querySelector('.spider').style.left = wallWidth / 2
-  - spiderWidth / 2 + 'px';
+spider.style.left = wallWidth / 2 - spiderWidth / 2 + 'px';
