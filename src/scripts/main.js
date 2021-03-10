1ee36e856a6414c57ae6a602a468fa5b458e7055
anastasiia <anastasiia.aliinyk@gmail.com>
2021-03-10 13:51:49 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,5 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-const halfWallWidth = wall.clientWidth / 2;
-const halfSpiderWidth = spider.clientWidth / 2;
-
-const halfWallHeight = wall.clientHeight / 2;
-const halfSpiderHeight = spider.clientHeight / 2;
-
-spider.style.top = halfWallHeight - halfSpiderHeight + 'px';
-spider.style.left = halfWallWidth - halfSpiderWidth + 'px';
+spider.style.top = (wall.clientHeight - spider.clientHeight) / 2 + 'px';
+spider.style.left = (wall.clientWidth - spider.clientWidth) / 2 + 'px';
