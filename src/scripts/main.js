7261a0d64c88c4c240e05449f4e94744b9e36f3c
anastasiia <anastasiia.aliinyk@gmail.com>
2021-03-10 11:13:30 +0200

    center spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const halfWallWidth = wall.clientWidth / 2;
+const halfSpiderWidth = spider.clientWidth / 2;
+
+const halfWallHeight = wall.clientHeight / 2;
+const halfSpiderHeight = spider.clientHeight / 2;
+
+spider.style.top = halfWallHeight - halfSpiderHeight + 'px';
+spider.style.left = halfWallWidth - halfSpiderWidth + 'px';
