5ec7dae7d1a3e171d5d2700a1b60662b2a1cf189
Olha Briukhanova <oaleksandrovva@gmail.com>
2021-03-10 11:24:25 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const wall = document.querySelector('div.wall');
-const spider = wall.querySelector('img.spider');
+const wall = document.querySelector('.wall');
+const spider = wall.querySelector('.spider');
 
 spider.style.top = (wall.clientWidth - spider.offsetWidth) / 2 + 'px';
 spider.style.left = (wall.clientHeight - spider.offsetHeight) / 2 + 'px';
