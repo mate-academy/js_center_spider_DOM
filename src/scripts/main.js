051f3b6b413b2698e3dee199ca0c26385efb516a
Ihor Drul <druleger@gmail.com>
2021-03-23 17:57:36 +0200

    Solution2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,15 +2,14 @@
 
 const spider = document.querySelector('.spider');
 const wall = document.querySelector('.wall');
+const wallSize = wall.offsetWidth;
+const spiderSize = spider.offsetWidth;
+const center = `${((wallSize / 2) - (spiderSize / 2) - 10)}px`;
 
-// const wallSize = wall.offsetWidth;
-// const spiderSize = spider.offsetWidth;
-// const center = `${((wallSize / 2) - (spiderSize / 2) - 10)}px`;
+spider.style.left = center;
+spider.style.top = center;
 
-// spider.style.left = center;
-// spider.style.top = center;
-
-spider.style.position = 'relative';
-wall.style.display = 'flex';
-wall.style.justifyContent = 'center';
-wall.style.alignItems = 'center';
+// spider.style.position = 'relative';
+// wall.style.display = 'flex';
+// wall.style.justifyContent = 'center';
+// wall.style.alignItems = 'center';
