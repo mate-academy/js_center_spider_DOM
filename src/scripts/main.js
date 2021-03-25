03314dfc16631620e129a6780f7bb2b4a20d019e
Inna Sorochynska <innasoro@gmail.com>
2021-03-25 15:35:23 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,10 +3,8 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-const height = parseFloat(getComputedStyle(wall).height) / 2
-  - parseFloat(getComputedStyle(spider).height) / 2;
-const width = parseFloat(getComputedStyle(wall).width) / 2
-  - parseFloat(getComputedStyle(spider).width) / 2;
+const height = (wall.clientHeight - spider.clientHeight) / 2;
+const width = (wall.clientWidth - spider.clientWidth) / 2;
 
 spider.style.top = `${height}px`;
 spider.style.left = `${width}px`;
