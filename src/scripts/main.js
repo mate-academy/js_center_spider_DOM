08ee57111f0947272c6c980e8dd1a9afadf6ca3e
Ruslan Kalyniak <krv241085krv@icloud.com>
2021-03-27 08:53:16 +0200

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
 // write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+function coordinatesCenter(obj, element) {
+  const leftX = Math.round(obj.clientWidth / 2 - element
+    .offsetWidth / 2) + 'px';
+  const topY = Math.round(obj.clientHeight / 2 - element
+    .offsetHeight / 2) + 'px';
+
+  element.style.top = topY;
+  element.style.left = leftX;
+
+  return element;
+}
+
+coordinatesCenter(wall, spider);
