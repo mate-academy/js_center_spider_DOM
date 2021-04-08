ef0ea0a6d1a4c4e8d1dc0fd867fafd15be883cae
Alex Fedorchenko <mralex.fed@gmail.com>
2021-04-08 11:15:58 +0300

    Solution after code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,4 +2,6 @@
 
 const spider = document.querySelector('.spider');
 
-spider.style.cssText = 'top: 0; left: 0; bottom: 0; right: 0; margin: auto;';
+spider.style.bottom = 0;
+spider.style.right = 0;
+spider.style.margin = 'auto';
