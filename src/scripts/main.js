1bb79e1990030f04130d92adb4ad990728f65f25
gennady.tabala <gennady.tabala@gmail.com>
2021-04-13 18:10:20 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const sp = document.querySelector('.spider');
+const spS = getComputedStyle(sp);
+
+const spP = sp.parentNode;
+const spPS = getComputedStyle(spP);
+
+const spSw = Number.parseFloat(spS.width);
+const spPSw = Number.parseFloat(spPS.width);
+
+const spSh = Number.parseFloat(spS.height);
+const spPSh = Number.parseFloat(spPS.height);
+
+sp.style.marginLeft = `${(spPSw - spSw) / 2}px`;
+sp.style.marginTop = `${(spPSh - spSh) / 2}px`;
