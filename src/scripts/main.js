b17af768ac7c9208ae879c6b948cca28532404d3
Serhii Kolesnyk <kolesniks203@gmail.com>
2021-05-22 20:36:29 +0300

    dom spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('img');
+const wall = document.querySelector('div');
+const border = Number.parseFloat(getComputedStyle(wall).border);
+
+const leftCoord = (wall.offsetWidth - spider.offsetWidth) / 2 - border;
+const topCoord = (wall.offsetHeight - spider.offsetHeight) / 2 - border;
+
+spider.style.left = leftCoord + 'px';
+spider.style.top = topCoord + 'px';
