b54c0351f7dce1864016d62a7817b236ac5aaedf
Serhii Kolesnyk <kolesniks203@gmail.com>
2021-05-22 21:22:45 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,10 @@
 
 const spider = document.querySelector('img');
 const wall = document.querySelector('div');
-const border = Number.parseFloat(getComputedStyle(wall).border);
+const borderSize = Number.parseFloat(getComputedStyle(wall).border);
 
-const leftCoord = (wall.offsetWidth - spider.offsetWidth) / 2 - border;
-const topCoord = (wall.offsetHeight - spider.offsetHeight) / 2 - border;
+const leftCoord = (wall.offsetWidth - spider.offsetWidth) / 2 - borderSize;
+const topCoord = (wall.offsetHeight - spider.offsetHeight) / 2 - borderSize;
 
 spider.style.left = leftCoord + 'px';
 spider.style.top = topCoord + 'px';
