44755a70d49ded67f4fdfd528d96c0c7c32414d2
Inna <inna_temis@ukr.net>
2021-05-25 17:59:27 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
 // write code here
+const spider = document.querySelector('.spider');
+
+spider.style.left = '50%';
+spider.style.top = '50%';
+spider.style.marginLeft = '-25px';
+spider.style.marginTop = '-25px';
