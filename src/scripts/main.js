7e5b60440e299afb32611cbaa538f8bec77f070c
Artem Yemelyanov <ae1666528@gmail.com>
2021-07-02 20:47:27 +0300

    Centered the spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+spider.style.top = `${
+  calculateCenteringOffset(
+    getComputedStyle(wall).height,
+    getComputedStyle(spider).height
+  )
+}px`;
+
+spider.style.left = `${
+  calculateCenteringOffset(
+    getComputedStyle(wall).width,
+    getComputedStyle(spider).width
+  )
+}px`;
+
+function calculateCenteringOffset(containerSize, childSize) {
+  const containerSizeNum = parseFloat(containerSize);
+  const childSizeNum = parseFloat(childSize);
+
+  return (containerSizeNum / 2) - (childSizeNum / 2);
+}
