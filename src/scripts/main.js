c7a1fc65a66625d1a77347ca47b578a8b4398ddc
korzh <korzh.alexey@yahoo.com>
2021-07-07 17:59:03 +0300

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+function centerSpider() {
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  window.setTimeout(() => {
+    spider.style.top = `${wall.clientHeight / 2 - spider.clientHeight / 2}px`;
+    spider.style.left = `${wall.clientWidth / 2 - spider.clientWidth / 2}px`;
+  }, 0);
+}
+
+centerSpider();
