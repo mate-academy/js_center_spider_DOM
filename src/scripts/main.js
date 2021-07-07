af77f4813f423ce8aa5471ec25a480d87204e6a9
John <jozaman96@gmail.com>
2021-07-07 15:08:23 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const findParamValue = (element, param) => {
+  return parseFloat(getComputedStyle(element)[param]);
+};
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+const wallWidth = findParamValue(wall, 'width');
+const wallHeight = findParamValue(wall, 'height');
+const spiderWidth = findParamValue(spider, 'width');
+const spiderHeight = findParamValue(spider, 'height');
+
+spider.style.left = `${wallWidth / 2 - spiderWidth / 2}px`;
+
+spider.style.top = `${wallHeight / 2 - spiderHeight / 2}px`;
