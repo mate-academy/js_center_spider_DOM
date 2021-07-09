0ceda1badcd34637512e7d224b6725a5bd372ee6
Андрій Наливайко <andrijnalivajko@MacBook-Pro-Andrij.local>
2021-07-09 17:45:17 +0300

    Check me

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+centerTheElement();
+
+function centerTheElement() {
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  const spiderSize = spider.clientWidth / 2;
+
+  const createWidth = wall.clientWidth / 2;
+  const createHeigth = wall.clientHeight / 2;
+
+  spider.style.top = `${createWidth - spiderSize}px`;
+  spider.style.left = `${createHeigth - spiderSize}px`;
+};
