636c29bdffb115a1852f1206237fe793804db529
Yaslik1033 <55955258+Yaslik1033@users.noreply.github.com>
2021-07-11 19:26:16 +0300

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const wallSize = wall.clientHeight;
+const spiderSize = spider.clientHeight;
+
+spider.style.top = `${(wallSize - spiderSize) / 2}px`;
+spider.style.left = `${(wallSize - spiderSize) / 2}px`;
