8ecb8964eae612cf684b7f0de26c9b3e3b6035bf
MaximTsyhanenko <maksym.tsyganenko85@gmail.com>
2021-08-14 00:50:20 +0300

    solution v2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,8 +4,6 @@ const field = document.querySelector('.wall');
 
 const spider = document.querySelector('.spider');
 
-const spiderCoords = spider.getBoundingClientRect();
+spider.style.left = field.clientWidth / 2 - spider.offsetWidth / 2 + 'px';
 
-spider.style.left = field.clientWidth / 2 - spiderCoords.width / 2 + 'px';
-
-spider.style.top = field.clientHeight / 2 - spiderCoords.height / 2 + 'px';
+spider.style.top = field.clientHeight / 2 - spider.offsetHeight / 2 + 'px';
