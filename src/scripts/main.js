e0098ad2ee3586f71fa49038fbe0aca2b8c00c55
MaximTsyhanenko <maksym.tsyganenko85@gmail.com>
2021-08-14 00:39:04 +0300

    solution v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write code here
+const field = document.querySelector('.wall');
+
+const spider = document.querySelector('.spider');
+
+const spiderCoords = spider.getBoundingClientRect();
+
+spider.style.left = field.clientWidth / 2 - spiderCoords.width / 2 + 'px';
+
+spider.style.top = field.clientHeight / 2 - spiderCoords.height / 2 + 'px';
