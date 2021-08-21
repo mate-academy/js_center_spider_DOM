7a2d13f217fabcca93fd3e7feb58435102320423
Svyatoslav Victor <victorsvyatoslav@gmail.com>
2021-08-21 10:58:33 +0300

    Correction 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+const spiderCoords = spider.offsetWidth / 2;
+const wallCoords = wall.clientWidth / 2;
 
-spider.style.top = '50%';
-spider.style.left = '50%';
-spider.style.transform = 'translateX(-50%) translateY(-50%)';
+spider.style.top = `${wallCoords - spiderCoords}px`;
+spider.style.left = spider.style.top;
