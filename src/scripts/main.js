7453df1dc4ab31643dbf6cdbffd72ecfb0764940
Oleksandr Hrachov <ol.grachov@gmail.com>
2021-08-25 16:30:55 +0300

    SOLUTION

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+
+// какой вариант лучше использовать getComputedStyle или clientWidth?
+
+// const wallStyle = getComputedStyle(wall);
+// spider.style.left = `${parseFloat(wallStyle.width) / 2}px`;
+// spider.style.top = `${parseFloat(wallStyle.height) / 2}px`;
+
+spider.style.left = `${wallWidth / 2}px`;
+spider.style.top = `${wallHeight / 2}px`;
+spider.style.transform = 'translate(-50%, -50%)';
