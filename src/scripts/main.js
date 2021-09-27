b29b710c773923674ae3ed1fa364e2f9735ecb39
Anton Forzun <anton_forzun@ukr.net>
2021-09-27 11:58:21 +0300

    spider_fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,13 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-spider.style.top = '45%';
-spider.style.left = '45%';
+const spiderStyle = getComputedStyle(spider);
+const width = parseFloat(spiderStyle.width);
+const height = parseFloat(spiderStyle.height);
 
-wall.append(spider);
+const wallStyle = getComputedStyle(wall);
+const wallWidth = parseFloat(wallStyle.width);
+const wallHeigt = parseFloat(wallStyle.height);
+
+spider.style.paddingTop = (wallWidth / 2) - (width / 2) + 'px';
+spider.style.paddingLeft = (wallHeigt / 2) - (height / 2) + 'px';
