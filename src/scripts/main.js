7db58c4401954c90bf20e4b6b009923282a6f1f7
Dmitriy Hrytsak <hrytsakmail@gmail.com>
2021-11-14 21:20:10 +0200

    Help Spider Man to get to the center of Manhattan building

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+const manhattanBuilding = document.querySelector('.wall');
+const spiderMan = document.querySelector('.spider');
+
+const centerX = manhattanBuilding.clientWidth / 2;
+const centerY = manhattanBuilding.clientHeight / 2;
+const spiderManFatness = spiderMan.offsetWidth / 2;
+const spiderManHeight = spiderMan.offsetHeight / 2;
+
+spiderMan.style.left = `${centerX - spiderManFatness}px`;
+spiderMan.style.top = `${centerY - spiderManHeight}px`;
