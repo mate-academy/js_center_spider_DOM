49ba9a8c9b3586692df573a9edbee712e5180aa8
Anastasiia Afanasieva <anastasiiaafanasieva@Anastasiias-MacBook-Air.local>
2021-12-06 18:03:54 +0200

    ccversion2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,9 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-wall.style.cssText = 'display: flex; align-items: center;';
+wall.style.display = 'flex';
+wall.style.alignItems = 'center';
 
-spider.style.cssText = 'margin-left: auto; margin-right: auto;'
-+ ' position: relative;';
+spider.style.marginLeft = 'auto';
+spider.style.marginRight = 'auto';
+spider.style.position = 'relative';
