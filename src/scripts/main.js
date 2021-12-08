20e3c65b5a8894b0071ae501bf6fd9da791819d3
Pasechnik Vitaly <pasechnik.vitaly.11@gmail.com>
2021-12-09 00:56:13 +0200

    Latest saved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const spyder = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+const style = getComputedStyle(wall);
+const spyderStyle = getComputedStyle(spyder);
+
+spyder.style.height = '70px';
+
+wall.style.height = '450px';
+wall.style.width = '450px';
+wall.style.border = '15px solid #e1e1e1';
+wall.style.margin = '30px';
+wall.style.backgroundImage = 'url(images/brick-wall.svg)';
+wall.style.backgroundColor = '#efb685';
+
+spyder.style.paddingLeft = `${parseInt(style.width)
+   / 2 - parseInt(spyderStyle.width) / 2}px`;
+
+spyder.style.paddingTop = `${parseInt(style.width)
+   / 2 - parseInt(spyderStyle.width) / 2}px`;
