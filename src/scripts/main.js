d813c9b746d560e0bdd08607dc3b67fef441eb89
Elina Kovalenko <kovalenkoelin@gmail.com>
2021-12-19 20:28:08 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+const base = document.querySelector('.wall');
+const dot = document.querySelector('.spider');
+const style = getComputedStyle(base);
+
+dot.style.top = `${parseFloat(style.height) / 2}px`;
+dot.style.left = `${parseFloat(style.width) / 2}px`;
+dot.style.transform = 'translate(-50%, -50%)';
