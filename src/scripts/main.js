ec46c1e3a5ae1656bcfb1836a2f70505c238f746
Pasechnik Vitaly <pasechnik.vitaly.11@gmail.com>
2021-12-31 02:51:06 +0200

    last saved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+const realWall = getComputedStyle(wall);
+
+spider.style.top = `${(parseFloat(realWall.height)
+  - spider.clientHeight) / 2}px`;
+
+spider.style.left = `${(parseFloat(realWall.width)
+  - spider.clientWidth) / 2}px`;
