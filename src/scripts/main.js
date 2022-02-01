f41244e5a0b7cbb25788664b00a89d9516e80968
Ivan Shulhan <92580512+IvanShulhan@users.noreply.github.com>
2022-02-01 13:40:18 +0200

    solution-2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,5 +2,4 @@
 
 const spider = document.querySelector('.spider');
 
-spider.style.cssText = 'top: 50%; left: 50%;'
-  + 'transform: translate3d(-50%, -50%, 0)';
+spider.style.cssText = 'margin: 50%; transform: translate3d(-50%, -50%, 0)';
