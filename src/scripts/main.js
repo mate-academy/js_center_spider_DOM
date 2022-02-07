a9e93aa9dae2b7ea3adbcc85a975dd53f03c84a1
Luhovyi <95681104+Luhovyi@users.noreply.github.com>
2022-02-07 17:10:03 +0200

    js_center_spider_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
 // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const style = getComputedStyle(wall);
+
+spider.style.top = `${parseFloat(style.height) / 2}px`;
+spider.style.left = `${parseFloat(style.width) / 2}px`;
+spider.style.transform = 'translate(-50%, -50%)';
