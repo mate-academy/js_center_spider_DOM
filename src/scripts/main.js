776f6642af1dc8b7f8a6269262eb67a0eae71cf3
KristinaTar <kristina.tarashchuk@gmail.com>
2022-02-20 18:31:25 +0200

    spider centered

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,7 @@
 'use strict';
 
-// write code here
+const el = document.getElementsByClassName('spider');
+
+el[0].style.top = '50%';
+el[0].style.left = '50%';
+el[0].style.transform = 'translate(-50%, -50%)';
