1b1aff2304a3f41224e9e2923bec370b6fcf9758
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-02-20 20:20:36 +0200

    Way with display checking

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,20 @@
 'use strict';
 // the first way of solving
 
-/* const wallElem = document.querySelector('.wall');
+const wallElem = document.querySelector('.wall');
 
 wallElem.style.cssText
-= 'display: flex; justify-content: center; align-items: center'; */
+= 'display: flex; justify-content: center; align-items: center';
 
 // the second way of solving
 
-const wallElem = document.querySelector('.wall');
+/* const wallElem = document.querySelector('.wall');
 const spiderElem = document.querySelector('.spider');
-const spiderTopPosition = parseFloat(wallElem.offsetHeight) / 2
+const spiderTopPosition = parseFloat(wallElem.clientHeight) / 2
 - parseFloat(spiderElem.offsetHeight) / 2;
-const spiderLeftPosition = parseFloat(wallElem.offsetWidth) / 2
+const spiderLeftPosition = parseFloat(wallElem.clientWidth) / 2
 - parseFloat(spiderElem.offsetWidth) / 2;
 
 spiderElem.style.top = `${spiderTopPosition}px`;
 
-spiderElem.style.left = `${spiderLeftPosition}px`;
+spiderElem.style.left = `${spiderLeftPosition}px`; */
