1b88044215a12c660b47ea646075dc127fae2eaa
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-02-20 20:24:37 +0200

    The sizes getting was fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,14 @@
 'use strict';
-// the first way of solving
+// the first way of solving if position absolute is absent
 
-const wallElem = document.querySelector('.wall');
+/* const wallElem = document.querySelector('.wall');
 
 wallElem.style.cssText
-= 'display: flex; justify-content: center; align-items: center';
+= 'display: flex; justify-content: center; align-items: center'; */
 
 // the second way of solving
 
-/* const wallElem = document.querySelector('.wall');
+const wallElem = document.querySelector('.wall');
 const spiderElem = document.querySelector('.spider');
 const spiderTopPosition = parseFloat(wallElem.clientHeight) / 2
 - parseFloat(spiderElem.offsetHeight) / 2;
@@ -17,4 +17,4 @@ const spiderLeftPosition = parseFloat(wallElem.clientWidth) / 2
 
 spiderElem.style.top = `${spiderTopPosition}px`;
 
-spiderElem.style.left = `${spiderLeftPosition}px`; */
+spiderElem.style.left = `${spiderLeftPosition}px`;
