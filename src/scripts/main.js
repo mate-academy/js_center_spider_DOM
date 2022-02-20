8329d5798abdeec0a910dd8a95018e920d2c72df
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-02-20 20:14:39 +0200

    The script was implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
+// the first way of solving
 
-// write code here
+/* const wallElem = document.querySelector('.wall');
+
+wallElem.style.cssText
+= 'display: flex; justify-content: center; align-items: center'; */
+
+// the second way of solving
+
+const wallElem = document.querySelector('.wall');
+const spiderElem = document.querySelector('.spider');
+const spiderTopPosition = parseFloat(wallElem.offsetHeight) / 2
+- parseFloat(spiderElem.offsetHeight) / 2;
+const spiderLeftPosition = parseFloat(wallElem.offsetWidth) / 2
+- parseFloat(spiderElem.offsetWidth) / 2;
+
+spiderElem.style.top = `${spiderTopPosition}px`;
+
+spiderElem.style.left = `${spiderLeftPosition}px`;
