b4e0049ab4989af2ddbe71873057e7958e3ae2da
KristinaTar <kristina.tarashchuk@gmail.com>
2022-02-21 15:18:04 +0200

    added querySelector

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const el = document.getElementsByClassName('spider');
+const el = document.querySelector('.spider');
 
-el[0].style.top = '50%';
-el[0].style.left = '50%';
-el[0].style.transform = 'translate(-50%, -50%)';
+el.style.top = '50%';
+el.style.left = '50%';
+el.style.transform = 'translate(-50%, -50%)';
