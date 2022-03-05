94827b63cad05ba232361fbbaa027781e67e1d15
Misha Novikov <novikov1807@gmail.com>
2022-03-05 18:27:07 +0200

    Complite

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+// const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+// const w = wall.style.width.replace('px', '') / 2
+// - spider.style.width.replace('px', '') / 2;
+// const h = wall.style.height / 2 - spider.style.height / 2;
+
+// console.log(wall.style);
+
+// spider.style.top = `${h}px`;
+// spider.style.left = `${w}px`;
+
+spider.style.top = `175px`;
+spider.style.left = `175px`;
