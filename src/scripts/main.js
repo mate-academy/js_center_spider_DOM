4157bfe761385988f7abcb19f515ae9829b6c4f7
Misha Novikov <novikov1807@gmail.com>
2022-03-09 22:30:42 +0200

    all complite like best

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,13 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const styleWall = getComputedStyle(wall);
 const spider = document.querySelector('.spider');
+const styleSpider = getComputedStyle(spider);
+const resultTop = styleWall.width.replace('px', '') / 2
+- styleSpider.width.replace('px', '') / 2;
+const resultLeft = styleWall.height.replace('px', '') / 2
+- styleSpider.height.replace('px', '') / 2;
 
-spider.style.top = `45%`;
-spider.style.left = `45%`;
+spider.style.top = `${resultTop}px`;
+spider.style.left = `${resultLeft}px`;
