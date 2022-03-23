c8b6c837f361f0b728a93b96ce576ac2e74dc073
Vladyslav317 <you@example.com>
2022-03-23 15:15:52 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+// wall.style.display = 'flex';
+// wall.style.justifyContent = 'center';
+// wall.style.alignItems = 'center';
+
+// spider.style.position = 'unset';
+
+spider.style.top = `${(wall.clientHeight - spider.clientHeight) / 2}px`;
+spider.style.left = `${(wall.clientWidth - spider.clientWidth) / 2}px`;
