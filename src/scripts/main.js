db92dc6263afa1935769a9da572eedbe8da0579d
Maria Luschik <marialyschik@Marias-MacBook-Air.local>
2022-03-31 13:28:01 +0300

    changed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,7 @@
 const field = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 const spiderTop = field.clientHeight / 2 - spider.clientHeight / 2;
-const spiderLeft = field.clientHeight / 2 - spider.clientHeight / 2;
+const spiderLeft = field.clientWidth / 2 - spider.clientWidth / 2;
 
 spider.style.top = `${spiderTop}px`;
 spider.style.left = `${spiderLeft}px`;
