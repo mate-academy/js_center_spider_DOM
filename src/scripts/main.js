b61a7b479167d16c3b87966f9c108ae5a56d8b0b
Maria Luschik <marialyschik@Marias-MacBook-Air.local>
2022-03-31 13:16:05 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+const field = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const spiderTop = field.clientHeight / 2 - spider.clientHeight / 2;
+const spiderLeft = field.clientHeight / 2 - spider.clientHeight / 2;
+
+spider.style.top = `${spiderTop}px`;
+spider.style.left = `${spiderLeft}px`;
