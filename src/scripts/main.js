ac62bd9723e31d1e7594b91e67937c36917fa4c2
Batick3737 <antonandriy@ukr.net>
2022-03-31 12:57:09 +0300

    write code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+const topSpider = (wall.clientHeight - spider.clientHeight) / 2;
+const leftSoider = (wall.clientWidth - spider.clientWidth) / 2;
+
+spider.style.top = `${topSpider}px`
+spider.style.left = `${leftSoider}px`
