649fbe9fd391b1513a35343058ef8eaf4f3b4da8
Max Shylenko <maxshylenko@gmail.com>
2022-04-18 15:24:34 +0200

    center spider DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const stylesForWall = getComputedStyle(wall);
+const stylesForSpider = getComputedStyle(spider);
+
+spider.style.top = `${(parseFloat(stylesForWall.height) / 2)
+  - (parseFloat(stylesForSpider.height) / 2)}px`;
+
+spider.style.left = `${(parseFloat(stylesForWall.width) / 2)
+  - (parseFloat(stylesForSpider.width) / 2)}px`;
