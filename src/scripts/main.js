d110b508442b4740e45e8b77b07465e9f80ddc89
MariaBondar <marybondar777@gmail.com>
2022-04-20 12:39:45 +0200

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,7 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+wall.style = 'display: flex; align-items: center; justify-content: center';
+spider.style = 'position: static';
