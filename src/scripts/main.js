1f446f25b984a3c83bd394ea429a805cc7dc89a6
ShapovalDenys <denys.igorevich@gmail.com>
2022-05-17 15:23:26 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+// search elements
+const field = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+// add style
+field.style.cssText = `
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  `;
+spider.style.position = `relative`;
