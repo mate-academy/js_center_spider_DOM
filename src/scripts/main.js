c18a1872f07322e535119498de58c6ade5f67b3e
Roman Lipatov <96875973+roman-lipatov@users.noreply.github.com>
2022-05-23 21:27:28 +0300

    Move Spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
 // write code here
+
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+const spiderTop = wall.clientHeight / 2 - spider.clientHeight / 2;
+const spiderLeft = wall.clientWidth / 2 - spider.clientWidth / 2;
+
+spider.style.top = `${spiderTop}px`;
+spider.style.left = `${spiderLeft}px`;
