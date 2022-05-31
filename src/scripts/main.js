9d31da7836dd5356c381152c9959f4bce68e3691
Katya Suvorova <katerinasuvorova97@gmail.com>
2022-05-31 12:25:47 +0300

    Solition

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('[class = wall]');
+const spider = document.querySelector('[class = spider]');
+
+const spiderHeight = spider.offsetHeight;
+
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+
+const countOfSides = 2;
+
+const topSide = (wallHeight - spiderHeight) / countOfSides;
+const leftSide = (wallWidth - spiderHeight) / countOfSides;
+
+spider.style.top = `${topSide}px`;
+spider.style.left = `${leftSide}px`;
