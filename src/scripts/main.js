c6cbb56ba433834f844dc73a61a2a8bca50345b5
Volodumur Vitinskiy <vovahunter1989@gmail.com>
2022-06-10 14:26:09 +0300

    mom, I chased away the spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const area = document.querySelector('.wall');
+const spiderMan = document.querySelector('.spider');
+
+area.style.display = 'flex';
+area.style.alignItems = 'center';
+area.style.justifyContent = 'center';
+spiderMan.style.top = 'auto';
+spiderMan.style.left = 'auto';
