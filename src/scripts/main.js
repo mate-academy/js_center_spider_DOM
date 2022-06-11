5b47e743454f30466806fcd71161b9c92b692efc
Volodumur Vitinskiy <vovahunter1989@gmail.com>
2022-06-11 16:13:21 +0300

    Mom, the spider came back, but I chased him away again

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,8 @@
 const area = document.querySelector('.wall');
 const spiderMan = document.querySelector('.spider');
 
-area.style.display = 'flex';
-area.style.alignItems = 'center';
-area.style.justifyContent = 'center';
-spiderMan.style.top = 'auto';
-spiderMan.style.left = 'auto';
+const spiderTop = area.clientHeight / 2 - spiderMan.clientHeight / 2;
+const spiderLeft = area.clientWidth / 2 - spiderMan.clientWidth / 2;
+
+spiderMan.style.top = `${spiderTop}px`;
+spiderMan.style.left = `${spiderLeft}px`;
