1b01e3d74607821afedd80272487f5b64353af53
bogdana18 <usenkobogdana19@gmail.com>
2022-06-15 17:36:41 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+spider.style.top = parseFloat(wall.clientHeight) / 2
+- parseFloat(spider.offsetHeight) / 2 + 'px';
+
+spider.style.left = parseFloat(wall.clientWidth) / 2
+- parseFloat(spider.offsetWidth) / 2 + 'px';
