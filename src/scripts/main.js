f5c0c36f624ae1cf33b9cb89bed2f0cf5639c222
Artem Furdela <furdela.artem@gmail.com>
2022-06-18 16:29:46 +0300

    Spider first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
 // write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+const calcDistance = prop => {
+  return `${(parseFloat(getComputedStyle(wall)[prop]) / 2)
+  - (parseFloat(getComputedStyle(spider)[prop]) / 2)}px`;
+};
+
+spider.style.top = calcDistance('height');
+spider.style.left = calcDistance('width');
