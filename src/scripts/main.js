2075e7305eb9787610f046340d6bf093fc597121
Vitaliy Anochin <anuhin.vital@gmail.com>
2022-06-23 09:11:09 +0300

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+const brick = document.querySelector('.wall');
+const element = document.querySelector('.spider');
+
+const width = brick.clientWidth / 2 - element.offsetWidth / 2;
+const height = brick.clientHeight / 2 - element.offsetHeight / 2;
+
+element.style = `top: ${height}px;  left: ${width}px`;
