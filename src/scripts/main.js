871503d3436923f5f58e54a2a3c3669b7fbfc2b3
Aleksey Pushkalov <pushkalov86@gmail.com>
2022-06-23 22:55:28 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+const spiderWidth = wall.clientWidth / 2 - spider.clientWidth / 2 - 10;
+const spiderHeight = wall.clientHeight / 2 - spider.clientHeight / 2 - 10;
+
+spider.style.left = spiderWidth + 'px';
+spider.style.top = spiderHeight + 'px';
