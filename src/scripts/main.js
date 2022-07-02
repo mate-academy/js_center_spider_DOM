ea993a445a40bf033a8a997f062ff94ec0dee3a8
DimaShm <shmagindmitry@gmail.com>
2022-07-02 21:31:28 +0300

    Solution spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+const wall = document.getElementsByClassName('wall')[0];
+const spider = document.getElementsByClassName('spider')[0];
+
+spider.style = ` 
+  top: ${wall.clientHeight / 2 - spider.offsetHeight / 2}px; 
+  left: ${wall.clientWidth / 2 - spider.offsetWidth / 2}px
+`;
