b60d9ea13f6e30c67a0c197ffaf617f0b7ec1e9a
silchyn <ivan.silchin@gmail.com>
2022-07-06 18:06:10 +0300

    tried another approach

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,8 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-spider.style.top = '50%';
-spider.style.left = '50%';
-spider.style.transform = 'translate(-50%, -50%)';
+spider.style.transform = `translate(${(wall.clientWidth
+  - spider.clientWidth) / 2}px, ${(wall.clientHeight
+  - spider.clientHeight) / 2}px)`;
