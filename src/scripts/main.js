e79774b8239363c0892bad0e1a58bcfad02838d4
Oleksii Budnik <budnikoleksii@gmail.com>
2022-07-06 19:03:31 +0300

    refactored with clientparams and offset

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,8 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-wall.style.display = 'flex';
-spider.style.position = 'static';
-spider.style.margin = 'auto';
+const centerWidth = wall.clientWidth / 2 - spider.offsetWidth / 2;
+const centerHeight = wall.clientHeight / 2 - spider.offsetHeight / 2;
+
+spider.style.left = `${centerWidth}px`;
+spider.style.top = `${centerHeight}px`;
