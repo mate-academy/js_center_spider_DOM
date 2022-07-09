7a534245c32622243e7f3679c592953aec3c14d9
Stanislav Bzhezinskyi <stasiva28@gmail.com>
2022-07-10 00:18:51 +0300

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // write code here
+const wall = document.querySelector('.wall');
+const newElem = document.createElement('div');
+const spider = document.querySelector('.spider');
+
+wall.append(newElem);
+newElem.append(spider);
+
+newElem.style.cssText = `
+  position: absolute;
+  top: ${wall.clientHeight / 2 - spider.clientHeight / 2}px;
+  left: ${wall.clientWidth / 2 - spider.clientHeight / 2}px
+`;
