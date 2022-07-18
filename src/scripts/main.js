86d7fdc4e59e2d2f4890670bc7d2fc8b58b3c320
Sergii Maniurov <maniurov_sv@ukr.net>
2022-07-18 17:19:30 +0300

    moved the spider to the center of the parent element

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
-// write code here
+const elWall = document.querySelector('.wall');
+const elSpider = document.querySelector('.spider');
+
+elSpider.style.transform = `translate(
+  ${(elWall.clientWidth - elSpider.clientWidth) / 2}px,
+  ${(elWall.clientHeight - elSpider.clientHeight) / 2}px)`;
