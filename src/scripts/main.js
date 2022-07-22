8a732cb3b6f255593692b34ee4a621d6a14c03d4
dasha <dashasv76@gmail.com>
2022-07-22 16:26:09 +0300

    spider in center

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+spider.style.transform = `translate(${
+  (wall.clientWidth - spider.clientWidth) / 2}px, ${
+  (wall.clientHeight - spider.clientHeight) / 2}px)`;
