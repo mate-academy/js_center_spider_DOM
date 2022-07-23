ad12313c21c798239c3400a48cf79e89d0c03590
Oleg Vetrov <oleg94038@gmail.com>
2022-07-23 20:05:28 +0300

    Added spyder on center

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+function setChildrenOnCenter(fatherElement, children) {
+  const width = (fatherElement.clientWidth / 2) - (children.clientWidth / 2);
+  const height = (fatherElement.clientHeight / 2) - (children.clientHeight / 2);
+
+  children.style.top = height + 'px';
+  children.style.left = width + 'px';
+}
+
+const wall = document.querySelector('.wall');
+const spyder = document.querySelector('.spider');
+
+setChildrenOnCenter(wall, spyder);
