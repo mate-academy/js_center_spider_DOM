994a57874e8fb72484d9b188a489ed71e7c98cc0
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2022-07-28 09:24:02 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+// const wall = document.querySelector('.wall');
+
+// console.log(spider.clientWidth)
+
+spider.style.top = '50%';
+spider.style.marginTop = -spider.offsetHeight / 2 + 'px';
+spider.style.marginLeft = -spider.offsetWidth / 2 + 'px';
+spider.style.left = '50%';
+
+// console.log(spider.offsetTop, spider.offsetRight)
