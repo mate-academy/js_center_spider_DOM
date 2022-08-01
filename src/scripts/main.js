ba13e239a5d1bfed27db9573aa42f4d164d740dd
Vitaliy Bushovskiy <Bushovskiy@gmail.com>
2022-08-01 15:12:39 +0300

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
-// write code here
+const div = document.querySelector('.wall');
+
+const img = document.querySelector('.spider');
+
+img.style.left = Math.round(div.clientWidth / 2 - img.offsetWidth / 2) + 'px';
+img.style.top = Math.round(div.clientHeight / 2 - img.offsetHeight / 2) + 'px';
