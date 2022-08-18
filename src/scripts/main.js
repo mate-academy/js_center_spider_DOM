2891741c18ef44aabc22e9644cbba81187ba9cef
Oksana Shvets <ksenyasvatko1993@gmail.com>
2022-08-18 12:43:04 +0200

    Solution4

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,15 +6,14 @@ const spider = document.querySelector('.spider');
 
 const startTopS = spider.getBoundingClientRect().top + window.pageYOffset;
 const startLeftS = spider.getBoundingClientRect().left + window.pageYOffset;
-const wallWidth = wall.getBoundingClientRect().width + window.pageYOffset;
-const spiderWidth = spider.getBoundingClientRect().width + window.pageYOffset;
+const wallWidth = wall.getBoundingClientRect().width + window.pageXOffset;
+const spiderWidth = spider.getBoundingClientRect().width + window.pageXOffset;
 
 const wallHeight = wall.getBoundingClientRect().height + window.pageXOffset;
-const spiderHeight = spider.getBoundingClientRect().height + window.pageXOffset;
-const coorsX = startTopS + (wallHeight / 2) - spiderHeight - 10;
+const coorsX = startTopS + (wallHeight / 2);
 
-spider.style.top = coorsX + 'px';
+spider.style.top = coorsX / 2 + 'px';
 
-const coorsY = startLeftS + (wallWidth / 2) - spiderWidth - 10;
+const coorsY = startLeftS + (wallWidth / 2) + spiderWidth;
 
-spider.style.left = coorsY + 'px';
+spider.style.left = coorsY / 2 + 'px';
