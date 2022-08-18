481f20ed08b16e1e69b9ae97696a0ca65def865a
Hovera-Liubomyr <ljusuk@gmail.com>
2022-08-18 12:13:05 +0300

    center_spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+const coorX = (wall.clientWidth / 2) - (spider.clientWidth / 2);
+const coorY = (wall.clientHeight / 2) - (spider.clientHeight / 2);
+
+spider.style.top = `${coorY}px`;
+spider.style.left = `${coorX}px`;
