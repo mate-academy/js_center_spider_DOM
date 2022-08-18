2f140094175323230465794dec8f33ead4305b47
Oksana Shvets <ksenyasvatko1993@gmail.com>
2022-08-18 13:09:23 +0200

    Solution5

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,8 @@
 'use strict';
 
 // write code here
-const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-const startTopS = spider.getBoundingClientRect().top + window.pageYOffset;
-const startLeftS = spider.getBoundingClientRect().left + window.pageYOffset;
-const wallWidth = wall.getBoundingClientRect().width + window.pageXOffset;
-const spiderWidth = spider.getBoundingClientRect().width + window.pageXOffset;
-
-const wallHeight = wall.getBoundingClientRect().height + window.pageXOffset;
-const coorsX = startTopS + (wallHeight / 2);
-
-spider.style.top = coorsX / 2 + 'px';
-
-const coorsY = startLeftS + (wallWidth / 2) + spiderWidth;
-
-spider.style.left = coorsY / 2 + 'px';
+spider.style.setProperty(`top`, `50%`);
+spider.style.setProperty(`left`, `50%`);
+spider.style.setProperty(`transform`, `translateX(-50%) translateY(-50%)`);
