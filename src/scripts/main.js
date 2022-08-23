67292a721df160e67c94c73b1c2e53f7ba5dd251
Oleksandr Brui <oleksandr.bruigit@gmail.com>
2022-08-23 23:07:22 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
 // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+// get all wall's styles
+const wallStyles = getComputedStyle(wall);
+// get border width of the wall
+const wallBorderWidth = parseFloat(wallStyles.borderWidth);
+// get content width of the wall
+const wallContentWidth = wall.offsetWidth - wallBorderWidth * 2;
+// get content height of the wall
+const wallContentHeight = wall.offsetHeight - wallBorderWidth * 2;
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
+
+spider.style.left = `${(wallContentWidth - spiderWidth) / 2}px`;
+spider.style.top = `${(wallContentHeight - spiderHeight) / 2}px`;
