ac5dc4ce6bdebf0000c6880e1ee600bf852c47aa
Andriy Kachalkin <kachalkin1306@gmail.com>
2022-08-27 14:27:47 +0300

    fixed semicolon

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,7 @@ const spider = document.querySelector('.spider');
 
 function centerElement(outer, inner) {
   inner.style.top = (outer.clientHeight - inner.clientHeight) / 2 + 'px';
-  inner.style.left = (outer.clientWidth - inner.clientWidth) / 2 + 'px'
+  inner.style.left = (outer.clientWidth - inner.clientWidth) / 2 + 'px';
 }
 
 centerElement(wall, spider);
