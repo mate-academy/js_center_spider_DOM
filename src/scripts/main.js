06babf439e89b2cbc372df39b38707841d93609c
Dolhov Andrey <andrej.dolgov.042110@gmail.com>
2022-08-29 19:36:51 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,7 @@
 'use strict';
 
-const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-spider.style.position = 'static';
-
-wall.style.display = 'flex';
-wall.style.justifyContent = 'center';
-wall.style.alignItems = 'center';
+spider.style.top = '50%';
+spider.style.left = '50%';
+spider.style.transform = 'translate(-50%, -50%)';
