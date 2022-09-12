b4494970ea7cc366e9282c5d151b12d2531a6c54
Boris Kerekesha <kerekesha90@ukr.net>
2022-09-12 09:21:44 +0300

    spider_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,8 @@
 const spider = document.getElementsByClassName('spider')[0];
 const field = document.getElementsByClassName('wall')[0];
 
-field.style.possition = 'relative';
-spider.style.possition = 'absolute';
+field.style.position = 'relative';
+spider.style.position = 'absolute';
 spider.style.top = '50%';
 spider.style.left = '50%';
 spider.style.transform = 'translate(-50%, -50%)';
