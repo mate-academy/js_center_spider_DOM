33a5762a6ddef696a4597d33931ce47c9c680df2
Anastasiia Mahalias <anastasiabatychenko@gmail.com>
2022-09-17 12:29:56 +0300

    Solution fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,7 @@
 'use strict';
 
-const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-wall.style.display = 'flex';
-wall.style.justifyContent = 'center';
-
-spider.style.position = 'relative';
-spider.style.margin = 'auto 0';
+spider.style.top = '50%';
+spider.style.left = '50%';
+spider.style.transform = 'translate(-50%, -50%)';
