c62b5a4d9cff5f9106122efea0ebd3ee7c3b9b78
Max Bovtun <maxbovtun@gmail.com>
2022-09-18 20:34:07 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
 // write code here
+const spider = document.querySelector('.spider');
+
+setTimeout(() => {
+  spider.style.left = '50%';
+  spider.style.top = '50%';
+  spider.style.transform = 'translate(-50%, -50%)';
+}, 600);
