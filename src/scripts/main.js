bca7d9db201ed4c3b37d2c3baee6da65b58b4219
Oleh Savchuk <sav4uk.oleg@gmail.com>
2022-09-22 23:33:33 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,8 @@
 
 const spider = document.querySelector('.spider');
 
-spider.style.top = 50 + '%';
+spider.style.top = '50%';
 
-spider.style.left = 50 + '%';
+spider.style.left = '50%';
 
 spider.style.transform = 'translate(-50%, -50%)';
