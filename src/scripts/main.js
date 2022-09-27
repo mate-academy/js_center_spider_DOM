1c19898fe704ce32b6fc71e550d7eaf137221c3d
Andriy Andrieiev <proph7000@gmail.com>
2022-09-27 19:07:12 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.style.display = 'grid';
+wall.style.placeItems = 'center';
+spider.style.position = 'initial';
