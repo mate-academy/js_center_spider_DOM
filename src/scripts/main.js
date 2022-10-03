7f75c666393acf4bcaed8972186daeb97362c233
Serhii Yunnikov <yunnikov@icloud.com>
2022-10-03 10:13:01 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,7 @@
 'use strict';
 
-// write code here
+const spyder = document.querySelector('.spider');
+
+spyder.style.transform = 'translate(-50%, -50%)';
+spyder.style.top = '50%';
+spyder.style.left = '50%';
