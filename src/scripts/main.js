44d01b3c1680cab8eb9f48d8601374a49221636e
Zykhor Andrii <zuxoran@gmail.com>
2022-10-06 22:27:17 +0300

    task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
 // write code here
+const blok = document.querySelector('.spider');
+
+blok.style.setProperty(`top`, `50%`);
+blok.style.setProperty(`left`, `50%`);
+blok.style.setProperty(`transform`, `translateX(-50%) translateY(-50%)`);
