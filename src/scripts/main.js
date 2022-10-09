063722c546852e4c4e05d3f12d7b0337bffae98f
Oleksiy Uchaev <corob.dir@gmail.com>
2022-10-09 08:58:44 +0300

    little improvement

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,7 @@
 
 const spider = document.querySelector('.spider');
 const wall = document.querySelector('.wall');
-const center = `${(wall.clientWidth - spider.offsetWidth) / 2}px`;
 
-spider.style.top = center;
-spider.style.left = center;
+spider.style.top
+  = spider.style.left
+  = `${(wall.clientWidth - spider.offsetWidth) / 2}px`;
