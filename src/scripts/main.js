d8f8afcf81810091e4da68c5605d154a7f14fc7d
Ruslan Nedaiborshch <rnedaiborshch@gmail.com>
2022-10-17 15:49:20 +0300

    cod for center spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+
+const styleWall = getComputedStyle(wall);
+
+const spider = document.querySelector('.spider');
+
+const styleSpider = getComputedStyle(spider);
+
+spider.style.top = `${(parseFloat(styleWall.width) / 2)
+  - parseFloat(styleSpider.width) / 2}px`;
+
+spider.style.left = `${(parseFloat(styleWall.height) / 2)
+- parseFloat(styleSpider.height) / 2}px`;
