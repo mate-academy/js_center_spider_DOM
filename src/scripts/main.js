38e36b6118d0d51992780d29190d715bba166be2
Sergii Iatsenko <sergii.iatsenko@gmail.com>
2022-10-17 19:55:41 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const spiderBox = document.createElement('div');
+
+wall.style.display = 'flex';
+wall.style.alignItems = 'center';
+wall.style.justifyContent = 'center';
+
+spider.style.position = 'static';
+
+wall.append(spiderBox);
+spiderBox.append(spider);
