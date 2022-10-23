abc952b9d18d243e84eaf8759ddc70f5010cd384
Mykola Yakymiv <mykola8426@gmail.com>
2022-10-23 13:43:07 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const divWall = document.body.getElementsByClassName('wall');
+const spider = document.body.getElementsByClassName('spider');
+
+const stylesOfSpider = getComputedStyle(spider[0]);
+const spiderImageWidth = parseFloat(stylesOfSpider.width);
+const spiderImageHeight = parseFloat(stylesOfSpider.height);
+
+divWall[0].style.position = 'relative';
+divWall[0].style.boxSizing = 'border-box';
+spider[0].style.position = 'absolute';
+spider[0].style.top = `50%`;
+spider[0].style.left = `50%`;
+
+spider[0].style.margin = `-${spiderImageWidth / 2}px 0px
+ 0px -${spiderImageHeight / 2}px`;
