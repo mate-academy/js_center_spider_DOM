5f25631ea32745cb49727d91d5ecf97fc0d26fdd
Max Fedak <max.fedak@gmail.com>
2022-11-02 17:04:10 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+
+spider.style.top = '50%';
+spider.style.left = '50%';
+spider.style.transform = 'translate(-50%, -50%)';
 // write code here
