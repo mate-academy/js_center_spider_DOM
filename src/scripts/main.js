53158e04e2e45780aee1d7115b33c89376c2eb7c
Denis Cherevaschenko <deniska.cherevaschenko@gmail.com>
2022-11-07 17:59:56 +0200

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
-// write code here
+const elem = document.querySelector('.spider');
+
+elem.style.position = 'absolute';
+elem.style.left = '50%';
+elem.style.top = '50%';
+elem.style.transform = 'translate(-50%, -50%)';
