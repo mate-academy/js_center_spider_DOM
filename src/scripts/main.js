6237d42e79f91d90c8920e49f9ae4f5c2ef94a10
Tetiana Melnyk <uasmiykaua@gmail.com>
2022-11-25 16:02:38 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,13 +3,11 @@
 const spider = document.querySelector('.spider');
 const wall = document.querySelector('.wall');
 
-const wallWidth = parseFloat(getComputedStyle(wall).width);
-const wallHeight = parseFloat(getComputedStyle(wall).height);
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
 
-const spiderWidth = parseFloat(getComputedStyle(spider).width);
-const spiderHeight = parseFloat(getComputedStyle(spider).height);
+const spiderWidth = spider.clientWidth;
+const spiderHeight = spider.clientHeight;
 
 spider.style.top = (wallHeight / 2 - spiderHeight / 2) + 'px';
 spider.style.left = (wallWidth / 2 - spiderWidth / 2) + 'px';
-
-
