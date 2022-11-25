f17bc5250cf437aee4d192ebbfeea4b6ff328e37
Maks <comanderius@gmail.com>
2022-11-25 14:00:13 +0100

    Center spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+const wallHeight = wall.clientHeight;
+const wallWigth = wall.clientWidth;
+const spiderHeight = spider.clientHeight;
+const spiderWigth = spider.clientWidth;
+
+spider.style.top = `${(wallHeight - spiderHeight) / 2}px`;
+spider.style.left = `${(wallWigth - spiderWigth) / 2}px`;
