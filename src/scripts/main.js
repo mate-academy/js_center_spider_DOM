f09d5a850a65be207fc1e536178176991f81fcac
Artem Hirzhev <a.hirzhev@gmail.com>
2022-12-05 20:41:06 +0200

    Initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.style.display = 'flex';
+wall.style.justifyContent = 'center';
+wall.style.alignItems = 'center';
+spider.style.position = 'static';
