1cabcebeb17242861ab1dee05e71ca9e58d0dce9
Anastasiia Shyptia <shyptianastya27@gmail.com>
2022-12-05 13:46:09 +0200

    Spider was centered

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const wallSize = wall.getBoundingClientRect().width;
+const spiderSize = spider.getBoundingClientRect().width;
+const difference = (wallSize / 2) - (spiderSize / 2);
+
+spider.style.left = `${difference}px`;
+spider.style.top = `${difference}px`;
