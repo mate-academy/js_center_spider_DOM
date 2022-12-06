36f7f4e881ef0fa36393623ba666ff9dc1c4450d
anton-sosonskyi <anton.sosonskyi@gmail.com>
2022-12-06 11:42:00 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
 // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const wallHeight = wall.clientHeight;
+const wallWidth = wall.clientWidth;
+
+spider.style.marginTop = `${wallHeight / 2 - (spider.clientHeight / 2)}px`;
+spider.style.marginLeft = `${wallWidth / 2 - spider.clientWidth / 2}px`;
