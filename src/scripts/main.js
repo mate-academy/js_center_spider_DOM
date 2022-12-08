6590c7cdb375f8fd6a00fc7dc1621aee84ad89c2
dmitriy-kovalenko <kovalenkodmitrij@gmail.com>
2022-12-08 14:35:53 +0200

    ok

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+const wallElement = document.body.firstElementChild;
+const spiderElement = wallElement.firstElementChild;
+
+const widthWall = wallElement.clientWidth;
+const heightWall = wallElement.clientHeight;
+const widthSpider = spiderElement.clientHeight;
+const heightSpider = spiderElement.clientHeight;
+
+spiderElement.style.top = `${heightWall / 2}` - `${heightSpider / 2}` + 'px';
+spiderElement.style.left = `${widthWall / 2}` - `${widthSpider / 2}` + 'px';
