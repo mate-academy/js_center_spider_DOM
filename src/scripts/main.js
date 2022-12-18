d44a9ecaeb2ffd924f87d7ad189e268cae844c35
Serhii Vovchenko <seriivigas@gmail.com>
2022-12-18 12:13:38 +0200

    Error correction

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,7 +4,7 @@ const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
 spider.style.left = (wall.clientWidth / 2
-- spider.offsetWidth / 2) + 'px';
+  - spider.offsetWidth / 2) + 'px';
 
 spider.style.top = (wall.clientHeight / 2
-- spider.offsetHeight / 2) + 'px';
+  - spider.offsetHeight / 2) + 'px';
