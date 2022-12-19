58fa3f26500deb608f7ee4ab01d5c45a20ec160b
Deniskakaka <krollik1000000@gmail.com>
2022-12-19 21:10:04 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+
+const width = spider.offsetParent.offsetWidth;
+const height = spider.offsetParent.offsetHeight;
+
+spider.style.top = `${height / 2 - (spider.offsetHeight / 2)}px`;
+spider.style.left = `${width / 2 - (spider.offsetWidth / 2)}px`;
