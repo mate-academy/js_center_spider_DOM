071e36773355dc306dc4c2419ae6547e146357d8
Deniskakaka <krollik1000000@gmail.com>
2022-12-20 20:04:59 +0200

    fix spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,8 @@ const spider = document.querySelector('.spider');
 
 const widthWall = spider.offsetParent.clientWidth;
 const heightWall = spider.offsetParent.clientHeight;
+const heightSpider = spider.offsetHeight;
+const widthSpider = spider.offsetWidth;
 
-spider.style.top = `${heightWall / 2 - 25}px`;
-spider.style.left = `${widthWall / 2 - 25}px`;
+spider.style.top = `${heightWall / 2 - heightSpider / 2}px`;
+spider.style.left = `${widthWall / 2 - widthSpider / 2}px`;
