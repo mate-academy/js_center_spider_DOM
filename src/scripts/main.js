1b879da8d99b20af5fd35372c1ae6a1e2c13106a
Denis Bokov <denisbokov1703@gmail.com>
2022-12-22 23:50:46 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+
+const wallStyles = getComputedStyle(wall);
+
+const wallSize = parseFloat(wallStyles.height);
+
+const spyder = document.querySelector('.spider');
+
+const spyderStyles = getComputedStyle(spyder);
+const spyderHeight = parseFloat(spyderStyles.height);
+
+const spyderTop = (wallSize / 2) - spyderHeight / 2;
+const spyderLeft = (wallSize / 2) - spyderHeight / 2;
+
+spyder.style.marginTop = `${spyderTop}px`;
+spyder.style.marginLeft = `${spyderLeft}px`;
