0974fd3b48f6373ece55d55e1b6954548a6648cb
Kukharuk <its.up.to.you.dk@gmail.com>
2022-12-27 21:19:40 +0200

    spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const styles = getComputedStyle(spider);
+const spiderWidth = parseFloat(styles.width);
+
+spider.style.top = '50%';
+spider.style.left = '50%';
+spider.style.marginLeft = `-${spiderWidth / 2}px`;
+spider.style.marginTop = `-${spiderWidth / 2}px`;
