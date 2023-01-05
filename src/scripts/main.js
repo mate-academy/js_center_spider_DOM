8d8352de12c93facd3bdbb9f5b87e764b0a713fd
Iryna Pysanko <malva37@gmail.com>
2023-01-05 08:18:33 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+const widthWall = wall.clientWidth;
+const heightWall = wall.clientHeight;
+const heightSpider = spider.offsetHeight;
+const widthSpider = spider.offsetWidth;
+
+spider.style.left = `${(widthWall - widthSpider) / 2}px`;
+spider.style.top = `${(heightWall - heightSpider) / 2}px`;
+
+// spider.style.top = '50%'; // easier way set spider in the middle of wall
+// spider.style.left = '50%';
+// spider.style.transform = 'translate(-50%, -50%)';
