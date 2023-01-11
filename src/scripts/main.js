399f65c51d95be5d8aeb0c9463480935cd0ad9ca
Burii Oleh <pirate.captainoleg@gmail.com>
2023-01-11 17:22:20 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+spider.style.left = Math.round(wall.clientWidth
+  / 2 - spider.offsetWidth / 2) + 'px';
+
+spider.style.top = Math.round(wall.clientHeight
+  / 2 - spider.offsetHeight / 2) + 'px';
