1dac7f78be6e726b874a5cc7f90817c7f2a0a39c
Kate <novak5683@gmail.com>
2023-02-08 17:30:56 +0100

    result

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const wall = document.getElementsByClassName('wall');
+const spider = document.getElementsByClassName('spider');
+
+const wallStyles = window.getComputedStyle(wall[0]);
+const border = +(wallStyles.borderWidth.slice(0, -2));
+
+spider[0].style.marginTop = `${wall[0].offsetHeight / 2
+- spider[0].offsetHeight / 2 - border}px`;
+
+spider[0].style.marginLeft = `${wall[0].offsetWidth / 2
+- spider[0].offsetWidth / 2 - border}px`;
