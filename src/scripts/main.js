6feba3be7e574945e5460e68b51b10ee08a72f3f
mykolachupyak <chupyak@gmail.com>
2023-02-17 21:26:10 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,7 @@
 'use strict';
 
-// write code here
+const element = document.querySelector('.spider');
+
+element.style.margin = 'auto';
+element.style.bottom = '0px';
+element.style.right = '0px';
