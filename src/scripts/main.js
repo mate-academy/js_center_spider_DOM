6a9f12ba4603be31d968bbc19e0e51505bfd2d82
Nikitin Evgeny <ngenya81@gmail.com>
2023-02-21 20:17:57 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,8 @@
 'use strict';
 
-const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
-const coordTop = (wall.getBoundingClientRect().height / 2)
-- (spider.getBoundingClientRect().height / 2) - 10;
-const coordLeft = (wall.getBoundingClientRect().width / 2)
-- (spider.getBoundingClientRect().width / 2) - 10;
 
 spider.style.boxSizing = 'border-box';
-spider.style.top = `${coordTop}px`;
-spider.style.left = `${coordLeft}px`;
+spider.style.top = `50%`;
+spider.style.left = `50%`;
+spider.style.transform = 'translate(-50%,-50%)';
