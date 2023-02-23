df7b1df0f614a9a959d668bc9f88e1fbd7ff3314
Vlad <bojko.vladislav16@gmail.com>
2023-02-23 10:59:06 +0200

    spider was centered

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const style = spider.style;
+
+style.left = '50%';
+style.top= '50%';
+style.transform = 'translate(-50%, -50%)';
