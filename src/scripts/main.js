28c3a917231bb1d32fec01650cd402120831b175
JulyaPetrovskaya <119932772+JulyaPetrovskaya@users.noreply.github.com>
2023-03-05 23:04:47 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const spiderImg = document.querySelector('.spider');
+
+const spiderWidth = spiderImg.offsetWidth;
+const spiderHeight = spiderImg.offsetHeight;
+
+const field = document.querySelector('.wall');
+
+const fieldWidth = field.offsetWidth;
+const fieldHeight = field.offsetHeight;
+
+const x = (fieldWidth - spiderWidth) / 2;
+const y = (fieldHeight - spiderHeight) / 2;
+
+spiderImg.style.left = x + 'px';
+spiderImg.style.top = y + 'px';
