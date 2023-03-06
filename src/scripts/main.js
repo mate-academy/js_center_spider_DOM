b5f0fe61d7b72cd614d7a40b2705c4245d997d6b
Nazarii Piatnochka <nazar.piatnochka@gmail.com>
2023-03-06 17:48:22 +0200

    Task completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
+
+spider.style.top = `${(wallWidth / 2) - (spiderWidth / 2)}px`;
+spider.style.left = `${(wallHeight / 2) - (spiderHeight / 2)}px`;
