2b253ab2353c5819270b337c55c681510e02637f
Andrew Litkovets <andriylao@gmail.com>
2023-03-07 12:53:56 +0200

    task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+const spiderWidth = spider.width;
+const spiderHeight = spider.height;
+
+spider.style.top = (wallWidth - spiderWidth) / 2 + 'px';
+spider.style.left = (wallHeight - spiderHeight) / 2 + 'px';
