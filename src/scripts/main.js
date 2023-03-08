d0207a436365016d28ff9f37674bf436e9fd2a58
Ilona Romaniuk <ilona09041983@gmail.com>
2023-03-08 13:15:54 +0200

    Position element with javascript

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const spiderWall = document.querySelector('.wall');
+
+spider.style.top = `${(spiderWall.clientHeight - spider.offsetHeight) / 2}px`;
+
+spider.style.left = `${(spiderWall.clientWidth - spider.offsetWidth) / 2}px`;
