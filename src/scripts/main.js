202020decd10f55e78b72d3825684fb341d0c568
Yevhen-Ivanchuk <e.ivanchyk@gmail.com>
2023-03-08 15:04:13 +0200

    Spider 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,5 +3,5 @@
 const spider = document.querySelector('.spider');
 const wall = document.querySelector('.wall');
 
-spider.style.left = wall.clientWidth / 2 - spider.clientWidth / 2 + 'px';
-spider.style.top = wall.clientHeight / 2 - spider.clientHeight / 2 + 'px';
+spider.style.left = `${wall.clientWidth / 2 - spider.clientWidth / 2}px`;
+spider.style.top = `${wall.clientHeight / 2 - spider.clientHeight / 2}px`;
