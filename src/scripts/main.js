08cba43620b3be39c8a6914a88e4ee953998417b
Tolik Petras <toliketras4@gmail.com>
2023-03-08 13:06:00 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,9 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
+const y = (wall.clientWidth - spider.width) / 2;
+const x = (wall.clientHeight - spider.height) / 2;
 
-spider.style.top = '50%';
-spider.style.left = '50%';
-spider.style.transform = 'translate(-50%, -50%)';
+spider.style.top = `${y}px`;
+spider.style.left = `${x}px`;
