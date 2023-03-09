4f23d03907a40cd7236df76d1141e0afd096f05d
Ilona Romaniuk <ilona09041983@gmail.com>
2023-03-09 12:55:20 +0200

    Improved code to position element with javascript

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,8 @@
 const spider = document.querySelector('.spider');
 const spiderWall = document.querySelector('.wall');
 
-spider.style.top = `${(spiderWall.clientHeight - spider.offsetHeight) / 2}px`;
+const positionY = (spiderWall.clientHeight - spider.offsetHeight) / 2;
+const positionX = (spiderWall.clientWidth - spider.offsetWidth) / 2;
 
-spider.style.left = `${(spiderWall.clientWidth - spider.offsetWidth) / 2}px`;
+spider.style.top = `${positionY}px`;
+spider.style.left = `${positionX}px`;
