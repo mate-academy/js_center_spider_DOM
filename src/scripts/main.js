d08c550af1f482eb63f9c82e8a43aadb08d3cdc6
Nazar <nazar378378379379@gmail.com>
2023-03-14 18:06:48 +0200

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+
+const percent = '50%';
+
+spider.style.top = percent;
+spider.style.left = percent;
+spider.style.transform = 'translate(-50%, -50%)';
