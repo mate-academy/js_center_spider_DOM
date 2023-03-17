b065ac048fb9bb0070e54ed4af0c08de76c0a6b7
Max Zelenskyi <rio1391@gmail.com>
2023-03-17 14:47:00 +0100

    Spider on the wall

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+
+const spider = document.querySelector('.spider');
+
+const positionY = (wall.clientHeight - spider.offsetHeight) / 2;
+const positionX = (wall.clientWidth - spider.offsetWidth) / 2;
+
+spider.style.top = `${positionY}px`;
+spider.style.left = `${positionX}px`;
