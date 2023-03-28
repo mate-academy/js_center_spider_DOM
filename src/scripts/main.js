e3715f95d4c37184ce7b9359a0ec03f467385ecb
Maksym Avramenko <nonste@outlook.com>
2023-03-28 10:19:39 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+const difference
+= (wall.clientHeight - spider.clientHeight) / spider.clientHeight;
+
+spider.style.top = `${50 - difference}%`;
+spider.style.left = `${50 - difference}%`;
