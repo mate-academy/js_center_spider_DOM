926f858b669b6f3bfcf728fc76ae3728cf7a7f1c
Maksym Avramenko <nonste@outlook.com>
2023-03-28 11:37:45 +0300

    transform: translate usage

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,7 @@
 'use strict';
 
 const spider = document.querySelector('.spider');
-const wall = document.querySelector('.wall');
-const difference
-= (wall.clientHeight - spider.clientHeight) / spider.clientHeight;
 
-spider.style.top = `${50 - difference}%`;
-spider.style.left = `${50 - difference}%`;
+spider.style.top = '50%';
+spider.style.left = '50%';
+spider.style.transform = 'translate(-50%, -50%)';
