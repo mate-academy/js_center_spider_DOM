78a498e84c63b6212c9250d3b1bb10d6f7b6ed3f
Leonid <leonidbondarchuk6@gmail.com>
2023-03-29 11:15:07 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.style.cssText = `
+  display: flex;
+  align-items: center;
+  justify-content: center;`;
+
+spider.style.position = 'static';
