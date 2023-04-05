30e2a1890599e52f20552659e19323dbf31ffdb9
Anna Peretiatko <tobossanna@gmail.com>
2023-04-05 22:22:40 +0200

    Center spider solution v.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write code here
+
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+const wallHeightCenter
+  = Math.round(wall.getBoundingClientRect().height / 2);
+const wallWidthCenter
+  = Math.round(wall.getBoundingClientRect().width / 2);
+
+const spiderWidthCenter
+  = Math.round(spider.getBoundingClientRect().width / 2);
+const spiderHeightCenter
+  = Math.round(spider.getBoundingClientRect().height / 2);
+
+spider.style.top
+  = (wallHeightCenter - spiderHeightCenter) - (spiderHeightCenter / 2) + 'px';
+
+spider.style.left
+  = (wallWidthCenter - spiderWidthCenter) - (spiderWidthCenter / 2) + 'px';
