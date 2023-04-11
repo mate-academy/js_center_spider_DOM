2346900b3edab859527a1fead48c156c3283d2f5
Pavlo Petrashevskyi <dragonthunderaddsdp@gmail.com>
2023-04-11 22:56:35 +0300

    spider was centered

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+const wallImage = document.querySelector('div.wall');
+
+wallImage.style.display = 'flex';
+wallImage.style.alignItems = 'center';
+wallImage.style.justifyContent = 'center';
+wallImage.children[0].style.position = 'unset';
+
+// document.querySelector('img').style.top = '175px';
+// document.querySelector('img').style.left = '175px';
+// extra short variant;
