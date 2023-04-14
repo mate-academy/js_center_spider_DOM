32cc18e0ef69fe0b9b00349ebf6a049e48675971
Pasha Moroz <pmoroz2004@gmail.com>
2023-04-14 10:18:39 +0300

    add my solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+const field = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+spider.style.position = 'unset';
+field.style.display = 'flex';
+field.style['justify-content'] = 'center';
+field.style['align-items'] = 'center';
