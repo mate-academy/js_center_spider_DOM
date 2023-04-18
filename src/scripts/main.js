083978e8e1222f6d72b8e4613dd95774ff29e4cf
адмін <admin@Mac-Max.local>
2023-04-18 19:20:18 +0300

    add task solution2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,16 @@
 
 const wall = document.querySelector('.wall');
 
-wall.style.display = 'flex';
-wall.style.justifyContent = 'center';
+// wall.style.display = 'flex';
+// wall.style.justifyContent = 'center';
 
 const spider = document.querySelector('.spider');
 
-spider.style.position = 'relative';
-spider.style.alignSelf = 'center';
+// spider.style.position = 'relative';
+// spider.style.alignSelf = 'center';
+
+const leftOffset = (wall.clientWidth - spider.clientWidth) / 2;
+const topOffset = (wall.clientHeight - spider.clientHeight) / 2;
+
+spider.style.left = `${leftOffset}px`;
+spider.style.top = `${topOffset}px`;
