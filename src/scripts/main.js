84d21d9ddbaf959668757969b05c38f8e296e624
DanielSorokowski <sorokowskidaniel0@gmail.com>
2023-04-18 12:45:50 +0200

    More exciting solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,10 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-wall.style.display = 'grid';
-wall.style.placeItems = 'center';
-spider.style.position = 'relative';
+const topPosition = ((wall.offsetHeight - wall.clientTop * 2) / 2)
+- spider.offsetHeight / 2;
+
+const leftPosition = ((wall.offsetWidth - wall.clientLeft * 2) / 2)
+- spider.offsetWidth / 2;
+
+spider.style.transform = `translate(${topPosition}px, ${leftPosition}px)`;
