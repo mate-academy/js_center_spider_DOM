6dc942a8e1f96c16160f0ba5c883bad1d1f73412
Mikołaj <ostermi@wp.pl>
2023-04-20 11:22:37 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+function centerObject(area, object) {
+  const objectWidth = object.width;
+  const objectHeight = object.height;
+  const areaHeight = area.clientHeight;
+  const areaWidth = area.clientWidth;
+
+  object.style.position = 'relative';
+  spider.style.position = 'absolute';
+  spider.style.left = `${(areaWidth - objectWidth) / 2}px`;
+  spider.style.top = `${(areaHeight - objectHeight) / 2}px`;
+}
+
+centerObject(wall, spider);
