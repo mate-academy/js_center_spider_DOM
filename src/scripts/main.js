cda45d252afc8c5e623d33897a7ccd4ca098be99
Vladyslav Fedenko <vadondrummer@gmail.com>
2023-04-22 04:14:30 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.style.position = 'relative';
+spider.style.left = '175px';
+spider.style.top = '175px';
