ae7d52b2f9d45b45afb785e73f608e992312ab44
Alla Gutsul <allagutsul1990@gmail.com>
2023-04-23 15:31:49 +0300

    center spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+spider.style.left = 'auto';
+spider.style.top = `${wall.clientHeight / 2}px`;
+spider.style.right = `${wall.clientWidth / 2}px`;
+spider.style.transform = 'translate(50%, -50%)';
