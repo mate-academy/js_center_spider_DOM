aa461743f4995e68943783ee7f2a0484f9897d1a
Nadiein Oleh <olegnadein1994@gmail.com>
2023-04-24 15:07:35 +0300

    removed unnecessary actions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,5 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-wall.className = 'wall';
-spider.className = 'spider';
-spider.style.top = `${wall.clientWidth / 2 - spider.offsetWidth / 2}px`;
-spider.style.left = `${wall.clientWidth / 2 - spider.offsetHeight / 2}px`;
+spider.style.top = `${(wall.clientWidth / 2) - (spider.offsetWidth / 2)}px`;
+spider.style.left = `${(wall.clientWidth / 2) - (spider.offsetHeight / 2)}px`;
