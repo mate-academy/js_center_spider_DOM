efe36ae91e97af5585a255632e9b841ec4cb0520
Vladyslav Fedenko <vadondrummer@gmail.com>
2023-04-26 00:46:52 +0300

    Fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,11 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
+const wallWidth = parseFloat(getComputedStyle(wall).width);
+const spiderWidth = parseFloat(getComputedStyle(spider).width);
+const wallHeight = parseFloat(getComputedStyle(wall).height);
+const spiderHeight = parseFloat(getComputedStyle(spider).height);
+
 wall.style.position = 'relative';
-spider.style.left = '175px';
-spider.style.top = '175px';
+spider.style.left = `${(wallWidth - spiderWidth) / 2}px`;
+spider.style.top = `${(wallHeight - spiderHeight) / 2}px`;
