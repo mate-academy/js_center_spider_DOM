a7de8d4db43d91254f950504bfbf61b71d892340
Antonio S <soi4.toxa@ukr.net>
2023-05-01 10:42:32 +0300

    Center-the-spider-1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const styleWall = getComputedStyle(wall);
+const styleSpider = getComputedStyle(spider);
+
+const widthWall = parseFloat(styleWall.width);
+const heightWall = parseFloat(styleWall.height);
+const widthSpider = parseFloat(styleSpider.width);
+const heightSpider = parseFloat(styleSpider.height);
+
+spider.style.top = `${(heightWall - heightSpider) / 2}px`;
+spider.style.left = `${(widthWall - widthSpider) / 2}px`;
