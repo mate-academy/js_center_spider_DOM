ffd47815dbd742db30034fbbe499a7207c87929d
Roman Moroz <65686226+morozit@users.noreply.github.com>
2023-05-03 00:33:13 +0300

    add task moroz v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
+const wallWidth = wall.offsetWidth;
+const wallHeight = wall.offsetHeight;
+
+const spiderLeft = (wallWidth / 2) - (spiderWidth / 2) - spider.offsetLeft;
+const spiderTop = (wallHeight / 2) - (spiderHeight / 2) - spider.offsetTop;
+
+spider.style.left = spiderLeft + 'px';
+spider.style.top = spiderTop + 'px';
