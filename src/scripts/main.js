12c3185c8304f1e6c116dc87a56b6113e6239e53
Kyryl Maslov <kyrylmaslov2007@gmail.com>
2023-05-06 23:24:02 +0200

    spider was centered

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write code here
+const field = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+spider.style.cssText = `
+  top: ${(parseFloat(field.clientHeight)
+    - parseFloat(spider.clientHeight)) / 2}px;
+  left: ${(parseFloat(field.clientWidth)
+    - parseFloat(spider.clientWidth)) / 2}px;
+`;
