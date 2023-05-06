854023a6f8bf98e4c4604f51023e80f0042190fc
OlegRak86 <rakolegit@gmail.com>
2023-05-06 19:25:29 +0300

    js_center_spider_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,7 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+spider.style.top = wall.clientHeight / 2 - spider.clientHeight / 2 + 'px';
+spider.style.left = wall.clientWidth / 2 - spider.clientWidth / 2 + 'px';
