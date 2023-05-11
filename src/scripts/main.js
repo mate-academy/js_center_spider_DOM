a1816387660345a7ba5df072442061b409680571
Oleksii Lavka <lavka.alexey37gmail.com>
2023-05-11 13:03:53 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+function centerElement(container, element) {
+  const containerWidth = container.clientWidth;
+  const containerHeight = container.clientHeight;
+
+  const elemWidth = element.offsetWidth;
+  const elemHeight = element.offsetHeight;
+
+  const elemOffsetLeft = (containerWidth / 2) - (elemWidth / 2);
+  const elemOffsetTop = (containerHeight / 2) - (elemHeight / 2);
+
+  element.style.left = `${elemOffsetLeft}px`;
+  element.style.top = `${elemOffsetTop}px`;
+}
+
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+centerElement(wall, spider);
