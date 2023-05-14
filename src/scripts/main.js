2d2036137be601afe33438db252a5624f30eef11
Veronika Pastushkova <d9169393507@gmai.com>
2023-05-14 14:55:21 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,7 @@
 'use strict';
 
-// write code here
+const picture = document.querySelector('.spider');
+const block = document.querySelector('.wall');
+
+picture.style.top = `${block.clientHeight / 2 - picture.clientHeight / 2}px`;
+picture.style.left = `${block.clientWidth / 2 - picture.clientWidth / 2}px`;
