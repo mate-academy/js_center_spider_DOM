9bc6728358b6de5dbc6aac3274e5b7f31e2e94be
DefaultGitUser <usingforgit@gmail.com>
2023-05-16 17:45:43 +0300

    Sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,7 @@
 'use strict';
 
-// write code here
+const spiderEl = document.querySelector('.spider');
+
+spiderEl.style.top = '50%';
+spiderEl.style.left = '50%';
+spiderEl.style.transform = 'translate(-50%,-50%)';
