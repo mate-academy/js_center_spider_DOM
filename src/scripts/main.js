285ee78301ba8c609862f888e28c2fe1033891f6
Izzet Yusufov <17izzet@gmail.com12345Hello>
2023-05-20 21:59:33 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,17 +3,5 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-const styleWall = getComputedStyle(wall);
-const styleSpider = getComputedStyle(spider);
-
-const wallHeight = parseFloat(styleWall.height);
-const spiderHeight = parseFloat(styleSpider.height);
-
-const wallWidth = parseFloat(styleWall.width);
-const spiderWidth = parseFloat(styleSpider.width);
-
-const positionY = `${(wallHeight / 2) - (spiderHeight / 2)}px`;
-const positionX = `${(wallWidth / 2) - (spiderWidth / 2)}px`;
-
-spider.style.top = positionY;
-spider.style.left = positionX;
+spider.style.top = `${(wall.clientHeight - spider.clientHeight) / 2}px`;
+spider.style.left = `${(wall.clientWidth - spider.clientWidth) / 2}px`;
