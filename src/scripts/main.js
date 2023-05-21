3a531cbfdc31f8e07a77ae9570d0aa148f4320b2
Anton <crossnaders4949@gmail.com>
2023-05-21 16:21:26 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const leftMove = (wall.clientWidth - spider.clientWidth) / 2;
+const topMove = (wall.clientHeight - spider.clientHeight) / 2;
+
+spider.style.left = `${leftMove}px`;
+spider.style.top = `${topMove}px`;
