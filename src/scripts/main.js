6548f3edf81685ddd78c4fdf31ac094677f8505f
pasternakv19@gmail.com <pasternakv19@gmail.com>
2023-05-22 17:40:11 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,13 @@
 'use strict';
 
 const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
 
-spider.style.top = 'calc(50% - 25px)';
-spider.style.left = 'calc(50% - 25px)';
+const widthWall = wall.clientWidth;
+const heightWall = wall.clientHeight;
+
+const widthSpider = spider.clientWidth;
+const heightSpider = spider.clientWidth;
+
+spider.style.cssText = `top: ${widthWall / 2 - heightSpider / 2}px;
+ left: ${heightWall / 2 - widthSpider / 2}px`;
