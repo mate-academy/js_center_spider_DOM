8dd628f0a514c8dc5438755553af6e947b3b790a
Vasyl Zhyliakov <snak871@gmail.com>
2023-05-22 20:16:27 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const parentElement = spider.offsetParent;
+const width = (parentElement.clientWidth / 2)
+  - (spider.getBoundingClientRect().width / 2);
+const height = (parentElement.clientHeight / 2)
+  - (spider.getBoundingClientRect().height / 2);
+
+spider.style.cssText = `position: absolute; top: ${width}px; left: ${height}px`;
