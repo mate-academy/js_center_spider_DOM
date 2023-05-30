740e48d97473cc39a30918d0b05fbb29f0a6d390
Roman <rdyakovich@gmail.com>
2023-05-30 13:53:52 +0300

    spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,45 @@
 'use strict';
 
-const wall = document.querySelector('.wall');
-const spider = document.querySelector('.spider');
+document.addEventListener('click', even => {
+  if (even.target.className !== 'wall') {
+    return;
+  }
 
-spider.style.left = 'auto';
-spider.style.top = `${wall.clientHeight / 2}px`;
-spider.style.right = `${wall.clientWidth / 2}px`;
-spider.style.transform = 'translate(50%, -50%)';
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  const maxX = wall.clientWidth - spider.clientWidth;
+  const maxY = wall.clientHeight - spider.clientHeight;
+
+  let spiderX = even.offsetX - (spider.clientWidth / 2);
+  let spiderY = even.offsetY - (spider.clientHeight / 2);
+
+  if (spiderX > maxX) {
+    spiderX = maxX;
+  }
+
+  if (spiderY > maxY) {
+    spiderY = maxY;
+  }
+
+  if (spiderX < 0) {
+    spiderX = 0;
+  }
+
+  if (spiderY < 0) {
+    spiderY = 0;
+  }
+  spider.style.left = `${spiderX}px`;
+  spider.style.top = `${spiderY}px`;
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  const centerX = (wall.clientWidth - spider.clientWidth) / 2;
+  const centerY = (wall.clientHeight - spider.clientHeight) / 2;
+
+  spider.style.left = `${centerX}px`;
+  spider.style.top = `${centerY}px`;
+});
