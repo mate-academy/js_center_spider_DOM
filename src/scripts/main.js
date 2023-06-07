6be0a63a6576762ccba552ec98d7f0b0ea035b8b
domkab <dominykas.bobinas@gmail.com>
2023-06-07 22:30:18 +0300

    1.0.0 initial

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
 // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('spider');
+
+//  wall needs display flex, justify-content, align-items
+//  spider, reset position property.
