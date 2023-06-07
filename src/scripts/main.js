3f8347e28db827f7361ab023b609f1c755f0d7e8
Hanna Balabukha <anna.aniskovets1992@ukr.net>
2023-06-07 17:20:18 +0300

    'done'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,8 @@
 'use strict';
-
-const wallElement = document.querySelector('.wall');
-
-wallElement.style.position = 'relative';
-
 const spiderElement = document.querySelector('.spider');
 
 spiderElement.style.position = 'absolute';
-spiderElement.style.top = '45%';
-spiderElement.style.left = '45%';
+spiderElement.style.top = '50%';
+spiderElement.style.left = '50%';
 
-spiderElement.style.transform = 'translate(-50%; -50%)';
+spiderElement.style.transform = 'translate(-50%, -50%)';
