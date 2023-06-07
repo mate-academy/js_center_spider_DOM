1c1904185535641b3e6daff4f8cc8cf648c51731
Hanna Balabukha <anna.aniskovets1992@ukr.net>
2023-06-07 13:29:15 +0300

    'fixed'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,5 @@
 'use strict';
 
-const wallElement = document.querySelector('.wall');
-const wallStyle = getComputedStyle(wallElement);
-const wallwidth = parseFloat(wallStyle.width) / 2;
-const wallheight = parseFloat(wallStyle.height) / 2;
-
 const spiderElement = document.querySelector('.spider');
-const spiderStyle = getComputedStyle(spiderElement);
-const spiderWidth = parseFloat(spiderStyle.width) / 2;
-const spiderheight = parseFloat(spiderStyle.height) / 2;
 
-wallElement.style.position = 'relative';
-spiderElement.style.position = 'absolute';
-
-spiderElement.style.top = `${wallheight - spiderheight}px`;
-spiderElement.style.left = `${wallwidth - spiderWidth}px`;
+spiderElement.style.transform = 'translate(175px, 175px)';
