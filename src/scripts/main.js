8e4d92971ea73af30617ad45efb4d84f3fea5e4f
Rufat Mammadov <rufat.mammadov.m@gmail.com>
2023-07-19 22:53:26 +0300

    center_spider_DOM first try

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write code here
+const fieldWidth = document.querySelector('.wall').clientWidth;
+const fieldHeight = document.querySelector('.wall').clientHeight;
+const spiderWidth = document.querySelector('.spider').clientWidth;
+const spiderHeight = document.querySelector('.spider').clientHeight;
+
+const spider = document.querySelector('.spider');
+
+spider.style.left = (fieldWidth / 2 - spiderWidth / 2) + 'px';
+spider.style.top = (fieldHeight / 2 - spiderHeight / 2) + 'px';
