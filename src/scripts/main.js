54c0bdbfafd00a9d67e84ac9227cc9d1c829b552
Rufat Mammadov <rufat.mammadov.m@gmail.com>
2023-07-19 22:59:29 +0300

    the code was changed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,12 @@
 'use strict';
 
-const fieldWidth = document.querySelector('.wall').clientWidth;
-const fieldHeight = document.querySelector('.wall').clientHeight;
-const spiderWidth = document.querySelector('.spider').clientWidth;
-const spiderHeight = document.querySelector('.spider').clientHeight;
+const field = document.querySelector('.field');
+const fieldWidth = field.clientWidth;
+const fieldHeight = field.clientHeight;
 
 const spider = document.querySelector('.spider');
+const spiderWidth = spider.clientWidth;
+const spiderHeight = spider.clientHeight;
 
 spider.style.left = (fieldWidth / 2 - spiderWidth / 2) + 'px';
 spider.style.top = (fieldHeight / 2 - spiderHeight / 2) + 'px';
