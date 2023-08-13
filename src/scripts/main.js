24f44ccd8f920b6a8cded31c451be24792458204
GGsmou <svyatoslav.golovin.2005@gmail.com>
2023-08-13 20:52:10 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,9 +5,10 @@ const spider = document.querySelector('.spider');
 
 const wallWidth = wall.offsetWidth;
 const wallHeight = wall.offsetHeight;
+const wallBorder = parseInt(getComputedStyle(wall).borderWidth, 10);
 
 const spiderWidth = spider.offsetWidth;
 const spiderHeight = spider.offsetHeight;
 
-spider.style.left = `${wallWidth / 2 - spiderWidth / 2}px`;
-spider.style.top = `${wallHeight / 2 - spiderHeight / 2}px`;
+spider.style.left = `${wallWidth / 2 - spiderWidth / 2 - wallBorder}px`;
+spider.style.top = `${wallHeight / 2 - spiderHeight / 2 - wallBorder}px`;
