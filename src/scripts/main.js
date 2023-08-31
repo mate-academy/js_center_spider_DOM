e170ccc9f976c475b60587b0e6101d45bda35125
Dmytro Lazarenko <lazarenkodmytro@gmail.com>
2023-08-31 23:22:13 +0200

    js_center_spider_DOM_solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const centerX = (wall.clientWidth - spider.clientWidth) / 2;
+const centerY = (wall.clientHeight - spider.clientHeight) / 2;
+
+spider.style.left = centerX + 'px';
+spider.style.top = centerY + 'px';
