3c601b39450692acfc75129d955a5860bae5a443
Volodymyr Zadorozhnyi <Torsida@gmail.com>
2023-09-10 18:29:27 +0300

    first version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+function centerSpider(first, second) {
+  const positionX
+    = (parseFloat(getComputedStyle(first).width)
+    - parseFloat(getComputedStyle(second).width)) / 2;
+  const positionY
+    = (parseFloat(getComputedStyle(first).height)
+    - parseFloat(getComputedStyle(second).height)) / 2;
+
+  second.style.left = `${positionX}px`;
+  second.style.top = `${positionY}px`;
+};
+centerSpider(wall, spider);
