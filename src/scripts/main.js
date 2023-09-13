90052e1045f0a994850d7107fb9f3cf4787ef1be
Eugene Kulish <genekulish13@gmail.com>
2023-09-13 20:03:53 +0300

    solve the task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+function center(edge, parentElement, childElement) {
+  switch (edge) {
+    case 'top':
+      return `${(
+        parentElement.clientHeight - childElement.clientHeight
+      ) / 2}px`;
+
+    case 'left':
+      return `${(parentElement.clientWidth - childElement.clientWidth) / 2}px`;
+
+    default:
+      return '0px';
+  }
+}
+
+spider.style.top = center('top', wall, spider);
+spider.style.left = center('left', wall, spider);
