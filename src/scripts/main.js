5718d3590dccc4bab7761571ab5022c61da99639
Liliya Kalinichenko <fedotova.liliya90@gmail.com>
2023-11-15 16:14:22 +0200

    task solution was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+
+const spiderHeight = spider.offsetHeight;
+const spiderWidth = spider.offsetWidth;
+
+spider.style.top = `calc(50% - ${spiderHeight / 2}px)`;
+spider.style.left = `calc(50% - ${spiderWidth / 2}px)`;
