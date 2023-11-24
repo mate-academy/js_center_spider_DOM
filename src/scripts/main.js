0ae9e2e97455a7b5e89d705065ebf1a55a273cda
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2023-11-24 22:59:47 +0100

    add task soulution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const spiderX = (wall.scrollWidth - spider.width) / 2;
+const spiderY = (wall.scrollHeight - spider.height) / 2;
+
+spider.style.marginTop = spiderY + 'px';
+spider.style.marginLeft = spiderX + 'px';
