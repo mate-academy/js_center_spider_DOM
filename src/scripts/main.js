e509573ec6df2d22786b3628091e80ac49246cee
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2023-12-19 23:39:20 +0200

    Revert "Spider was centered"
    This reverts commit 1f12b269aeb4a2b10eca5a494b71212da6b7ae61.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,3 @@
 'use strict';
 
-const wall = document.querySelector('.wall');
-const spider = document.querySelector('.spider');
-
-spider.style.top = `${wall.clientHeight / 2 - spider.height / 2}px`;
-spider.style.left = `${wall.clientWidth / 2 - spider.width / 2}px`;
+// write code here
