caacc4b40ca274105103a27ef16d0bd38975f9a5
Dmytro Fefelov <reseneweb@gmail.com>
2024-01-06 17:58:50 +0200

    Develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const wallWidth = parseFloat(getComputedStyle(wall).width);
+const wallHeight = parseFloat(getComputedStyle(wall).height);
+const spiderWidth = parseFloat(getComputedStyle(spider).width);
+const spiderHeight = parseFloat(getComputedStyle(spider).height);
+
+spider.style.top = `${(wallWidth - spiderWidth) / 2}px`;
+spider.style.left = `${(wallHeight - spiderHeight) / 2}px`;
