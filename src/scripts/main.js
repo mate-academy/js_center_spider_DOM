72ff7f0284d2df3c068bf9065432954150610e1e
Vovchuk <kulivackijvoloda@gmail.com>
2024-01-07 08:07:03 +0200

    add

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const field = document.querySelector('.wall');
+
+const spider = document.querySelector('.spider');
+
+const fieldRect = field.getBoundingClientRect();
+const spiderRect = spider.getBoundingClientRect();
+
+const newTop = (fieldRect.height - spiderRect.height) / 2;
+const newLeft = (fieldRect.width - spiderRect.width) / 2;
+
+spider.style.position = 'absolute';
+spider.style.top = newTop + 'px';
+spider.style.left = newLeft + 'px';
