879f4a116654745a1ab33597f20b6affb8f8f1df
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2024-01-17 10:21:29 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const wall = document.getElementsByClassName('wall')[0];
+const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-const wallWidth = parseInt(window.getComputedStyle(wall).width, 10);
-const spiderWidth = parseInt(window.getComputedStyle(spider).width, 10);
+const wallWidth = wall.clientWidth;
+const spiderWidth = spider.clientWidth;
 
 spider.style.left = `${(wallWidth / 2) - (spiderWidth / 2)}px`;
 spider.style.top = `${(wallWidth / 2) - (spiderWidth / 2)}px`;
