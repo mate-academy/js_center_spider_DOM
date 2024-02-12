25a243e6167af7f1430a1d6b0dc53ae7bca68e0c
dima.rogkov <dima.rogkov@cheitgroup.com>
2024-02-12 14:14:08 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
+/* eslint-disable padding-line-between-statements */
+/* eslint-disable operator-linebreak */
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const centeredSpider = () => {
+    const spider = document.querySelector('.spider');
+    if (spider) {
+      const spiderStyles =
+        'top: 50%;left: 50%;transform: translate(-50%, -50%);';
+
+      spider.style.cssText = spiderStyles;
+    }
+  };
+
+  centeredSpider();
+});
