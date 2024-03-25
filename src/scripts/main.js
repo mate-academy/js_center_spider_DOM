3a99669d755f729da21dfce637200190a8f4e3e5
Heorhii Churhuliia <gio4929189@gmail.com>
2024-03-25 20:48:29 +0000

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
-// write code here
+const WALL = document.querySelector('.wall');
+const SPIDER = document.querySelector('.spider');
+
+WALL.style.position = 'relative';
+SPIDER.style.top = '170px';
+SPIDER.style.left = '170px';
