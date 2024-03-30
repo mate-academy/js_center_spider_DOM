a925b13ef8f1d21e91410e0045ce2f4cd7be7ab4
Yehor <eee.houmich@gmail.com>
2024-03-30 17:43:22 +0200

    Complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write code here
+const wallElement = document.querySelector('.wall');
+
+wallElement.style.display = 'flex';
+wallElement.style.justifyContent = 'center';
+wallElement.style.alignItems = 'center';
+
+const spiderElement = document.querySelector('.spider');
+
+spiderElement.style.position = 'relative';
