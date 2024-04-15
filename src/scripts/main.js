9c79453db9866d08f1f93c09c30aaa98d9af801a
Yurii Shkrobut <georgebest2409@gmail.com>
2024-04-15 17:10:11 +0300

    'Done'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
+/* eslint-disable no-useless-computed-key */
 'use strict';
 
 // write code here
+
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.style.display = 'flex';
+wall.style.justifyContent = 'center';
+wall.style.alignItems = 'center';
+
+spider.style.position = 'relative';
