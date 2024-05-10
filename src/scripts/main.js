f481d840a3d6be214fd48c6c2949422e4e7d0db1
Kostya Niekrasov <kostyanekrasov1501@gmail.com>
2024-05-10 10:47:08 +0300

    updated code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,32 +1,10 @@
 'use strict';
 
-const wallElement = document.querySelector('.wall');
-const spiderElement = document.querySelector('.spider');
+const spiderContainer = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
 
-const centerSpider = () => {
-  const wallRect = wallElement.getBoundingClientRect();
-  const spiderRect = spiderElement.getBoundingClientRect();
+const leftPosition = (spiderContainer.clientHeight - spider.clientHeight) / 2;
+const topPosition = (spiderContainer.clientWidth - spider.clientWidth) / 2;
 
-  const wallStyles = window.getComputedStyle(wallElement);
-
-  const wallBorderWidth =
-    parseFloat(wallStyles.borderLeftWidth) +
-    parseFloat(wallStyles.borderRightWidth);
-  const wallBorderHeight =
-    parseFloat(wallStyles.borderTopWidth) +
-    parseFloat(wallStyles.borderBottomWidth);
-
-  const wallHeight = wallRect.height;
-  const wallWidth = wallRect.width;
-
-  const spiderHeight = spiderRect.height;
-  const spiderWidth = spiderRect.width;
-
-  const spiderTop = (wallHeight - wallBorderHeight) / 2 - spiderHeight / 2;
-  const spiderLeft = (wallWidth - wallBorderWidth) / 2 - spiderWidth / 2;
-
-  spiderElement.style.top = `${spiderTop}px`;
-  spiderElement.style.left = `${spiderLeft}px`;
-};
-
-centerSpider();
+spider.style.left = `${leftPosition}px`;
+spider.style.top = `${topPosition}px`;
