0128fb93182328400189962606b93e7a320bea61
Kostya Niekrasov <kostyanekrasov1501@gmail.com>
2024-05-10 10:23:49 +0300

    center spider DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const wallElement = document.querySelector('.wall');
+const spiderElement = document.querySelector('.spider');
+
+const centerSpider = () => {
+  const wallRect = wallElement.getBoundingClientRect();
+  const spiderRect = spiderElement.getBoundingClientRect();
+
+  const wallStyles = window.getComputedStyle(wallElement);
+
+  const wallBorderWidth =
+    parseFloat(wallStyles.borderLeftWidth) +
+    parseFloat(wallStyles.borderRightWidth);
+  const wallBorderHeight =
+    parseFloat(wallStyles.borderTopWidth) +
+    parseFloat(wallStyles.borderBottomWidth);
+
+  const wallHeight = wallRect.height;
+  const wallWidth = wallRect.width;
+
+  const spiderHeight = spiderRect.height;
+  const spiderWidth = spiderRect.width;
+
+  const spiderTop = (wallHeight - wallBorderHeight) / 2 - spiderHeight / 2;
+  const spiderLeft = (wallWidth - wallBorderWidth) / 2 - spiderWidth / 2;
+
+  spiderElement.style.top = `${spiderTop}px`;
+  spiderElement.style.left = `${spiderLeft}px`;
+};
+
+centerSpider();
