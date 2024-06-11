e34e6565d20967fc65b3b0bfdda4a7770256ae4f
Anton Polishchuk <hardixxxxx@gmail.com>
2024-06-11 19:39:11 +0200

    centered spider using DOM elements properties+

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,5 +6,12 @@ const spider = field.querySelector('.spider');
 const spiderPosTop = (field.clientWidth - spider.clientWidth) / 2 + 'px';
 const spiderPosLeft = (field.clientHeight - spider.clientHeight) / 2 + 'px';
 
-spider.style.top = spiderPosTop;
-spider.style.left = spiderPosLeft;
+setInterval(() => {
+  spider.style.top = spiderPosTop;
+  spider.style.left = spiderPosLeft;
+
+  setTimeout(() => {
+    spider.style.top = 0 + 'px';
+    spider.style.left = 0 + 'px';
+  }, 1000);
+}, 2000);
