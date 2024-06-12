d1fc54ba3f13515dbbb1ed6f24811283e7640257
Oleksii Bidiak <oleksii.bidiak@gmail.com>
2024-06-12 14:35:31 +0300

    make build

---
+++ dist/index.33b9fbc8.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.clientHeight/2-t.clientHeight/2,l=e.clientWidth/2-t.clientWidth/2;t.style.top="".concat(c,"px"),t.style.left="".concat(l,"px");
+//# sourceMappingURL=index.33b9fbc8.js.map
