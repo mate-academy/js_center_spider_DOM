0883e8d05dcf3b3a580acf286c907b0f32211f45
Anton Polishchuk <hardixxxxx@gmail.com>
2024-06-12 16:36:03 +0200

    solutions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,12 +6,5 @@ const spider = field.querySelector('.spider');
 const spiderPosTop = (field.clientWidth - spider.clientWidth) / 2 + 'px';
 const spiderPosLeft = (field.clientHeight - spider.clientHeight) / 2 + 'px';
 
-setInterval(() => {
-  spider.style.top = spiderPosTop;
-  spider.style.left = spiderPosLeft;
-
-  setTimeout(() => {
-    spider.style.top = 0 + 'px';
-    spider.style.left = 0 + 'px';
-  }, 1000);
-}, 2000);
+spider.style.top = spiderPosTop;
+spider.style.left = spiderPosLeft;
