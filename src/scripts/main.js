398bf5a93abf3348914a348fd12fc4b4209c07c8
mikezhylka <115790814+mikezhylka@users.noreply.github.com>
2024-06-13 14:22:44 +0200

    center spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+// get img and div elements by their class
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+// set styles to center a spider
+wall.style.position = 'relative';
+spider.style.position = 'absolute';
+spider.style.top = '50%';
+spider.style.left = '50%';
+spider.style.transform = 'translate(-50%, -50%)';
