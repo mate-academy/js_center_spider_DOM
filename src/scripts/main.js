caf8a51636e550e4104dc4e7a3d70dffe1e0c3f9
Arthur Tolkachov <babanjerelo@gmail.com>
2024-06-18 11:24:37 +0300

    [Feature] centered spider on the wall

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+
+const { width, height } = spider.getBoundingClientRect();
+
+spider.style.cssText = `
+  left: 50%;
+  top: 50%;
+  margin-left: -${width / 2}px;
+  margin-top: -${height / 2}px;
+`;
