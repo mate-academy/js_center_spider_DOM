860af4710d6b24a4251e134cba954bee543ac7f2
Victor Skits <vityok.nice@gmail.com>
2024-06-19 12:39:22 +0300

    solve task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const width = wall.clientWidth;
+const height = wall.clientHeight;
+
+const spiderWidth = spider.clientWidth;
+const spiderHeight = spider.clientHeight;
+
+const spiderTop = (height - spiderHeight) / 2;
+const spiderLeft = (width - spiderWidth) / 2;
+
+spider.style.top = `${spiderTop}px`;
+spider.style.left = `${spiderLeft}px`;
