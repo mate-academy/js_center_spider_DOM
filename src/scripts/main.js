e18a913a6d70e9d0e74402aa847cc64087e2df23
Nazarii <Kyzenko.nr@gmail.com>
2024-06-20 12:21:40 +0300

    DevelopTask

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
 // write code here
+const spider = document.querySelector('.spider');
+
+spider.style.cssText = `
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
