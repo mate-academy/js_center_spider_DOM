5c337118ae961a55be40a1efa8a8d83cc50ebb8e
MelnykVlad <21vladmel15@gmail.com>
2024-06-29 15:20:01 +0300

    add center spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+spider.style.position = 'relative';
+wall.style.display = 'flex';
+wall.style.alignItems = 'center';
+wall.style.justifyContent = 'center';
