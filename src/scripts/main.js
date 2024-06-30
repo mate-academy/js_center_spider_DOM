162b584c3ea13647ecccfd78713bcdcf7b45ffff
luter <newgglutt@gmail.com>
2024-06-30 16:49:21 +0300

    new111

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,19 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   const wall = document.querySelector('.wall');
+
   const spider = document.querySelector('.spider');
 
-  const wallRect = wall.getBoundingClientRect();
-  const spiderRect = spider.getBoundingClientRect();
+  wall.style.position = 'relative';
 
-  const spiderTop = (wallRect.height - spiderRect.height) / 2;
-  const spiderLeft = (wallRect.width - spiderRect.width) / 2;
+  const wallDef = wall.getBoundingClientRect();
+  const spiderDef = spider.getBoundingClientRect();
 
+  const spiderTop = (wallDef.height - spiderDef.height) / 2;
+
+  const spiderLeft = (wallDef.width - spiderDef.width) / 2;
+
+  spider.style.position = 'absolute';
   spider.style.top = `${spiderTop}px`;
   spider.style.left = `${spiderLeft}px`;
 });
