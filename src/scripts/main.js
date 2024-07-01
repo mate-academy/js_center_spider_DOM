05617442596c905c1f1e99a8dd0523f2ef77efe8
Anastasiia <vasilisandreeva00@gmail.com>
2024-07-01 15:51:29 +0300

    change spider position

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const wallWidth = convertToNumber(getComputedStyle(wall).width);
+const wallHeight = convertToNumber(getComputedStyle(wall).height);
+
+const spiderWidth = convertToNumber(getComputedStyle(spider).width);
+const spiderHeight = convertToNumber(getComputedStyle(spider).height);
+
+const newSpiderLeft = wallWidth / 2 - spiderWidth / 2;
+const newSpiderTop = wallHeight / 2 - spiderHeight / 2;
+
+spider.style.left = newSpiderLeft + 'px';
+
+spider.style.top = newSpiderTop + 'px';
+
+function convertToNumber(value) {
+  return parseFloat(value);
+}
