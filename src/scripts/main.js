46f4ac9ede2e5b969bea39692aae2b78711075ee
smachyloooo <41301896+smachylooo@users.noreply.github.com>
2024-07-02 17:16:13 +0200

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,7 +13,7 @@ const wallHeight = wall.clientHeight;
 const wallWidth = wall.clientWidth; 
 
 const spiderHeight = spider.clientHeight; 
-const spiderWidth = spider.clientHeight; 
+const spiderWidth = spider.clientWidth; 
 
 spider.style.top = `${(wallHeight / 2) - (spiderHeight / 2)}px`;
 spider.style.left = `${(wallWidth / 2) - (spiderHeight / 2)}px`;
