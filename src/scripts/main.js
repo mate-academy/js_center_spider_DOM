10dbd563895f83b6f2b099a2b70764102962f895
MTS13 <mtstaras@gmail.com>
2024-07-04 20:02:16 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const half = wall.clientWidth / 2 + 'px';
+
+spider.style.left = half;
+spider.style.top = half;
+spider.style.transform = 'translate(-50%, -50%)';
