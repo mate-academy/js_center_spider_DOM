cf31098aee6f6e1db26614ec2270b0e7263f5ba6
Artem Zakharchuk <artemzakharchuk@gmail.com>
2024-07-04 15:10:17 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,5 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+
+spider.style.cssText = `position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%)`;
