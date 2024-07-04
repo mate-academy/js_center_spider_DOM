79966ad136f2d9629225c21fcd6cc9e2772d8ae8
Galers <galosaandrew@gmail.com>
2024-07-04 21:58:49 +0300

    Solution

--- cypress/integration/centerSpider.spec.js
+++ cypress/integration/centerSpider.spec.js
@@ -9,8 +9,10 @@ describe('Spider application', () => {
     cy.get('.wall').then(($wall) => {
       cy.get('.spider').should(($spider) => {
         const spiderLeft = $spider.offset().left;
-        const expectedLeft = ($wall.innerWidth() / 2)
-          + ($wall.offset().left + 10) - ($spider.width() / 2);
+        const expectedLeft =
+          $wall.innerWidth() / 2 +
+          ($wall.offset().left + 10) -
+          $spider.width() / 2;
 
         expect(spiderLeft).to.equal(expectedLeft);
       });
@@ -21,8 +23,10 @@ describe('Spider application', () => {
     cy.get('.wall').then(($wall) => {
       cy.get('.spider').should(($spider) => {
         const spiderTop = $spider.offset().top;
-        const expectedTopAbs = ($wall.innerHeight() / 2)
-          + ($wall.offset().top + 10) - ($spider.height() / 2);
+        const expectedTopAbs =
+          $wall.innerHeight() / 2 +
+          ($wall.offset().top + 10) -
+          $spider.height() / 2;
 
         expect(spiderTop).to.equal(expectedTopAbs);
       });
