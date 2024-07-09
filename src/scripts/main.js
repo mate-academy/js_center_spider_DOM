6701fa3bcd9040c6ca56ae2d0da59cfc28899dd3
unknown <tenetaevgen@gmail.com>
2024-07-09 15:31:43 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const wallCoords = wall.getBoundingClientRect();
+const spider = document.querySelector('.spider');
+
+spider.style.left = `${wallCoords.width / 2}px`;
+spider.style.top = `${wallCoords.height / 2}px`;
+spider.style.transform = 'translate(-50%, -50%)';
