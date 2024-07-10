acbdbc2d59e61dfd59b302e89671d925a15cd24c
Oleksandr Sushko <alexandrsushko25@gmail.com>
2024-07-10 16:39:33 +0200

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const elementSpider = document.querySelector('.spider');
+const elementWall = document.querySelector('.wall');
+
+elementSpider.style.position = 'absolute';
+
+elementSpider.style.top = '50%';
+elementSpider.style.left = '50%';
+elementSpider.style.transform = 'translate(-50%, -50%)';
