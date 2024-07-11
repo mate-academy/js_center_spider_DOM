06cd648a8d4c18a367764d7bbf14e37a9b69dcfc
Mykhal <misha.soroka.49@gmail.com>
2024-07-11 16:50:40 +0200

    This should work

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function () {
   const wall = document.querySelector('.wall');
   const spider = document.querySelector('.spider');
 
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const spiderWidth = spiderRect.width;
     const spiderHeight = spiderRect.height;
 
-    const spiderLeft = (wallWidth - spiderWidth) / 2;
+    const spiderLeft = (wallWidth - spiderWidth) / 2 - 10;
     const spiderTop = (wallHeight - spiderHeight) / 2 - 10;
 
     spider.style.position = 'absolute';
