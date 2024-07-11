39551b8742d7c9029bb9fcda21c1a9ada4f554c7
Viktoriia Melnyk <melnychka1993@gmail.com>
2024-07-11 16:48:28 +0100

    centered

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+function convertPxToNumber(pxString) {
+  const numberString = pxString.replace('px', '');
+
+  const number = parseInt(numberString, 10);
+
+  return number;
+}
+
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const wallWidth = convertPxToNumber(getComputedStyle(wall).width);
+const spiderWith = convertPxToNumber(getComputedStyle(spider).width);
+const wallHeight = convertPxToNumber(getComputedStyle(wall).height);
+const spiderHeight = convertPxToNumber(getComputedStyle(spider).height);
+
+spider.style.top = `${wallHeight / 2 - spiderHeight / 2}px`;
+spider.style.left = `${wallWidth / 2 - spiderWith / 2}px`;
