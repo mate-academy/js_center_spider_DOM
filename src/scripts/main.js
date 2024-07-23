fbf58b5d061e5a1e45c3eea063f719740955c140
mrsvolodya <vovamursky@gmail.com>
2024-07-23 15:11:10 +0300

    center-spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
+const div = document.querySelector('div');
+const img = document.querySelector('img');
+const wallCenter = div.clientWidth / 2;
+const spiderCenter = img.offsetWidth / 2;
+const center = wallCenter - spiderCenter;
+img.style.top = `${center}px`;
+img.style.left = `${center}px`;
 
-// write code here
+console.log(spiderCenter);
