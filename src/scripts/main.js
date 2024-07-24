0622484adce3a3731a08edeb8252903d68f06ab7
Katya <evil.666.buny@gmail.com>
2024-07-24 11:44:29 +0300

    script added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+function centerTheSpider() {
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  const wallSize = {
+    width: wall.clientWidth,
+    height: wall.clientHeight,
+  };
+
+  const spiderSize = {
+    width: spider.clientWidth,
+    height: spider.clientHeight,
+  };
+
+  spider.style.position = 'relative';
+  spider.style.left = `${wallSize.width / 2 - spiderSize.width / 2}px`;
+  spider.style.top = `${wallSize.height / 2 - spiderSize.height / 2}px`;
+}
+
+centerTheSpider();
