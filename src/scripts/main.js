31838520b8f4cb29cde96db27d376bf4de567931
Misha Remeslo <misha.remeslol@gmail.com>
2024-07-24 22:01:09 +0300

    Spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+// eslint-disable-next-line no-unused-vars
+const wall = document.querySelector('div');
+const spider = document.querySelector('img');
+
+spider.style.top = '50%';
+spider.style.left = '50%';
+spider.style.transform = 'translate(-50%, -50%)';
