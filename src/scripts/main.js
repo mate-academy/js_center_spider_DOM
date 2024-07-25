01de86e85cf7259352b2666f0e3aa49c9f253fb4
svyatoslav <svyatoslavik9@gmail.com>
2024-07-25 13:49:40 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 // write code here
+const field = document.querySelector('.wall');
+const character = document.querySelector('.spider');
+
+const setCharacterByCenter = () => {
+  const fieldWidth = field.clientWidth / 2;
+  const characterWidth = character.clientWidth / 2;
+  const characterCenterPosition = fieldWidth - characterWidth;
+
+  character.style.left = `${characterCenterPosition}px`;
+  character.style.top = `${characterCenterPosition}px`;
+};
+
+setCharacterByCenter();
