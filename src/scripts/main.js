48c36fce7c9428cc6ac5d411fc83ce230d289b8f
Vitalii Venher <vengervn@gmail.com>
2024-07-26 22:09:52 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const field = document.querySelector('.wall');
+const img = document.querySelector('.spider');
+const topPosition = field.clientHeight / 2;
+const leftPositon = field.clientWidth / 2;
+
+img.style.top = `${topPosition}px`;
+img.style.left = `${leftPositon}px`;
+img.style.transform = 'translate(-50%, -50%)';
