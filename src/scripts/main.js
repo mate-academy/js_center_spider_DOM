8aeaffe92d184071fc8e6a2689ad34d61c626896
NataliaDavida <nataliadavida88@gmail.com>
2024-07-28 09:12:20 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const wallEl = document.querySelector('.wall');
+const spiderEl = document.querySelector('.spider');
+
+spiderEl.style.top =
+  Math.round(wallEl.clientHeight / 2 - spiderEl.clientHeight / 2) + 'px';
+
+spiderEl.style.left =
+  Math.round(wallEl.clientWidth / 2 - spiderEl.clientWidth / 2) + 'px';
