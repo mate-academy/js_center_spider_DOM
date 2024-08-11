8c0187999f1a66736f2f407813f234fc115d8997
MaksymKolesnyk <maksymka@icloud.com>
2024-08-11 14:38:14 -0400

    add solution v1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const wallClientRect = wall.getBoundingClientRect();
+const spiderClientRect = spider.getBoundingClientRect();
+const wallWidth = wallClientRect.width;
+const wallHeight = wallClientRect.height;
+const spiderHeight = spiderClientRect.height;
+const spiderWidth = spiderClientRect.width;
+
+spider.style.left = (wallWidth - spiderWidth - wall.clientLeft) / 2 + 'px';
+spider.style.top = (wallHeight - spiderHeight - wall.clientTop) / 2 + 'px';
