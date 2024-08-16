525a8b140019a37e2005ab69c2ac7d16db6c16aa
Rostyslav <rostislavkpi@gmail.com>
2024-08-16 11:44:04 +0300

    Remove comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,12 +5,3 @@ const spider = document.querySelector('.spider');
 
 spider.style.top = `${wall.clientHeight / 2 - spider.clientHeight / 2}px`;
 spider.style.left = `${wall.clientWidth / 2 - spider.clientWidth / 2}px`;
-
-// Second variant of solution
-/*
-spider.style.cssText = `
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-`;
-*/
