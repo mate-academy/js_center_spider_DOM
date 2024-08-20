9864b4af3203cff94932dc59b3826fdb641f03a0
Kateryna <moskkat40@gmail.com>
2024-08-20 13:23:04 +0200

    add spider center task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const marginLeft = (wall.clientWidth - spider.clientWidth) / 2;
+const marginTop = (wall.clientHeight - spider.clientHeight) / 2;
+
+spider.style.marginLeft = `${marginLeft}px`;
+spider.style.marginTop = `${marginTop}px`;
