b54973eac2da336037f44080db4bbb2f24750a18
May <miaqu138@gmail.com>
2024-08-24 18:35:12 +0200

    test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+const wallRect = wall.getBoundingClientRect();
+const spiderRect = spider.getBoundingClientRect();
+
+const centerX = wallRect.width / 2 - spiderRect.width / 2;
+const centerY = wallRect.height / 2 - spiderRect.height / 2;
+
+spider.style.left = centerX + 'px';
+spider.style.top = centerY + 'px';
