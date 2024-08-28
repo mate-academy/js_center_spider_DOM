6077e67bc811dca3208f54eded5b780c7fa3e97f
Stas <staskapica93@gmail.com>
2024-08-28 18:56:28 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('img');
+const wall = document.querySelector('div');
+
+const spiderHeight = spider.offsetHeight;
+const spiderWidth = spider.offsetWidth;
+
+const topСoordinates = (wall.offsetHeight - spiderHeight) / 2;
+const leftCoordinates = (wall.offsetWidth - spiderWidth) / 2;
+
+spider.style.top = `${topСoordinates}px`;
+spider.style.left = `${leftCoordinates}px`;
