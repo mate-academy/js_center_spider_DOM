aac911051f21678527372b4da1121494786f0e04
Maksym Mohyla <greenparksquad@gmail.com>
2024-08-30 16:48:33 +0300

    center spider solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+// made it via event listener so spider moves swiftly after page is loaded
+document.addEventListener('DOMContentLoaded', () => {
+  const spider = document.querySelector('.spider');
+
+  spider.style.top = '40%';
+  spider.style.left = '40%';
+});
