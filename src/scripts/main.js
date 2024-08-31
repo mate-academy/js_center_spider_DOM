b05349aae5499eb7ec4b821ab457c6d8186e466a
Yevhenii Lutsenko <evgenlutscenko@icloud.com>
2024-09-01 00:58:13 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const wallHalfWidth = wall.clientWidth / 2 - spider.offsetWidth / 2;
+const wallHalfHeight = wall.clientHeight / 2 - spider.offsetHeight / 2;
+
+spider.style.left = wallHalfWidth + 'px';
+spider.style.top = wallHalfHeight + 'px';
