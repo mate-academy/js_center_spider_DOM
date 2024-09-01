a8e27ac4e19ac28aa77d0de1eb1ff121daccb3e6
Serhii Babskyi <sergbald99@gmail.com>
2024-09-01 17:13:34 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const fild = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const fildWidth = fild.clientWidth;
+const fildHeight = fild.clientHeight;
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
+
+const getCenterCoordinates = (widthParent, widthChild) => {
+  return (widthParent - widthChild) / 2;
+};
+
+const centerByX = getCenterCoordinates(fildWidth, spiderWidth);
+const centerByY = getCenterCoordinates(fildHeight, spiderHeight);
+
+spider.style.left = `${centerByX}px`;
+spider.style.top = `${centerByY}px`;
