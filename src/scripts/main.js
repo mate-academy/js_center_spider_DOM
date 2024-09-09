d229ae1b305d6d83d1f71b97b1334fda82cce8cf
Artem-Brui <artembryj@gmail.com>
2024-09-09 11:14:34 +0200

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
 // write code here
+const el = {
+  wall: document.getElementsByClassName('wall')[0],
+  spider: document.getElementsByClassName('spider')[0],
+};
+
+const spiderTop = el.wall.clientHeight / 2 - el.spider.clientHeight / 2;
+const spiderLeft = el.wall.clientWidth / 2 - el.spider.clientWidth / 2;
+
+el.spider.style.top = `${spiderTop}px`;
+el.spider.style.left = `${spiderLeft}px`;
