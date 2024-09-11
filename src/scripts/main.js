2d74debd3e563c14a33d20a267972c55f7b1df2f
Maksym Fedorchuk <maxeffect.apollo@gmail.com>
2024-09-11 05:19:56 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write code here
+const centeringContainer = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+function centering(container, element) {
+  const containerCenterX = container.clientWidth / 2;
+  const containerCenterY = container.clientHeight / 2;
+  const elementCenterX = element.clientWidth / 2;
+  const elementCenterY = element.clientHeight / 2;
+
+  element.style.left = `${containerCenterX - elementCenterX}px`;
+  element.style.top = `${containerCenterY - elementCenterY}px`;
+}
+
+centering(centeringContainer, spider);
