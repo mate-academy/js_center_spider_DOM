ad8cd234dbee597482ba7d495700da9e8cef4f53
Vitaliy <vitaliklevytskyi07@gmail.com>
2024-09-14 14:12:33 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+function centerSpider() {
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  const wallRect = wall.getBoundingClientRect();
+  const spiderRect = spider.getBoundingClientRect();
+
+  const centeredTop = (wallRect.height - spiderRect.height) / 2;
+  const centeredLeft = (wallRect.width - spiderRect.width) / 2;
+
+  spider.style.top = `${centeredTop}px`;
+  spider.style.left = `${centeredLeft}px`;
+}
+
+centerSpider();
+
+window.addEventListener('resize', centerSpider);
