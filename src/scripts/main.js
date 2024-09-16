304bbd4e1201e38084fc549e968f5d8ec062862b
alex_motalex <tokyopac@gmail.com>
2024-09-16 22:10:35 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+const spiderLeft =
+  Math.round((wall.clientWidth - spider.offsetWidth) / 2) + 'px';
+
+const spiderTop =
+  Math.round((wall.clientHeight - spider.offsetHeight) / 2) + 'px';
+
+spider.style.left = spiderLeft;
+spider.style.top = spiderTop;
