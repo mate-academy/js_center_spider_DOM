19e134df1182af5963470ce4b3546d93da6f1311
Zyrak Nur <noorzirak7@gmail.com>
2024-09-20 16:33:29 +0100

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,6 @@ const spider = document.querySelector('.spider');
 const wallWidth = wall.clientWidth;
 const wallHeight = wall.clientHeight;
 
-spider.style.top = `${wallHeight}px`;
-spider.style.left = `${wallWidth}px`;
-spider.style.transform = 'translate(-50%, -50%)'
+spider.style.top = `${wallHeight / 2}px`;
+spider.style.left = `${wallWidth / 2}px`;
+spider.style.transform = 'translate(-50%, -50%)';
