cf2ffc88fa54c8fb7fdafadfe56de42b92706d06
Heorhii Lytvynenko <104064704+Grifion@users.noreply.github.com>
2024-09-21 07:02:38 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,8 @@
 'use strict';
 
-document.querySelector('.spider').style.cssText = `
+const spider = document.querySelector('.spider');
+
+spider.style.cssText = `
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
