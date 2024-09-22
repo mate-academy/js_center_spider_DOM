71e92cee4ae35948deb8d224882d7d621716e57c
Vika <rudaviktoria0@gmail.com>
2024-09-22 20:40:44 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
+/* eslint-disable no-unused-vars */
 'use strict';
 
-// write code here
+const getWall = document.querySelector('.wall');
+const getSpider = document.querySelector('.spider');
+
+const devisionWidth = getWall.clientWidth / 2;
+
+const devisionHeight = getWall.clientHeight / 2;
+
+const devisionSpiderHeight = getSpider.clientHeight / 2;
+
+const devisionSpiderWidth = getSpider.clientWidth / 2;
+
+getSpider.style.left = devisionWidth - devisionSpiderWidth + 'px';
+getSpider.style.top = devisionHeight - devisionSpiderHeight + 'px';
+
+getWall.appendChild(getSpider);
