c0f3d2abd2456f32f8941b05a4a4380b0747a085
Mykyta Forostovets <122729163+Galenwood@users.noreply.github.com>
2024-09-22 10:46:20 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
 // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+spider.style.position = 'inherit';
+wall.style.display = 'flex';
+wall.style.alignItems = 'center';
+wall.style.justifyContent = 'center';
