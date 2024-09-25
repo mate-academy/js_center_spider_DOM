52f7af828b737e5be9814e0f7b9e378f9cc1172a
Kseniia Karpenko <ksushakarpenko635@gmail.com>
2024-09-25 17:55:23 +0200

    centrs spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const image = document.querySelector('.spider');
+
+wall.style.position = 'relative';
+image.style.position = 'absolute';
+
+const centerH = wall.clientHeight / 2 - image.clientHeight / 2;
+const centerW = wall.clientWidth / 2 - image.clientWidth / 2;
+
+image.style.top = centerH + 'px';
+image.style.left = centerW + 'px';
