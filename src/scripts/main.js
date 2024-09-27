0d6229909c30cf0ab9e4f45c7598a6214bc4a031
Anastasiia-Tatarchenko <neveykina.nastya@gmail.com>
2024-09-27 11:48:48 +0200

    removed_unused_lines

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,11 +9,4 @@ wall.style.justifyContent = 'center';
 
 spider.style.position = 'relative';
 
-// const wall = document.querySelector('.wall');
-// const spider = document.querySelector('.spider');
 
-// wall.style.display = 'flex';
-// wall.style.alignItems = 'center';
-// wall.style.justifyContent = 'center';
-
-// spider.style.position = 'relative';
