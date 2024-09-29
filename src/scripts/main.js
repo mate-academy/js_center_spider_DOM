6d838b99c0e072ae7ad653aa1c94612334d9cdb4
Mykhailo Lapchynskyi <mykhailolapchynskyi@mac.local>
2024-09-29 15:40:21 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
 // write code here
+const spider = document.querySelector('.spider');
+
+const parentElem = spider.parentElement;
+
+const parentWidth = parentElem.clientWidth;
+const parentHeight = parentElem.clientHeight;
+
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
+
+const newLeft = (parentWidth - spiderWidth) / 2;
+const newTop = (parentHeight - spiderHeight) / 2;
+
+spider.style.left = `${newLeft}px`;
+spider.style.top = `${newTop}px`;
