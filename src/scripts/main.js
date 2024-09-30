d5fe3a656b1994c330cf487686cb4622e9b94bbb
Yana Myshkivska <ymyshkivska@gmail.com>
2024-09-30 22:39:02 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  const { width: wallWidth, height: wallHeight } = wall.getBoundingClientRect();
+  const { width: spiderWidth, height: spiderHeight } =
+    spider.getBoundingClientRect();
+
+  const centerX = (wallWidth - spiderWidth) / 2;
+  const centerY = (wallHeight - spiderHeight) / 2;
+
+  Object.assign(spider.style, {
+    position: 'absolute',
+    left: `${centerX}px`,
+    top: `${centerY}px`,
+  });
+});
