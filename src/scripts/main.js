83bc407de84eca890f168cd9556891b8ddad845d
AndriiZhovtko <andriy.zhovtko@gmail.com>
2024-10-03 12:05:27 +0200

    add task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,16 +2,8 @@
 
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
+const centerWidth = (wall.clientWidth - spider.clientWidth) / 2;
+const centerHeight = (wall.clientHeight - spider.clientHeight) / 2;
 
-const wallWidth = wall.offsetWidth;
-const wallHeight = wall.offsetHeight;
-
-const spiderWidth = spider.offsetWidth;
-const spiderHeight = spider.offsetHeight;
-
-const centerX = (wallWidth - spiderWidth) / 2;
-const centerY = (wallHeight - spiderHeight) / 2;
-
-spider.style.position = 'absolute';
-spider.style.left = `${centerX}px`;
-spider.style.top = `${centerY}px`;
+spider.style.left = centerWidth + 'px';
+spider.style.top = centerHeight + 'px';
