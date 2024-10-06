4b9489e0b9e71d3193b6ee3d511ef2df55cf7259
SerhiiSemennikov <semennik@hotmail.com>
2024-10-06 19:19:10 +0200

    t1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const getwall = getComputedStyle(wall);
+const getSpider = getComputedStyle(spider);
+
+const topCenter = (killPx(getwall.width) - killPx(getSpider.width)) / 2;
+const leftCenter = (killPx(getwall.height) - killPx(getSpider.height)) / 2;
+
+spider.style.cssText = `
+top:${topCenter}px;
+left:${leftCenter}px;`;
+
+function killPx(px) {
+  return Number(px.replace('px', ''));
+}
