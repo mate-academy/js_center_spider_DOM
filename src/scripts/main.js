058e1737883ce602cb50b89a0fd7e42200f779c0
Andrii Dudko <Annbpiu@gmail.com>
2024-10-09 09:34:20 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const div = document.querySelector('div');
+const img = document.querySelector('img');
+
+div.style.display = 'flex';
+div.style.justifyContent = 'center';
+div.style.alignItems = 'center';
+
+img.style.position = 'relative';
