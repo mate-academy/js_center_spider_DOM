4389196c2231d5567b4ddd90460dec8d9ce193f8
Blervin <blervin121212@gmail.com>
2025-01-03 22:15:14 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
 // write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+const offsetX = (wall.clientWidth - spider.clientWidth) / 2;
+const offsetY = (wall.clientHeight - spider.clientHeight) / 2;
+
+spider.style.transform = `translateX(${offsetX}px) translateY(${offsetY}px)`;
