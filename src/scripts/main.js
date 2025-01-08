a6bb9368e50c0f738bdc72fef18d8de156622d19
Nikolay <diedaemon@gmail.com>
2025-01-08 19:30:01 +0100

    Task complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+const wallStyles = getComputedStyle(wall);
+const spiderStyles = getComputedStyle(spider);
+let targetX = wallStyles.width;
+let targetY = wallStyles.height;
+let spiderWidth = spiderStyles.width;
+let spiderHeight = spiderStyles.height;
+
+targetX = +targetX.slice(0, -2) / 2;
+targetY = +targetY.slice(0, -2) / 2;
+
+spiderWidth = +spiderWidth.slice(0, -2) / 2;
+spiderHeight = +spiderHeight.slice(0, -2) / 2;
+
+const spiderX = targetX - spiderWidth;
+const spiderY = targetY - spiderHeight;
+
+spider.style.left = spiderX + 'px';
+spider.style.top = spiderY + 'px';
