f2b27889629bcd55cf91ffe3093ba95a4528a95c
Volodymyr Vynnyk <vinnikvvv@gmail.com>
2025-01-10 19:43:18 +0200

    add position

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+if (wall && spider) {
+  const centerX = (wall.clientWidth - spider.clientWidth) / 2;
+  const centerY = (wall.clientHeight - spider.clientHeight) / 2;
+
+  spider.style.left = `${centerX}px`;
+  spider.style.top = `${centerY}px`;
+}
