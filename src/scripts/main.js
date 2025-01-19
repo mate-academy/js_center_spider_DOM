924afe484d0d4318a4317cea75257b2c6bb14372
artur.manuilenko <artur.manuilenko@yalantis.net>
2025-01-19 20:40:49 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
 // write code here
+const img = document.querySelector('.spider');
+
+img.style.top = '50%';
+img.style.left = '50%';
+img.style.transform = 'translate(-25px, -25px)';
