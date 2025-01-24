99cd280cac160ca0a9b879599fe421ab28fd0782
Oleh Mysko <olegmysko1996@gmail.com>
2025-01-24 15:01:24 +0200

    task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
+const spiderMan = document.querySelector('img');
+
+spiderMan.style.top = '50%';
+spiderMan.style.left = '50%';
+spiderMan.style.transform = 'translate(-50%, -50%)';
 // write code here
