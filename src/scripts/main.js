f3b147388787c2fc1f12de98b0d286297cd98d61
Bilokon Yaroslav <uvill.github@gmail.com>
2025-01-26 20:21:04 +0200

    moove spider to the center

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const spiderHeigth = spider.clientHeight;
+const spiderWidth = spider.clientWidth;
+const wall = document.querySelector('.wall');
+const wallHeigth = wall.clientHeight;
+const wallWidth = wall.clientWidth;
+const spiderTop = wallHeigth / 2 - spiderHeigth / 2;
+const spiderLeft = wallWidth / 2 - spiderWidth / 2;
+
+spider.setAttribute('style', `top: ${spiderTop}px; left: ${spiderLeft}px`);
