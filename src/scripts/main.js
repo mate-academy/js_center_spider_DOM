ec35821b4c73beba73ecbb5a862ca7da9c183857
Oleh Yavoriv <oleh.yavoriv@gmail.com>
2025-01-29 09:53:26 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,7 @@
 'use strict';
 
-// write code here
+const spiderImage = document.querySelector('.spider');
+
+spiderImage.style.top = '50%';
+spiderImage.style.left = '50%';
+spiderImage.style.transform = 'translate(-50%, -50%)';
