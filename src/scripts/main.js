f870c44d62844776cb83c64e2b3cc7d144f527f9
Uusuff <90629840+Uusuff@users.noreply.github.com>
2025-01-30 11:03:41 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const centerSpider = () => {
+  const wallRect = wall.getBoundingClientRect();
+  const spiderRect = spider.getBoundingClientRect();
+  const posLeft = (wallRect.width - spiderRect.width) / 2;
+  const posTop = (wallRect.height - spiderRect.height) / 2;
+
+  spider.style.left = `${posLeft}px`;
+  spider.style.top = `${posTop}px`;
+};
+
+window.addEventListener('load', centerSpider);
