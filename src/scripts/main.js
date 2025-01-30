cb66a6a28528b7912ae025edcdffa5a8ff6dc6db
marushchak <marushchak099@gmail.com>
2025-01-30 14:16:15 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const block = document.querySelector('.wall');
+const blockWidth = block.clientWidth;
+const blockHeight = block.clientHeight;
+
+const picture = document.querySelector('.spider');
+const pictureWidth = picture.clientWidth;
+const pictureHeight = picture.clientHeight;
+
+const positionLeft = (blockWidth - pictureWidth) / 2;
+const positionTop = (blockHeight - pictureHeight) / 2;
+
+picture.style.left = positionLeft + 'px';
+picture.style.top = positionTop + 'px';
