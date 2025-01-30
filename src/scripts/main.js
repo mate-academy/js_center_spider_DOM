9f605399a78f32d655c72d6a72bffdf303aa0bc8
Mykyta Kalniev <nikitamal4ik@gmail.com>
2025-01-30 09:14:13 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+
+function getSize(el) {
+  const height = document.querySelector(el).clientHeight;
+  const width = document.querySelector(el).clientWidth;
+
+  return {
+    height: height,
+    width: width,
+  };
+}
+
+spider.style.cssText = `
+  position: absolute;
+  top: ${getSize('.wall').height / 2 - getSize('.spider').height / 2}px;
+  left: ${getSize('.wall').width / 2 - getSize('.spider').width / 2}px;
+`;
