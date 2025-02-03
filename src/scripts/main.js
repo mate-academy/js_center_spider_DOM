1adf633a49bc8a30166338102083de5d24156fc8
Alina <alina220380@gmail.com>
2025-02-03 18:24:57 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // write code here
+const pole = document.querySelector('.wall');
+const element = document.querySelector('.spider');
+
+const poleSize = pole.getBoundingClientRect();
+const elementSize = element.getBoundingClientRect();
+
+const x = (poleSize.width - elementSize.width) / 2;
+const y = (poleSize.height - elementSize.height) / 2;
+
+element.style.position = 'absolute';
+element.style.left = x + 'px';
+element.style.top = y + 'px';
