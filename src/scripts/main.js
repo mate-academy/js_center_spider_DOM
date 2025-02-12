4d0f1856d851308662245ec55bc9f808b3c40792
IURII FEDYNA <iurii.fedyna@gmail.com>
2025-02-12 16:50:00 -0330

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+
+const centerX = (wallWidth - spider.offsetWidth) / 2;
+const centerY = (wallHeight - spider.offsetHeight) / 2;
+
+spider.style.position = 'absolute';
+spider.style.left = centerX + 'px';
+spider.style.top = centerY + 'px';
