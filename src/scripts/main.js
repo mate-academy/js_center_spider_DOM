4efcd1558df095afd08f241be1d412c12bbb16df
Oleh <pryge321@gmail.com>
2025-02-14 20:25:59 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', function () {
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  const wallStyles = getComputedStyle(wall);
+  const borderSize = parseFloat(wallStyles.borderWidth);
+
+  const wallHeight = wall.getBoundingClientRect().height;
+  const wallWidth = wall.getBoundingClientRect().width;
+  const spiderHeight = spider.getBoundingClientRect().height;
+  const spiderWidth = spider.getBoundingClientRect().width;
+
+  spider.style.top = `${(wallHeight - spiderHeight) / 2 - borderSize}px`;
+  spider.style.left = `${(wallWidth - spiderWidth) / 2 - borderSize}px`;
+});
