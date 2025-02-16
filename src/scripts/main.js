9dd8945fd01265f4ba78eb908598046a21404160
troleksii <aliosha123418@gmail.com>
2025-02-16 17:26:42 +0100

    Fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,5 +10,5 @@ const coordX = (wallRect.width - spiderRect.width) / 2 - wall.clientLeft;
 const coordY = (wallRect.height - spiderRect.height) / 2 - wall.clientTop;
 
 spider.style.position = 'absolute';
-spider.style.top = `${coordX}px`;
-spider.style.left = `${coordY}px`;
+spider.style.top = `${coordY}px`;
+spider.style.left = `${coordX}px`;
