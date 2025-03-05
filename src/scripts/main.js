3c3ff905f5caa36414958b42f15639d651bef2d7
inikiforov <ievgen.nikiforov@tedata.de>
2025-03-05 12:23:57 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 // write code here
+const wall = document.getElementsByClassName('wall')[0];
+const wallHeight = wall.offsetHeight;
+const wallWidth = wall.offsetWidth;
+const wallTop = wall.offsetTop;
+const wallLeft = wall.offsetLeft;
+const centerX = wallWidth / 2;
+const centerY = wallHeight / 2;
+const spider = document.getElementsByClassName('spider')[0];
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
+
+spider.style.left = centerX - spiderWidth / 2 + 'px';
+spider.style.top = centerY - spiderHeight / 2 + 'px';
