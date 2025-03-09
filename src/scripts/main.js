88a60c3447b046d5aff0569d1098bb7aec5c5001
Yaroslav <yarmoshyk96@gmail.com>
2025-03-09 19:38:31 +0200

    Add center spider DOM solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+function centerSpider() {
+  const wall = document.querySelector('.wall');
+  const wallHeight = wall.clientHeight;
+  const wallWidth = wall.clientWidth;
+  const spider = document.querySelector('.spider');
+  const spiderHeight = spider.offsetHeight;
+  const spiderWidth = spider.offsetWidth;
+
+  spider.style.left = Math.round(wallWidth / 2 - spiderWidth / 2) + 'px';
+  spider.style.top = Math.round(wallHeight / 2 - spiderHeight / 2) + 'px';
+}
+
+centerSpider();
