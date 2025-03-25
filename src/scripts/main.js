91190beb933f55f31feba4660f5d2703936fdf06
Maria <m.abeeva.ma@gmail.com>
2025-03-25 14:05:52 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
 // write code here
+const spider = document.querySelector('.spider');
+const field = document.querySelector('.wall');
+
+spider.style.left =
+  Math.round(field.clientWidth / 2 - spider.offsetWidth / 2) + 'px';
+
+spider.style.top =
+  Math.round(field.clientHeight / 2 - spider.offsetHeight / 2) + 'px';
