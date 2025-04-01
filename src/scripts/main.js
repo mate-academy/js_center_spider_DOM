3d93ca62f5e32626fa6b9f37f86168f70744dcbf
Viktor Shchehelskyi <victorshegelsky@gmail.com>
2025-04-01 03:38:55 +0300

    finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const left =
+  (parseFloat(getComputedStyle(wall).width) -
+    parseFloat(getComputedStyle(spider).width)) /
+  2;
+
+const topp =
+  (parseFloat(getComputedStyle(wall).height) -
+    parseFloat(getComputedStyle(spider).height)) /
+  2;
+
+spider.style.top = topp + 'px';
+spider.style.left = left + 'px';
