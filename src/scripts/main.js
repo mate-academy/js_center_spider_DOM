72cbb40bb429e8c92446a3024529230ee92ff808
BoGoBooM <myPikcha@gmail.com>
2025-04-04 13:58:46 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const spiderTop = (wall.clientHeight - spider.clientHeight) / 2;
+const spiderRight = (wall.clientWidth - spider.clientWidth) / 2;
+
+spider.style.top = `${spiderTop}px`;
+spider.style.right = `${spiderRight}px`;
