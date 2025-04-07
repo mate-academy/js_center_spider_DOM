db1df86c15657d7a510b3975916cae0bd0cbf777
gabai62 <gabai62it@gmail.com>
2025-04-07 14:08:45 +0300

    centered

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
-// write code here
+const wallEl = document.querySelector('.wall');
+const spiderEl = document.querySelector('.spider');
+
+spiderEl.style.top = (wallEl.clientHeight - spiderEl.clientHeight) / 2 + 'px';
+
+spiderEl.style.left = (wallEl.clientWidth - spiderEl.clientWidth) / 2 + 'px';
