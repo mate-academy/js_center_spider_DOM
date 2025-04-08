dffc80a39a1b074f3eadce0eaafcd5e753a42c57
Hanna <hannachachacha7@gmail.com>
2025-04-08 16:48:45 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const field = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+field.style.position = 'relative';
+spider.style.position = 'absolute';
+spider.style.top = '50%';
+spider.style.left = '50%';
+spider.style.transform = 'translate(-50%, -50%)';
