e844cc94024208a612961c070aed761e7b5bd0a4
AVyhaa <artemvyhovanec1@gmail.com>
2025-04-08 15:56:54 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const centerSpider = () => {
+  const wallElement = document.querySelector('.wall');
+  const spiderElement = document.querySelector('.spider');
+
+  wallElement.style.display = 'flex';
+  wallElement.style.justifyContent = 'center';
+  wallElement.style.alignItems = 'center';
+
+  spiderElement.style.top = 'unset';
+  spiderElement.style.left = 'unset';
+};
+
+centerSpider();
