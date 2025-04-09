82b37a7f62f9340f2b46d9ef3acce5ca83a8f4cf
IhorChaykovskyy <chaikovskiyihor@gmail.com>
2025-04-09 16:15:19 +0300

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,15 @@
 'use strict';
 
-const wall = document.querySelector('.wall');
-const spider = document.querySelector('.spider');
+document.addEventListener('DOMContentLoader', () => {
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
 
-wall.style.position = 'relative';
-spider.style.position = 'absolute';
+  wall.style.position = 'relative';
+  spider.style.position = 'absolute';
 
-spider.addEventListener('load', () => {
   const left = (wall.clientWidth - spider.offsetWidth) / 2;
-  const top = (wall.clientHeight - spider.offsetHeight) / 2;
+  const topOffset = (wall.clientHeight - spider.offsetHeight) / 2;
 
   spider.style.left = `${left}px`;
-  spider.style.top = `${top}px`;
+  spider.style.top = `${topOffset}px`;
 });
