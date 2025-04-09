e54ccbc7216c5cf30936e7c1344bd42cc7c7be83
IhorChaykovskyy <chaikovskiyihor@gmail.com>
2025-04-09 16:10:10 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.style.position = 'relative';
+spider.style.position = 'absolute';
+
+spider.addEventListener('load', () => {
+  const left = (wall.clientWidth - spider.offsetWidth) / 2;
+  const top = (wall.clientHeight - spider.offsetHeight) / 2;
+
+  spider.style.left = `${left}px`;
+  spider.style.top = `${top}px`;
+});
