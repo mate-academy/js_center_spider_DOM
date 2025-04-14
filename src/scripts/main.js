2243d33da46c3011cd24fcbec3edb147a55bccd6
apr1ori <vladgolenok4@gmail.com>
2025-04-14 21:23:43 +0200

    task 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+
+spider.style.width = '30px';
+spider.style.position = 'relative';
+spider.style.top = `${spider.offsetParent.clientHeight * 0.5 - spider.clientHeight * 0.5}px`;
+spider.style.left = `${spider.offsetParent.offsetWidth * 0.5 - spider.offsetWidth * 0.5}px`;
