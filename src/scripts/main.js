ca7fb67e8996423481ace0511fd2aa4486e24434
Vhatever <fserrorka@gmail.com>
2025-04-14 15:29:40 +0300

    tryN1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 // write code here
+function center() {
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+  const hPosition = wall.clientWidth / 2 - spider.clientWidth / 2;
+  const vPosition = wall.clientHeight / 2 - spider.clientHeight / 2;
+
+  spider.style.left = `${hPosition}px`;
+  spider.style.top = `${vPosition}px`;
+}
+
+center();
