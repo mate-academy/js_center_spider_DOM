cb9e5752e2733454dccbf341ac3d413eb52d39ad
Oksana Sidanych <sasha3sashkova@gmail.com>
2025-05-01 19:29:34 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+function centerSpider() {
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  const wallWidth = wall.offsetWidth;
+  const wallHeight = wall.offsetHeight;
+  const spiderWidth = spider.offsetWidth;
+  const spiderHeight = spider.offsetHeight;
+
+  const spiderLeft = (wallWidth - spiderWidth) / 2;
+  const spiderTop = (wallHeight - spiderHeight) / 2;
+
+  spider.style.position = 'absolute';
+  spider.style.left = `${spiderLeft}px`;
+  spider.style.top = `${spiderTop}px`;
+}
+
+window.onload = centerSpider;
+
+window.onresize = centerSpider;
