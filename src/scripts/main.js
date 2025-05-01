a00cb7527660b929fea86d7976f89e0a139582e9
Dmytro Kovtun <kovtundima2910@gmail.com>
2025-05-01 17:11:03 +0300

    i added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  const wallRect = wall.getBoundingClientRect();
+
+  const wallWidth = wall.offsetWidth;
+  const wallHeight = wall.offsetHeight;
+
+  const spiderWidth = spider.offsetWidth;
+  const spiderHeight = spider.offsetHeight;
+
+  const l = wallRect.left + (wallWidth - spiderWidth) / 2;
+  const t = wallRect.top + (wallHeight - spiderHeight) / 2;
+
+  spider.style.position = 'fixed';
+  spider.style.left = l + 'px';
+  spider.style.top = t + 'px';
+});
