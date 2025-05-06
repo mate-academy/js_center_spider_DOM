6d11c5e9455d6c2c653e9dfdf2dd538513bcd382
Bogdan Kalytovskiy <bogdankalytovskkiy@gmail.com>
2025-05-06 20:56:49 +0300

    add sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const left = (wall.clientWidth - spider.clientWidth) / 2;
+const top1 = (wall.clientHeight - spider.clientHeight) / 2;
+
+// Устанавливаем позицию
+spider.style.position = 'absolute';
+spider.style.left = `${left}px`;
+spider.style.top = `${top1}px`;
