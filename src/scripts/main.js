6c22c6a3b56768a22fd98836a64c9d63620ce20c
Vlad Mitlosh <vlad.mitlosh@gmail.com>
2025-05-24 21:06:26 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  function centerSpider() {
+    const spiderRect = spider.getBoundingClientRect();
+
+    const centerX = (wall.clientWidth - spiderRect.width) / 2;
+    const centerY = (wall.clientHeight - spiderRect.height) / 2;
+
+    spider.style.left = `${centerX}px`;
+    spider.style.top = `${centerY}px`;
+  }
+
+  centerSpider();
+
+  window.addEventListener('resize', centerSpider);
+});
