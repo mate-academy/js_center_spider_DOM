2e03d22b757982108f6c1ddafb9034cc9b197248
Yevhen <zk4599693@gmail.com>
2025-06-11 23:04:50 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const wallElement = document.querySelector('.wall');
+  const spiderElement = document.querySelector('.spider');
+
+  if (!wallElement || !spiderElement) {
+    return;
+  }
+
+  const wallWidth = wallElement.offsetWidth;
+  const wallHeight = wallElement.offsetHeight;
+
+  const spiderWidth = spiderElement.offsetWidth;
+  const spiderHeight = spiderElement.offsetHeight;
+
+  const centerLeft = (wallWidth - spiderWidth) / 2;
+  const centerTop = (wallHeight - spiderHeight) / 2;
+
+  spiderElement.style.left = `${centerLeft}px`;
+  spiderElement.style.top = `${centerTop}px`;
+});
