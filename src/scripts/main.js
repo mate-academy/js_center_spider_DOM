0b3e9f400d6b41d820ca09e8a30e5b69b3ea9af1
Oleksii Myronenko <oleksiimrnko@gmail.com>
2025-06-17 23:09:16 +0300

    add test solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+window.addEventListener('DOMContentLoaded', () => {
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  spider.style.position = 'absolute';
+
+  const leftPos = (wall.clientWidth - spider.width) / 2;
+  const topPos = (wall.clientHeight - spider.height) / 2;
+
+  spider.style.left = `${leftPos}px`;
+  spider.style.top = `${topPos}px`;
+});
