c5c40948dd5c07f74f06ed94db48b4d50dfb5b98
Artur Shatskov <artur.shackov77@gmail.com>
2025-07-02 21:40:46 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const padTop =
+  document.querySelector('.wall').clientHeight / 2 -
+  document.querySelector('.spider').clientHeight / 2;
+
+const padLeft =
+  document.querySelector('.wall').clientWidth / 2 -
+  document.querySelector('.spider').clientWidth / 2;
+
+const spider = document.querySelector('.spider');
+
+spider.style.top = `${padTop}px`;
+spider.style.left = `${padLeft}px`;
