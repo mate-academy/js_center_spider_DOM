2af61f727dd1442839b325e18f2c20cf287a139f
dima91020 <vereskodima76@gmail.com>
2025-07-03 11:45:10 +0300

    add main.js file

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+function calculateCenter(container, obj) {
+  const neededTop = container.clientHeight / 2;
+  const neededLeft = container.clientWidth / 2;
+
+  obj.style.top = `${neededTop - obj.clientHeight / 2}px`;
+  obj.style.left = `${neededLeft - obj.clientWidth / 2}px`;
+}
+
+calculateCenter(wall, spider);
