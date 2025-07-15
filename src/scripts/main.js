4e5497a1be200f9e600643cf887065f377485ad2
Andriï Zakhartchenko <andriizakhartchenko@outlook.fr>
2025-07-15 21:16:56 +0200

    save before push

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,3 @@
-'use strict';
-
-// write code here
+document.querySelector('.spider').style.top = '50%';
+document.querySelector('.spider').style.left = `50%`;
+document.querySelector('.spider').style.transform = `translate(-50%, -50%)`;
