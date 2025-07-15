eeda2580515fe82b9e25dc67abe1c8c9e9102646
Vladyslav Stepanenko <vlad.step.186@gmail.com>
2025-07-15 11:46:46 +0300

    centered spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+const widthSpider = spider.offsetWidth;
+const heightSpider = spider.offsetHeight;
+
+const widthWall = wall.clientWidth;
+const heightWall = wall.clientHeight;
+
+const center = (heightWall - heightSpider) / 2;
+const left = (widthWall - widthSpider) / 2;
+
+spider.style.top = `${center}px`;
+spider.style.left = `${left}px`;
