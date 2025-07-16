9723c5d0881e8bebc2bd7bc3ef57f8ff8c711901
Taras Niklevych <niklevychtaras@gmail.com>
2025-07-16 11:49:25 +0300

    add minor fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,8 +4,8 @@ function centerSpider() {
   const wall = document.querySelector('.wall');
   const spider = document.querySelector('.spider');
 
-  spider.style.top = wall.clientHeight / 2 - spider.clientHeight / 2 + 'px';
-  spider.style.left = wall.clientWidth / 2 - spider.clientWidth / 2 + 'px';
+  spider.style.top = `${wall.clientHeight / 2 - spider.clientHeight / 2}px`;
+  spider.style.left = `${wall.clientWidth / 2 - spider.clientWidth / 2}px`;
 }
 
 centerSpider();
