f25804b894070545b7a277125c34e1fb23258cde
Mykyta Musaiev <n.musaiev@gmail.com>
2025-07-19 17:46:27 +0300

    task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const wallH = wall.offsetHeight;
+const wallW = wall.offsetWidth;
+const spiderH = spider.offsetHeight;
+const spiderW = spider.offsetWidth;
+const spiderPosH = (wallH - spiderH) / 2;
+const spiderPosW = (wallW - spiderW) / 2;
+
+spider.style.position = 'absolute';
+spider.style.top = `${spiderPosH}px`;
+spider.style.left = `${spiderPosW}px`;
