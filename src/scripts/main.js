90371a899ebec6faf60b7b7c6ee7e09e4a5c44ef
Denis <denchick1546450@gmail.com>
2025-07-22 16:06:30 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+
+const spider = document.querySelector('.spider');
+spider.style.top = '50%';
+spider.style.left = '50%';
+spider.style.transform = 'translate(-50%, -50%)';
+// spider :
+// width: 50px;
+// height: 50px;
+
+
+// wall :
+// width: 400px;
+// height: 400px;
