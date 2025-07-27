d11b6058aea4461478ca79d838dacfe29c79b140
Sagi4A <voitko.katya.1@gmail.com>
2025-07-27 15:00:08 +0200

    spider man, spider man

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const wallRect = wall.getBoundingClientRect();
+const spiderRect = spider.getBoundingClientRect();
+
+const centerTop = (wallRect.height - spiderRect.height) / 2;
+const centerLeft = (wallRect.width - spiderRect.width) / 2;
+
+spider.style.top = `${centerTop}px`;
+spider.style.left = `${centerLeft}px`;
