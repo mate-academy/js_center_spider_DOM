e4bc947b3d14c1805654e28b382527b94a1da831
Nazar <nazar.shevchuk.121@gmail.com>
2025-07-27 15:11:21 +0300

    sol1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const spider = document.getElementsByClassName('spider')[0];
+const wall = document.getElementsByClassName('wall')[0];
+
+spider.style.position = 'relative';
+
+wall.style.display = 'flex';
+wall.style.justifyContent = 'center';
+wall.style.alignItems = 'center';
