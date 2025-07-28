43f7a40a2637223e37fced161df8b4b4b9b2f75f
Dmytro Yakovchuk <iknowtooyou@gmail.com>
2025-07-28 23:46:54 +0300

    spider in center

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
 // write code here
+const containerDiv = document.querySelector('div');
+
+containerDiv.classList.add('wall');
+
+const containerImg = document.querySelector('img');
+
+containerImg.classList.add('spider');
+
+containerDiv.style.position = 'relative';
+containerDiv.style.paddingLeft = '10px';
+containerDiv.style.paddingTop = '10px';
+
+containerImg.style.position = 'absolute';
+containerImg.style.top = '50%';
+containerImg.style.left = '50%';
+containerImg.style.transform = 'translate(-50%, -50%)';
+
+containerDiv.appendChild(containerImg);
