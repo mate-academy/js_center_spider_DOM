b846662fb5707c017d8637e349aa4d1c635300c7
Maxim <makson202007@gmail.com>
2025-08-06 10:25:07 +0300

    added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
 // write code here
+const $spiderElement = document.querySelector('img.spider');
+const $wallElement = document.querySelector('div.wall');
+
+const spiderBoundingRect = $spiderElement.getBoundingClientRect();
+
+$spiderElement.style.left = `${($wallElement.clientWidth - spiderBoundingRect.width) / 2}px`;
+$spiderElement.style.top = `${($wallElement.clientHeight - spiderBoundingRect.height) / 2}px`;
