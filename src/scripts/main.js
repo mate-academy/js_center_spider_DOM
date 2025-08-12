9ba47c3547903d7d3ccc0f3dbbd06ee5d300897c
Yan Kazymyr <kazymyr.yan@gmail.com>
2025-08-12 22:34:53 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+// Получение размеров
+const wallHeight = wall.clientHeight;
+const wallWidth = wall.clientWidth;
+const spiderHeight = spider.clientHeight;
+const spiderWidth = spider.clientWidth;
+
+// Вычисление центра
+const spiderTop = (wallHeight - spiderHeight) / 2;
+const spiderLeft = (wallWidth - spiderWidth) / 2;
+
+// Установка стилей
+spider.style.top = `${spiderTop}px`;
+spider.style.left = `${spiderLeft}px`;
