ea2b9d00b1da22a5b1c142819c750efb540c97e2
Matvey Turluk <matychencia.ua@gmail.com>
2025-08-20 17:25:46 +0300

    added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
 // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const turnRight = wall.clientWidth / 2 - spider.clientWidth / 2;
+const turnBottom = wall.clientHeight / 2 - spider.clientHeight / 2;
+
+spider.style.left = `${turnRight}px`;
+spider.style.top = `${turnBottom}px`;
