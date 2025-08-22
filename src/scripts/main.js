f5669015b3d25e0eac19093c3b5e04ea6e444ce5
Tetiana Susol <tetyanamyeskalo@gmail.com>
2025-08-22 19:08:29 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+'use strict';
+
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+// Отримуємо розміри контейнера і павука
+const wallRect = wall.getBoundingClientRect();
+const spiderRect = spider.getBoundingClientRect();
+
+// Обчислюємо координати для центру
+const left = (wallRect.width - spiderRect.width) / 2;
+const offsetTop = (wallRect.height - spiderRect.height) / 2;
+
+// Призначаємо павуку абсолютне позиціонування та координати
+spider.style.position = 'absolute';
+spider.style.left = `${left}px`;
+spider.style.top = `${offsetTop}px`;
