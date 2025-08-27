d383965e9ca296ecf86c848480715630bf860be0
Volodymyr Kolosov <kolosovvova0@gmail.com>
2025-08-27 13:31:13 +0300

    spider center

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+const plate = document.querySelector('.wall');
+const plateWidth = plate.offsetWidth;
+const plateHeight = plate.offsetHeight;
+
+const plateSpider = document.querySelector('.spider');
+const spiderWidth = plateSpider.offsetWidth;
+const spiderHeight = plateSpider.offsetHeight;
+
+plateSpider.style.top = plateHeight / 2 - spiderHeight / 2 + 'px';
+plateSpider.style.left = plateWidth / 2 - spiderWidth / 2 + 'px';
