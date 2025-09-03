be43730d3c75c10f670646a519b46daa35c59bd9
TetiankaSh <shh.tetiana@gmail.com>
2025-09-03 17:30:03 +0300

    first attempt

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const field = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const fieldWidth = field.clientWidth;
+const fieldHeight = field.clientHeight;
+
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
+
+const centerX = (fieldWidth - spiderWidth) / 2;
+const centerY = (fieldHeight - spiderHeight) / 2;
+
+spider.style.position = 'absolute';
+spider.style.left = `${centerX}px`;
+spider.style.top = `${centerY}px`;
