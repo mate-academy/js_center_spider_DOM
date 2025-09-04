44b66c4572df7bf72a86b04a292ee48c22415c03
InhaKr <inkr3891@gmail.com>
2025-09-04 16:25:52 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
 // write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  if (wall && spider) {
+    spider.style.top = '50%';
+    spider.style.left = '50%';
+    spider.style.transform = 'translate(-50%, -50%)';
+  }
+});
