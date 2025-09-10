cc787617dfb711b02cf2e971af101459aaa276b6
Vladyslav Kostiuk <k4nezoor@gmail.com>
2025-09-10 15:27:49 +0200

    changed some issues with scss

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  const wallWidth = wall.clientWidth;
+  const wallHeight = wall.clientHeight;
+
+  const spiderWidth = spider.clientWidth;
+  const spiderHeight = spider.clientHeight;
+
+  const left = (wallWidth - spiderWidth) / 2;
+  const up = (wallHeight - spiderHeight) / 2;
+
+  spider.style.left = `${left}px`;
+  spider.style.top = `${up}px`;
+});
