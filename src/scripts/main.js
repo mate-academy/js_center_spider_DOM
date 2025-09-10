7398e24b428ca6126655b8526c62125b5fb8c84d
Nazariy <nazariy.holovach2007@gmail.com>
2025-09-10 19:05:42 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const spiderObj = document.querySelector('.spider');
+const wallObj = document.querySelector('.wall');
+
+if (!spiderObj || !wallObj) {
+  throw new Error('Objects are not found');
+}
+
+wallObj.style.position = 'relative';
+spiderObj.style.position = 'absolute';
+spiderObj.style.top = '50%';
+spiderObj.style.left = '50%';
+spiderObj.style.transform = 'translate(-50%, -50%)';
