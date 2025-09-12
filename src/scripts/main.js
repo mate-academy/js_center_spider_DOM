5b525e98505ca340a3388e1591f0d79733e1f330
Oleksandr <kozyrev.sania@gmail.com>
2025-09-12 13:35:39 +0300

    fix solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,25 @@
 'use strict';
 
-const element = document.querySelector('.spider');
-const field = document.querySelector('.wall');
+document.addEventListener('DOMContentLoaded', () => {
+  const element = document.querySelector('.spider');
+  const field = document.querySelector('.wall');
 
-function positionsCenter(area, item) {
-  const areaSize = [area.clientWidth, area.clientHeight];
-  const itemSize = [item.clientWidth, item.clientHeight];
+  function positionsCenter(area, item) {
+    const areaSize = [area.clientWidth, area.clientHeight];
+    const itemSize = [item.clientWidth, item.clientHeight];
 
-  const [areaWidth, areaHeight] = areaSize;
-  const [itemWidth, itemHeight] = itemSize;
+    const [areaWidth, areaHeight] = areaSize;
+    const [itemWidth, itemHeight] = itemSize;
 
-  const centerWidth = areaWidth / 2 - itemWidth / 2;
-  const centerHeight = areaHeight / 2 - itemHeight / 2;
+    const centerWidth = areaWidth / 2 - itemWidth / 2;
+    const centerHeight = areaHeight / 2 - itemHeight / 2;
 
-  return [centerWidth, centerHeight];
-}
+    return {left: centerWidth, top: centerHeight};
+  }
 
-const getPosition = positionsCenter(field, element);
+  const getPosition = positionsCenter(field, element);
 
-element.style.top = `${getPosition[0]}px`;
-element.style.left = `${getPosition[1]}px`;
+  element.style.position = 'absolute';
+  element.style.top = `${getPosition.top}px`;
+  element.style.left = `${getPosition.left}px`;
+});
