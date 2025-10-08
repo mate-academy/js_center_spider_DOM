fa4acf45f95c719cf76709b7af9a18b0dafef8f6
Luna <yanka16247@mail.com>
2025-10-08 19:52:52 +0300

    add task solution3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,11 +11,15 @@ const spiderHeight = spider.clientHeight;
 const centerTop = (wallHeight - spiderHeight) / 2;
 const centerLeft = (wallWidth - spiderWidth) / 2;
 const wallCS = window.getComputedStyle(wall);
+const spiderCS = window.getComputedStyle(spider);
 
 if (wallCS.position === 'static') {
   wall.style.position = 'relative';
 }
 
+if (spiderCS.position !== 'static') {
+  spider.style.position = 'absolute';
+}
 spider.style.top = centerTop + 'px';
 spider.style.left = centerLeft + 'px';
 // write code here
