27d10b0127fef0bea24a504b16e06cd7ce8a38d1
Artem Yakhno <artem.yakhno.ua@gmail.com>
2025-10-09 22:31:17 +0300

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const topValue = Math.round(wall.clientHeight / 2 - spider.clientHeight / 2);
+const leftValue = Math.round(wall.clientWidth / 2 - spider.clientWidth / 2);
+
+spider.style.position = 'absolute';
+spider.style.top = `${topValue}px`;
+spider.style.left = `${leftValue}px`;
