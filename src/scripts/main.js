5686b6447ffd389ab679d947f535f27a6803c2b7
Artem Yakhno <artem.yakhno.ua@gmail.com>
2025-10-10 00:44:04 +0300

    fixed issues

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,22 @@
 'use strict';
 
-const wall = document.querySelector('.wall');
-const spider = document.querySelector('.spider');
-const topValue = Math.round(wall.clientHeight / 2 - spider.clientHeight / 2);
-const leftValue = Math.round(wall.clientWidth / 2 - spider.clientWidth / 2);
+function centerElement(wall, spider) {
+  if (!wall || !spider) {
+    return;
+  }
 
-spider.style.position = 'absolute';
-spider.style.top = `${topValue}px`;
-spider.style.left = `${leftValue}px`;
+  const topValue = Math.round(wall.clientHeight / 2 - spider.clientHeight / 2);
+  const leftValue = Math.round(wall.clientWidth / 2 - spider.clientWidth / 2);
+
+  wall.style.position = 'relative';
+  spider.style.position = 'absolute';
+  spider.style.top = `${topValue}px`;
+  spider.style.left = `${leftValue}px`;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  centerElement(wall, spider);
+});
