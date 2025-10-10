b0500cd5a2945f92f76c560bcbc7c6bd620c1d43
Yaroslav Molchanov <y.molchanoff@gmail.com>
2025-10-10 20:33:20 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
 // write code here
+window.addEventListener('load', () => {
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  // Робимо, щоб позиціювання працювало
+  wall.style.position = 'relative';
+  spider.style.position = 'absolute';
+
+  // Отримуємо розміри
+  const wallWidth = wall.clientWidth;
+  const wallHeight = wall.clientHeight;
+  const spiderWidth = spider.clientWidth;
+  const spiderHeight = spider.clientHeight;
+
+  // Розрахунок координат центру
+  const leftPos = (wallWidth - spiderWidth) / 2;
+  const topPos = (wallHeight - spiderHeight) / 2;
+
+  // Застосовуємо координати
+  spider.style.left = `${leftPos}px`;
+  spider.style.top = `${topPos}px`;
+});
