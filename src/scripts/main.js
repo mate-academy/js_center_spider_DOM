79276d5ec45cb47a5b60ab1d943fd5f321200a69
Dmytro Popov <lort20178@gmail.com>
2025-10-26 16:15:37 +0200

    Add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const wallWidth = wall.offsetWidth;
+const wallHeight = wall.offsetHeight;
+
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
+
+const topPosition = (wallHeight - spiderHeight) / 2;
+const leftPosition = (wallWidth - spiderWidth) / 2;
+
+spider.style.position = 'absolute';
+spider.style.top = `${topPosition}px`;
+spider.style.left = `${leftPosition}px`;
+
 // write code here
