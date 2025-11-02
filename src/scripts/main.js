2e27f7bdb344b8dd430ae8824172b5a70e4667a5
Marina Kramarchuk <kramarchukmm20182018@gmail.com>
2025-11-02 18:28:13 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const elemSpider = document.querySelector('.spider');
+
+const elemWall = document.querySelector('.wall');
+
+elemWall.style.position = 'relative';
+
+elemSpider.style.position = 'absolute';
+
+const gorysontal = elemWall.clientWidth / 2 - elemSpider.offsetWidth / 2;
+
+const vertical = elemWall.clientHeight / 2 - elemSpider.offsetHeight / 2;
+
+elemSpider.style.left = gorysontal + 'px';
+elemSpider.style.top = vertical + 'px';
