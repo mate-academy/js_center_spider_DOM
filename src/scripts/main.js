96001a4de0ac46da6cec32b5efaec6d3c576a616
Oksana Rubel <okhymych480@gmail.com>
2025-11-05 14:32:28 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write code here
+const container = document.querySelector('.wall');
+const elem = document.querySelector('.spider');
+
+container.style.position = 'relative';
+
+elem.style.position = 'absolute';
+elem.style.top = '50%';
+elem.style.left = '50%';
+elem.style.transform = 'translate(-50%, -50%)';
