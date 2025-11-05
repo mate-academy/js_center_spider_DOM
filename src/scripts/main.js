076470d8d2e9865235fa5eb44dfb2a551c292ff8
Oksana Rubel <okhymych480@gmail.com>
2025-11-05 14:41:00 +0200

    Update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,8 +4,15 @@ const container = document.querySelector('.wall');
 const elem = document.querySelector('.spider');
 
 container.style.position = 'relative';
-
 elem.style.position = 'absolute';
-elem.style.top = '50%';
-elem.style.left = '50%';
-elem.style.transform = 'translate(-50%, -50%)';
+
+const containerWidth = container.offsetWidth;
+const containerHeight = container.offsetHeight;
+const elemWidth = elem.offsetWidth;
+const elemHeight = elem.offsetHeight;
+
+const sizeTop = (containerHeight - elemHeight) / 2;
+const sizeLeft = (containerWidth - elemWidth) / 2;
+
+elem.style.top = `${sizeTop}px`;
+elem.style.left = `${sizeLeft}px`;
