93e85beaff30823af88afac4477f1fd1fd4b561e
Paulina <paulina.zwolenik@gmail.com>
2025-11-07 10:19:39 +0100

    Add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const wallElement = document.querySelector('.wall');
+const spiderElement = document.querySelector('.spider');
+const wallHeight = wallElement.offsetHeight;
+const wallWidth = wallElement.offsetWidth;
+const spiderHeight = spiderElement.offsetHeight;
+const spiderWidth = spiderElement.offsetWidth;
+
+const positionTop = (wallHeight - spiderHeight) / 2;
+const positionLeft = (wallWidth - spiderWidth) / 2;
+
+spiderElement.style.top = positionTop + 'px';
+spiderElement.style.left = positionLeft + 'px';
