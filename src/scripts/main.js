2958d6c81801fdc41e7f09588e1c63c1beb1e2c3
Anastasia <ahinata1717@gmail.com>
2025-11-13 13:55:53 +0200

    fix vertical positioning

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,7 @@ const spider = document.querySelector('.spider');
 
 function centerItem() {
   spider.style.left = wall.clientWidth / 2 - spider.clientWidth / 2 + 'px';
-  spider.style.top = wall.clientWidth / 2 - spider.clientWidth / 2 + 'px';
+  spider.style.top = wall.clientHeight / 2 - spider.clientHeight / 2 + 'px';
 }
 
 centerItem();
