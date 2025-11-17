bb882746536efa9a88abfc8718116740778c2811
Olga Korynenko <olga.korynenko@gmail.com>
2025-11-17 21:01:55 +0400

    create js file

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const wallElem = document.querySelector('.wall');
+const spiderElem = wallElem.querySelector('.spider');
+
+const wallCoords = wallElem.getBoundingClientRect();
+
+const spiderCoords = spiderElem.getBoundingClientRect();
+
+const wallLeft = (wallCoords.width - spiderCoords.width) / 2;
+
+const wallTop = (wallCoords.height - spiderCoords.height) / 2;
+
+spiderElem.style.top = wallTop + 'px';
+spiderElem.style.left = wallLeft + 'px';
