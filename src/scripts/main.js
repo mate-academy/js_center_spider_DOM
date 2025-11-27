dac4c9adc73d86a588543a644a8fbf7b35452380
Dmytro Odnoroh <falcon121299@icloud.com>
2025-11-27 20:20:23 +0200

    sol by topo

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const width = wall.clientWidth / 2 - spider.offsetWidth / 2;
+const height = wall.clientHeight / 2 - spider.offsetHeight / 2;
+
+wall.style.position = 'relative';
+spider.style.position = 'absolute';
+spider.style.left = width + 'px';
+spider.style.top = height + 'px';
