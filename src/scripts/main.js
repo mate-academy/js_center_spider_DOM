3cd9e626078a0b5e0db41b91a863fb652526dbad
Tymofii <zrozik@MacBook-Air-Tymofii.local>
2025-11-28 17:58:53 +0200

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = wall.children[0];
+
+spider.style.position = 'absolute';
+
+const centerX = (wall.offsetWidth - spider.offsetWidth) / 2;
+const centerY = (wall.offsetHeight - spider.offsetHeight) / 2;
+
+spider.style.left = `${centerX}px`;
+spider.style.top = `${centerY}px`;
