4c71d468b90e85a5bdcdf4043bd14a251086cd63
DevT <dev.tymofiichuk@gmail.com>
2026-01-12 17:59:19 +0200

    Center the spider task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
+
+const spiderCenterX = Math.round(wallWidth / 2) - Math.round(spiderWidth / 2);
+const spiderCenterY = Math.round(wallHeight / 2) - Math.round(spiderHeight / 2);
+
+spider.style.left = spiderCenterX + 'px';
+spider.style.top = spiderCenterY + 'px';
