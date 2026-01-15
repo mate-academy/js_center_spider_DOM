b4f2ce0532c957b45a96f085c76405090d35b537
Ihor Haidai <igayday74@gmail.com>
2026-01-15 11:07:50 -0500

    Add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+// Робимо павука позиціонованим елементом
+spider.style.position = 'absolute';
+
+// Обчислюємо центр
+const centerX = (wall.clientWidth - spider.clientWidth) / 2;
+const centerY = (wall.clientHeight - spider.clientHeight) / 2;
+
+// Встановлюємо координати
+spider.style.left = `${centerX}px`;
+spider.style.top = `${centerY}px`;
