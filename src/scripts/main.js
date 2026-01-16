30df54af2db3f8e38a6f80f3be7875033140355e
shymkivvasyl <shymkiv.vasyl333@gmail.com>
2026-01-16 16:47:41 +0100

    centter spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
+/* eslint-disable no-unused-vars */
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const wallRect = wall.getBoundingClientRect();
+const spiderRect = spider.getBoundingClientRect();
+
+const padding = 10;
+
+const positionX =
+  wallRect.left +
+  window.pageXOffset +
+  10 +
+  wall.clientWidth / 2 -
+  spiderRect.width / 2;
+
+const positionY =
+  wallRect.top +
+  window.pageYOffset +
+  10 +
+  wall.clientHeight / 2 -
+  spiderRect.height / 2;
+
+spider.style.position = 'fixed';
+spider.style.left = `${positionX}px`;
+spider.style.top = `${positionY}px`;
