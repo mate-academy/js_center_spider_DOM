7ac4266d7230e6fc48fcb2935bcbf9572a4e532e
shymkivvasyl <shymkiv.vasyl333@gmail.com>
2026-01-16 16:55:06 +0100

    fixed mistake

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,22 +6,18 @@ const spider = document.querySelector('.spider');
 const wallRect = wall.getBoundingClientRect();
 const spiderRect = spider.getBoundingClientRect();
 
-const padding = 10;
-
 const positionX =
   wallRect.left +
   window.pageXOffset +
-  10 +
   wall.clientWidth / 2 -
   spiderRect.width / 2;
 
 const positionY =
   wallRect.top +
   window.pageYOffset +
-  10 +
   wall.clientHeight / 2 -
   spiderRect.height / 2;
 
-spider.style.position = 'fixed';
+spider.style.position = 'absolute';
 spider.style.left = `${positionX}px`;
 spider.style.top = `${positionY}px`;
