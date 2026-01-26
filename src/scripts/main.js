788b2fbb66f839ea552ba993607d356facaec9dc
unknown <dkuz6323@gmail.com>
2026-01-26 19:04:55 +0200

    completed tasks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
 // write code here
+
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const x = (wall.offsetWidth - spider.offsetWidth) / 2;
+const y = (wall.offsetHeight - spider.offsetHeight) / 2;
+
+spider.style.position = 'absolute';
+spider.style.top = y + 'px';
+spider.style.left = x + 'px';
