1740d227e61e570b4a755d5b56e0066207426203
Illia Safonov <safonov.illia@gmail.com>
2026-01-26 18:49:53 +0100

    spider_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,16 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-spider.style.top = '50%';
-spider.style.left = '50%';
-spider.style.transform = 'translate(-50%, -50%)';
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
+
+const topPosition = wallHeight / 2 - spiderHeight / 2;
+const leftPosition = wallWidth / 2 - spiderWidth / 2;
+
+spider.style.top = `${topPosition}px`;
+spider.style.left = `${leftPosition}px`;
