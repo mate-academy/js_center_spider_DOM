fa09cbed7f369ce4da5abcfc32c82f8e40ef24c4
Olena Dolna <dolnaolena2019@gmail.com>
2026-02-02 16:33:47 +0100

    offset dimensions used

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,12 @@
 'use strict';
 
 const spider = document.querySelector('.spider');
-const spiderStyles = getComputedStyle(spider);
 
 const spiderTop =
-  spider.offsetParent.clientHeight / 2 - parseFloat(spiderStyles.height) / 2;
+  spider.offsetParent.clientHeight / 2 - parseFloat(spider.offsetHeight) / 2;
 
 const spiderLeft =
-  spider.offsetParent.clientWidth / 2 - parseFloat(spiderStyles.width) / 2;
+  spider.offsetParent.clientWidth / 2 - parseFloat(spider.offsetWidth) / 2;
 
 spider.style.top = spiderTop + 'px';
 spider.style.left = spiderLeft + 'px';
