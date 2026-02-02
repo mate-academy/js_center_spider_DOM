f1b9ccaffe5b26edb4ebc65af2ec012dc03940ec
Olena Dolna <dolnaolena2019@gmail.com>
2026-02-02 16:37:49 +0100

    removed type conversion

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,10 +3,10 @@
 const spider = document.querySelector('.spider');
 
 const spiderTop =
-  spider.offsetParent.clientHeight / 2 - parseFloat(spider.offsetHeight) / 2;
+  spider.offsetParent.clientHeight / 2 - spider.offsetHeight / 2;
 
 const spiderLeft =
-  spider.offsetParent.clientWidth / 2 - parseFloat(spider.offsetWidth) / 2;
+  spider.offsetParent.clientWidth / 2 - spider.offsetWidth / 2;
 
 spider.style.top = spiderTop + 'px';
 spider.style.left = spiderLeft + 'px';
