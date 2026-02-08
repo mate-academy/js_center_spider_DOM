49a068ca1d58e6abbaa26aaaf2e9f64ffaff2883
Ivanov Ihor <i.ivanovvv21122006@gmail.com>
2026-02-08 23:16:26 +0200

    add task solutiom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 // write code here
+const field = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+field.style.position = 'relative';
+spider.style.position = 'absolute';
+
+const topCenter = field.clientHeight / 2 - spider.clientHeight / 2;
+const leftCenter = field.clientWidth / 2 - spider.clientWidth / 2;
+
+spider.style.top = `${topCenter}px`;
+spider.style.left = `${leftCenter}px`;
