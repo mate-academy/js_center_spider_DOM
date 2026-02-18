56fd7eb453fc44307407a6e1153051a4961ac72e
GabrielSpelchuk <gabrielspelciuc24@gmail.com>
2026-02-18 12:27:29 +0200

    add DOMContentLoaded and verification if wall ond spider not null or undefined

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,21 @@
 
 // write code here
 
-const wall = document.querySelector('.wall');
-const spider = document.querySelector('.spider');
+document.addEventListener('DOMContentLoaded', () => {
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
 
-const wallHeight = wall.clientHeight;
-const wallWidth = wall.clientWidth;
+  if (!wall && !spider) {
+    return;
+  }
 
-const spiderHeight = spider.clientHeight;
-const spiderWidth = spider.clientWidth;
+  const wallHeight = wall.clientHeight;
+  const wallWidth = wall.clientWidth;
 
-spider.style.top = `${wallHeight / 2 - spiderHeight / 2}px`;
-spider.style.left = `${wallWidth / 2 - spiderWidth / 2}px`;
+  const spiderHeight = spider.clientHeight;
+  const spiderWidth = spider.clientWidth;
+
+  spider.style.position = 'absolute';
+  spider.style.top = `${wallHeight / 2 - spiderHeight / 2}px`;
+  spider.style.left = `${wallWidth / 2 - spiderWidth / 2}px`;
+});
