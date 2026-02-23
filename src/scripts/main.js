7c42b1491dec880f8ec5b0c660586a2941819e91
yuliana111007-debug <yuliana111007@gmail.com>
2026-02-23 23:34:50 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
+
+const calcLeft = (wallWidth - spiderWidth) / 2;
+const calcTop = (wallHeight - spiderHeight) / 2;
+
+spider.style.left = `${calcLeft}px`;
+spider.style.top = `${calcTop}px`;
