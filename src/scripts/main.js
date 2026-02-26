a4b668752450ea0bc8e5d3f356885a9fc7c8c17a
Teti <rohatetiana@gmail.com>
2026-02-26 17:05:23 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+if (!wall || !spider) {
+  throw new Error('Wall or spider not found');
+}
+
+function debounce(callback, delay) {
+  let timeoutId;
+
+  return (...args) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback(...args), delay);
+  };
+}
+
+function centerSpider() {
+  spider.style.left = `${wall.clientWidth / 2 - spider.offsetWidth / 2}px`;
+  spider.style.top = `${wall.clientHeight / 2 - spider.offsetHeight / 2}px`;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  centerSpider();
+  spider.addEventListener('load', centerSpider);
+});
+
+window.addEventListener('resize', debounce(centerSpider, 100));
