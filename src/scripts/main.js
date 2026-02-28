e3dd5b8102a8ea0234e2053dcd14ae15a26fad65
Volodymyr Shuper <vshuper@gmail.com>
2026-02-28 14:55:01 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
 // write code here
+const spider = document.querySelector('.spider');
+
+spider.style.transform = 'translate(-50%, -50%)';
+spider.style.position = 'absolute';
+spider.style.top = '50%';
+spider.style.left = '50%';
