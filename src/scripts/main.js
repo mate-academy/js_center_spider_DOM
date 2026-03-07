fe2ef66408498012333be05a23041fb4bb9387f6
Anzhela <fcku3734@gmail.com>
2026-03-07 17:06:13 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+/** розміри стіни */
+const wallWidth = wall.offsetWidth;
+const wallHeight = wall.offsetHeight;
+
+/** розміри павука */
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
+
+/** центр по горизонталі та вертикалі */
+const leftPosition = (wallWidth - spiderWidth) / 2;
+const topPosition = (wallHeight - spiderHeight) / 2;
+
+spider.style.position = 'absolute';
+spider.style.left = leftPosition + 'px';
+spider.style.top = topPosition + 'px';
