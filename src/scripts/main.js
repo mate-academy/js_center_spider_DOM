08f7cc4e61a82b5017ecb3f65d16869805018a04
Anzhela <fcku3734@gmail.com>
2026-03-07 17:36:20 +0200

    change of decision

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,12 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
+wall.style.position = 'relative';
+spider.style.position = 'absolute';
+
 /** розміри стіни */
-const wallWidth = wall.offsetWidth;
-const wallHeight = wall.offsetHeight;
+const wallWidth = spider.offsetParent.clientWidth;
+const wallHeight = spider.offsetParent.clientHeight;
 
 /** розміри павука */
 const spiderWidth = spider.offsetWidth;
@@ -14,9 +17,6 @@ const spiderHeight = spider.offsetHeight;
 /** центр по горизонталі та вертикалі */
 const leftPosition = (wallWidth - spiderWidth) / 2;
 const topPosition = (wallHeight - spiderHeight) / 2;
-
-wall.style.position = 'relative';
-spider.style.position = 'absolute';
 
 spider.style.left = leftPosition + 'px';
 spider.style.top = topPosition + 'px';
