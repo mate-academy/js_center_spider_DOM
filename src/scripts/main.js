287c2add1079c4b357ad5f592b4d72b2a8d6632b
Taras Pidkuimukha <taraspidkuimukha@gmail.com>
2026-03-14 16:46:47 +0100

    feat: the spider is centered

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+spider.style.position = 'absolute';
+wall.style.position = 'relative';
+
+const spiderWidth = spider.clientWidth;
+const spiderHeight = spider.clientHeight;
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+
+const leftPos = (wallWidth - spiderWidth) / 2;
+const topPos = (wallHeight - spiderHeight) / 2;
+
+spider.style.left = `${leftPos}px`;
+spider.style.top = `${topPos}px`;
