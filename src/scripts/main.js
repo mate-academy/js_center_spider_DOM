2625b7b366701a252c0d04fa4e7ff7537098ef11
Taras Yelahin <inflades@gmail.com>
2026-03-15 14:57:26 +0200

    dom_center_spider_task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+function centerSpider() {
+  const wall = document.querySelector('.wall');
+
+  wall.style.position = 'relative';
+
+  const spider = document.querySelector('.spider');
+
+  spider.style.position = 'absolute';
+
+  const reactWall = wall.getBoundingClientRect();
+  const reactSpider = spider.getBoundingClientRect();
+
+  const centerX = reactWall.width / 2;
+  const centerY = reactWall.height / 2;
+
+  const x = centerX - reactSpider.width / 2;
+  const y = centerY - reactSpider.height / 2;
+
+  spider.style.left = `${x}px`;
+  spider.style.top = `${y}px`;
+}
+
+centerSpider();
