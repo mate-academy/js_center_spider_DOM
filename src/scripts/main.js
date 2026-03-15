db848004a0befa640ae6beb688bb76ae5d45b1d4
fedechkova <fedechko.anya@gmail.com>
2026-03-15 18:47:29 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+function moveSpider() {
+  const wallRect = wall.getBoundingClientRect();
+  const spiderRect = spider.getBoundingClientRect();
+
+  const x = (wallRect.width - spiderRect.width) / 2;
+  const y = (wallRect.height - spiderRect.height) / 2;
+
+  spider.style.transform = `translate(${x}px, ${y}px)`;
+}
+
+moveSpider();
