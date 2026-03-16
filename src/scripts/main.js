0e11726bb205fab1f7d1b10b8dafba6ddc58ac22
Taras Demianiuk <demtaras1@gamil.com>
2026-03-16 16:23:44 +0200

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const wall = document.getElementsByClassName('wall')[0];
+const spider = document.getElementsByClassName('spider')[0];
+
+const centerX = wall.clientWidth / 2 - spider.clientWidth / 2;
+const centerY = wall.clientHeight / 2 - spider.clientHeight / 2;
+
+spider.style.left = centerX + 'px';
+spider.style.top = centerY + 'px';
