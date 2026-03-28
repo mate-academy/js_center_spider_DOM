35f1aa528c2c6add81c431ba755cf2d42d18141f
Valentyna Bahyrly <valentynabahyrly@gmail.com>
2026-03-28 20:47:15 +0200

    Fixed: clientWidth, clientHeight

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,11 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-const wallWidth = wall.offsetWidth;
-const wallHeight = wall.offsetHeight;
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
 
-const spiderWidth = spider.offsetWidth;
-const spiderHeight = spider.offsetHeight;
+const spiderWidth = spider.clientWidth;
+const spiderHeight = spider.clientHeight;
 
 spider.style.left = wallWidth / 2 - spiderWidth / 2 + 'px';
 spider.style.top = wallHeight / 2 - spiderHeight / 2 + 'px';
