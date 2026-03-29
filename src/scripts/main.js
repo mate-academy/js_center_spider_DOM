344bbd14141ca25c5dd4cb71a633a0ef1789708d
Valentyna Bahyrly <valentynabahyrly@gmail.com>
2026-03-29 09:01:32 +0300

    added spider.style.position = absolute;

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,5 +9,7 @@ const wallHeight = wall.clientHeight;
 const spiderWidth = spider.clientWidth;
 const spiderHeight = spider.clientHeight;
 
+spider.style.position = 'absolute';
+
 spider.style.left = wallWidth / 2 - spiderWidth / 2 + 'px';
 spider.style.top = wallHeight / 2 - spiderHeight / 2 + 'px';
