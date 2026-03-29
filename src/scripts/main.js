4b3a28ba4d6a953f2aa840c20ed5749e160f711e
Valentyna Bahyrly <valentynabahyrly@gmail.com>
2026-03-29 09:06:15 +0300

    changed offsetWidth

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,11 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-const wallWidth = wall.clientWidth;
-const wallHeight = wall.clientHeight;
+const wallWidth = wall.offsetWidth;
+const wallHeight = wall.offsetHeight;
 
-const spiderWidth = spider.clientWidth;
-const spiderHeight = spider.clientHeight;
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
 
 spider.style.position = 'absolute';
 
