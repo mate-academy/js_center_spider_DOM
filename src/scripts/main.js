29990d1f99d7b11aaf7b4d9d8a0427b13abeec5e
Banderos14 <anton.shyshenko@gmail.com>
2026-04-03 11:27:30 +0200

    feat: task_solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const widthWall = wall.clientWidth;
+const widthSpider = spider.clientWidth;
+
+const heightWall = wall.clientHeight;
+const heightSpider = spider.clientHeight;
+
+const leftPosition = (widthWall - widthSpider) / 2;
+const topPosition = (heightWall - heightSpider) / 2;
+
+spider.style.position = 'absolute';
+spider.style.left = `${leftPosition}px`;
+spider.style.top = `${topPosition}px`;
