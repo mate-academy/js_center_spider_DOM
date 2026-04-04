65019ffe0a7f595c81de41554ea5f0da3a0f9dc8
Ihor Tutak <igor2tak2212@gmail.com>
2026-04-04 17:05:38 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,12 @@
 
 const spider = document.querySelector('.spider');
 const wall = document.querySelector('.wall');
-const wallRect = wall.getBoundingClientRect();
-const spiderRect = spider.getBoundingClientRect();
-const x = (wallRect.width - spiderRect.width) / 2;
-const y = (wallRect.height - spiderRect.height) / 2;
+const spiderWidth = spider.clientWidth;
+const spiderHeight = spider.clientHeight;
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+const x = (wallWidth - spiderWidth) / 2;
+const y = (wallHeight - spiderHeight) / 2;
 
 spider.style.top = `${y}px`;
 spider.style.left = `${x}px`;
