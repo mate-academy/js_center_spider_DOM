3f45094a9c8555125c03c853d1d320a439660e09
Bohdan <felbohdan@gmail.com>
2026-04-11 22:59:52 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,10 @@
 
 const spider = document.querySelector('.spider');
 const wall = document.querySelector('.wall');
-const wallWidth = wall.offsetWidth - wall.clientLeft * 2;
 
-spider.style.top = wallWidth / 2 - spider.offsetHeight / 2 + 'px';
-spider.style.left = wallWidth / 2 - spider.offsetHeight / 2 + 'px';
+if (wall) {
+  const wallWidth = wall.clientWidth;
+
+  spider.style.top = wallWidth / 2 - spider.offsetHeight / 2 + 'px';
+  spider.style.left = wallWidth / 2 - spider.offsetHeight / 2 + 'px';
+}
