6968241bb92bc5cf81074e21a2ee00fda87607c9
Natalia Lunkevych <gadyka08@gmail.com>
2026-04-11 19:03:28 +0300

    uygy

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,15 +2,14 @@
 // викликаємо функцію, коли вікно повністю завантажиться (всі картинки на місці)
 
 window.addEventListener('load', () => {
-  // вимірюємо поточну ширину та висоту елементів (враховуючи відступи)
-const wall = document.querySelector('.wall');
-const spider = document.querySelector('.spider');
-const spiderWidth = spider.offsetWidth;
-const spiderHeight = spider.offsetHeight;
-const wallWidth = wall.offsetWidth;
-const wallHeight = wall.offsetHeight;
+  // СПОЧАТКУ знаходимо елементи
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
 
-  if (!wall || !spider) return;
+  // ПІСЛЯ ЦЬОГО робимо перевірку
+  if (!wall || !spider) {
+    return;
+  }
   // перевірка наявності
   // встановлюємо правила позиціонування:
   wall.style.position = 'relative';
@@ -19,6 +18,12 @@ const wallHeight = wall.offsetHeight;
   spider.style.position = 'absolute';
   // рахуємо координати за формулою: (РозмірСтіни - РозмірПавука) / 2
   // щоб залишити рівну кількість вільного місця з обох боків
+
+  // вимірюємо поточну ширину та висоту елементів (враховуючи відступи)
+  const spiderWidth = spider.offsetWidth;
+  const spiderHeight = spider.offsetHeight;
+  const wallWidth = wall.offsetWidth;
+  const wallHeight = wall.offsetHeight;
 
   const left = (wallWidth - spiderWidth) / 2;
   const topPos = (wallHeight - spiderHeight) / 2;
