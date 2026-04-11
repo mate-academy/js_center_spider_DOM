b0bbeec82fa41dd5264c16b3a13b24aedba40655
Natalia Lunkevych <gadyka08@gmail.com>
2026-04-11 18:58:46 +0300

    fixed after lint

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 'use strict';
 // викликаємо функцію, коли вікно повністю завантажиться (всі картинки на місці)
+
 window.addEventListener('load', () => {
   // вимірюємо поточну ширину та висоту елементів (враховуючи відступи)
 const wall = document.querySelector('.wall');
@@ -8,16 +9,22 @@ const spiderWidth = spider.offsetWidth;
 const spiderHeight = spider.offsetHeight;
 const wallWidth = wall.offsetWidth;
 const wallHeight = wall.offsetHeight;
-if (!wall || !spider) return;     // перевірка наявності
-// встановлюємо правила позиціонування:
-wall.style.position = 'relative'; // робимо стіну контейнером, відносно якої пересув.павук
-spider.style.position = 'absolute';
-// рахуємо координати за формулою: (РозмірСтіни - РозмірПавука) / 2
-// щоб залишити рівну кількість вільного місця з обох боків
-const left = (wallWidth - spiderWidth) / 2;
-const top = (wallHeight - spiderHeight) / 2;
-// записуємо розраховані значення у CSS властивості павука
-// додаємо 'px', щоб браузер розумів одиниці виміру
-spider.style.left = `${left}px`;
-spider.style.top = `${top}px`;
+
+  if (!wall || !spider) return;
+  // перевірка наявності
+  // встановлюємо правила позиціонування:
+  wall.style.position = 'relative';
+
+  // робимо стіну контейнером, відносно якої пересув.павук
+  spider.style.position = 'absolute';
+  // рахуємо координати за формулою: (РозмірСтіни - РозмірПавука) / 2
+  // щоб залишити рівну кількість вільного місця з обох боків
+
+  const left = (wallWidth - spiderWidth) / 2;
+  const topPos = (wallHeight - spiderHeight) / 2;
+  // записуємо розраховані значення у CSS властивості павука
+  // додаємо 'px', щоб браузер розумів одиниці виміру
+
+  spider.style.left = `${left}px`;
+  spider.style.top = `${topPos}px`;
 });
