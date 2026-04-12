19913d401f8ef66de35a60deb04fd28128f3586a
Maksym <maxim.golya@gmail.com>
2026-04-12 19:20:32 +0300

    added requested changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,14 +3,16 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-const wallWidth = wall.clientWidth;
-const wallHeight = wall.clientHeight;
-const spiderWidth = spider.clientWidth;
-const spiderHeight = spider.clientHeight;
+window.addEventListener('load', () => {
+  const wallWidth = wall.clientWidth;
+  const wallHeight = wall.clientHeight;
+  const spiderWidth = spider.clientWidth;
+  const spiderHeight = spider.clientHeight;
 
-const centerX = wallWidth / 2 - spiderWidth / 2;
-const centerY = wallHeight / 2 - spiderHeight / 2;
+  const centerX = wallWidth / 2 - spiderWidth / 2;
+  const centerY = wallHeight / 2 - spiderHeight / 2;
 
-spider.style.position = 'absolute';
-spider.style.left = centerX + 'px';
-spider.style.top = centerY + 'px';
+  spider.style.position = 'absolute';
+  spider.style.left = centerX + 'px';
+  spider.style.top = centerY + 'px';
+});
