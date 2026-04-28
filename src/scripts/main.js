5e9799e70727f08c19ad0c90f45f623bec50fa70
Maksym <zubchenko.2001@gmail.com>
2026-04-28 14:37:25 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+function spiderCenter(field, img) {
+  const fieldWidth = field.offsetWidth;
+  const fieldHeight = field.offsetHeight;
+
+  const imgWidth = img.offsetWidth;
+  const imgHeight = img.offsetHeight;
+
+  const imgLeft = (fieldWidth - imgWidth) / 2 - 10;
+  const imgTop = (fieldHeight - imgHeight) / 2 - 10;
+
+  img.style.left = imgLeft + 'px';
+  img.style.top = imgTop + 'px';
+}
+
+spiderCenter(wall, spider);
