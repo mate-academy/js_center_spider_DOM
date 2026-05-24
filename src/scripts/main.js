87cfbf99beaf2f285d73c8b4f88502c5339ae1f0
Vital <vitaligna@gmail.com>
2026-05-24 09:34:42 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+const ourWall = document.querySelector('.wall');
+const ourSpider = document.querySelector('.spider');
+const ourLeft = ourWall.clientWidth / 2 - ourSpider.clientWidth / 2;
+const ourTop = ourWall.clientHeight / 2 - ourSpider.clientHeight / 2;
+
+ourSpider.style.top = `${ourTop}px`;
+ourSpider.style.left = `${ourLeft}px`;
