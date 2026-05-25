b382a016ba41e87333fb2e6e051de209b69cde22
Tatiana Makohon (Palamarchuk) <tatiana@MacBook-Pro-Tatiana.local>
2026-05-25 12:35:16 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  const wallStyle = getComputedStyle(wall);
+  const paddingLeft = parseFloat(wallStyle.paddingLeft);
+  const paddingTop = parseFloat(wallStyle.paddingTop);
+
+  const wallInnerWidth =
+    wall.clientWidth - paddingLeft - parseFloat(wallStyle.paddingRight);
+  const wallInnerHeight =
+    wall.clientHeight - paddingTop - parseFloat(wallStyle.paddingBottom);
+
+  const centerLeft = paddingLeft + (wallInnerWidth - spider.offsetWidth) / 2;
+  const centerTop = paddingTop + (wallInnerHeight - spider.offsetHeight) / 2;
+
+  spider.style.position = 'absolute';
+  spider.style.left = `${centerLeft}px`;
+  spider.style.top = `${centerTop}px`;
+});
