6df54599fcbacf317558d3ddeeb1dedf88f4f009
Mykhailo Oliinyk <mykhailo.oliinyk9@gmail.com>
2026-06-01 19:36:46 +0300

    fixed solution 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,12 +3,20 @@
 // write code here
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
+// const wallStyles = getComputedStyle(wall);
+// const paddingLeft = parseFloat(wallStyles.paddingLeft);
+// const paddingTop = parseFloat(wallStyles.paddingTop);
 
 wall.style.position = 'relative';
 spider.style.position = 'absolute';
 
-const leftPos = (wall.offsetWidth - spider.offsetWidth) / 2;
-const topPos = (wall.offsetHeight - spider.offsetHeight) / 2;
+// const leftPos =
+//  wall.offsetLeft + paddingLeft + (wall.clientWidth - spider.offsetWidth) / 2;
+// const topPos =
+//  wall.offsetTop + paddingTop + (wall.clientHeight - spider.offsetHeight) / 2;
 
-spider.style.left = `${leftPos}px`;
-spider.style.top = `${topPos}px`;
+// spider.style.left = `${leftPos}px`;
+// spider.style.top = `${topPos}px`;
+
+spider.style.left = `${(wall.clientWidth - spider.offsetWidth) / 2}px`;
+spider.style.top = `${(wall.clientHeight - spider.offsetHeight) / 2}px`;
