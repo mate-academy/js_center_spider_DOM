6f8149ea444d1e51ebdb59c9978c13e998ff6cb4
ILLYA SMYK <smykillya@gmail.com>
2026-06-04 23:11:48 +0200

    add task solution2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,19 +3,9 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-const wallOffsetWidth = wall.offsetWidth;
-const wallOffsetHeight = wall.offsetHeight;
-const wallStyles = getComputedStyle(wall);
-const borderLeft = parseInt(wallStyles.borderLeftWidth, 10);
+const spiderLeft = (wall.clientWidth - spider.offsetWidth) / 2;
 
-const borderTop = parseInt(wallStyles.borderTopWidth, 10);
-
-const spiderOffsetWidth = spider.offsetWidth;
-const spiderOffsetHeight = spider.offsetHeight;
-
-const spiderLeft = wallOffsetWidth / 2 - spiderOffsetWidth / 2 - borderLeft;
-
-const spiderTop = wallOffsetHeight / 2 - spiderOffsetHeight / 2 - borderTop;
+const spiderTop = (wall.clientHeight - spider.offsetHeight) / 2;
 
 spider.style.top = `${spiderTop}px`;
 spider.style.left = `${spiderLeft}px`;
