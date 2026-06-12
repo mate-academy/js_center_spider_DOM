006cdaf99189e26a6a930c9c963c30786985e9aa
interpunktus <240490702+interpunktus@users.noreply.github.com>
2026-06-12 21:26:11 +0300

    try2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,12 @@
 'use strict';
 
-const spider = document.querySelector('.spider');
+window.addEventListener('DOMContentLoaded', () => {
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
 
-spider.style.position = 'absolute';
-spider.style.left = '50%';
-spider.style.top = '50%';
-spider.style.transform = 'translate(-50%, -50%)';
+  const left = (wall.clientWidth - spider.clientWidth) / 2;
+  const topp = (wall.clientHeight - spider.clientHeight) / 2;
+
+  spider.style.left = `${left}px`;
+  spider.style.top = `${topp}px`;
+});
