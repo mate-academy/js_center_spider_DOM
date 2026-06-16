1bafa74e3dc02f14640657e954514d175c2852da
Tendeeer <leifisisidj747@gmail.com>
2026-06-16 20:56:01 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,9 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
+wall.style.position = 'relative';
 spider.style.position = 'absolute';
-spider.style.left = '50%';
-spider.style.top = '50%';
-spider.style.transform = 'translate(-50%, -50%)';
+spider.style.left = `${(wall.clientWidth - spider.clientWidth) / 2}px`;
+spider.style.top = `${(wall.clientHeight - spider.clientHeight) / 2}px`;
 
 wall.append(spider);
