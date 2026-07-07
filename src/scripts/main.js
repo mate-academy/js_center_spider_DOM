7131bfbc94ab272952ab19d3bc6f3e5c462a19e9
ptrodion <rdnpetrenko@gmail.com>
2026-07-07 22:45:48 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  if (!wall || !spider) {
+    return;
+  }
+
+  const wallWidth = wall.clientWidth;
+  const wallHeight = wall.clientHeight;
+
+  const spiderWidth = spider.offsetWidth;
+  const spiderHeight = spider.offsetHeight;
+
+  const leftPosition = (wallWidth - spiderWidth) / 2;
+  const topPosition = (wallHeight - spiderHeight) / 2;
+
+  spider.style.left = `${leftPosition}px`;
+  spider.style.top = `${topPosition}px`;
+});
