5543e1e595f3b42ec5606c71a0e4d23041bb3533
Mariana <zh.yu.st22@gmail.com>
2026-07-12 23:10:14 +0300

    added 2 new values

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,12 +3,17 @@
 // write code here
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
+const wallStyles = getComputedStyle(wall);
+const borderLeftRight =
+  wallStyles.borderRightWidth + wallStyles.borderLeftWidth;
+const borderTopBottom =
+  wallStyles.borderTopWidth + wallStyles.borderBottomWidth;
 const wallWidth = wall.offsetWidth;
 const wallHeight = wall.offsetHeight;
 const wightSpider = spider.offsetWidth;
 const heightSpider = spider.offsetHeight;
-const centerWidth = (wallWidth - wightSpider - 20) / 2;
-const centerHeight = (wallHeight - heightSpider - 20) / 2;
+const centerWidth = (wallWidth - wightSpider - borderLeftRight) / 2;
+const centerHeight = (wallHeight - heightSpider - borderTopBottom) / 2;
 
 spider.style.left = centerWidth + 'px';
 spider.style.top = centerHeight + 'px';
