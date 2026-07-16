582555c104ca8d38eeee69b9d3e0298f0de44cb2
kateryna Habchak <kvizovapidtrimka@gmail.com>
2026-07-16 22:16:21 +0200

    i have done changes in strict number 4 add .

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const wallWidth = wall.offsetWidth;
+const wallHeight = wall.offsetHeight;
+
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
+
+const leftPosition = (wallWidth - spiderWidth) / 2;
+const topPosition = (wallHeight - spiderHeight) / 2;
+
+spider.style.left = `${leftPosition}px`;
+spider.style.top = `${topPosition}px`;
