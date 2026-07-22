f6a7b728a303c4a08f1874afb2e7052e1a2eb8ce
Andrew <a.tupko98>
2026-07-22 09:46:13 +0200

    fix2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,11 @@
 
 // write code here
 const div = document.querySelector('.wall');
-const spider = document.querySelector('img');
+const spider = document.querySelector('.spider');
 
 const divWidth = div.offsetWidth;
 const divHeight = div.offsetHeight;
+
 const spiderWidth = spider.offsetWidth;
 const spiderHeight = spider.offsetHeight;
 
@@ -14,5 +15,6 @@ const topPosition = (divHeight - spiderHeight) / 2;
 
 div.style.position = 'relative';
 spider.style.position = 'absolute';
+
 spider.style.left = left + 'px';
 spider.style.top = topPosition + 'px';
