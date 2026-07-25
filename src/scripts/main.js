02efe6193546be5f66b29b01505604d4ec633a58
Danyliuk Yaroslav <unityismylove@gmail.com>
2026-07-25 17:54:05 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall').getBoundingClientRect();
+const spider = document.querySelector('.spider');
+const spiderRect = spider.getBoundingClientRect();
+
+spider.style.left = (wall.width - spiderRect.width) / 2 + 'px';
+spider.style.top = (wall.height - spiderRect.height) / 2 + 'px';
