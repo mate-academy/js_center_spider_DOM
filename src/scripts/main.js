ea594f05ba2b67d07c9776e8e81ada0b7c4a55fc
Yehor Mishchenko <130080322+myehor@users.noreply.github.com>
2026-07-27 14:26:02 +0200

    added changes to the variables

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,9 +5,9 @@ const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
 const wallWidth = wall.clientWidth;
-const wallHeight = wall.clientWidth;
+const wallHeight = wall.clientHeight;
 const spiderWidth = spider.clientWidth;
-const spiderHeight = spider.clientWidth;
+const spiderHeight = spider.clientHeight;
 const topPosition = (wallHeight - spiderHeight) / 2;
 const leftPosition = (wallWidth - spiderWidth) / 2;
 
