972c31f0d47964b899891e7208608146f2b9a8cd
Danyliuk Yaroslav <unityismylove@gmail.com>
2026-07-27 13:37:27 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,10 +4,10 @@ const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 const wallRect = wall.getBoundingClientRect();
 const spiderRect = spider.getBoundingClientRect();
-const bord = getComputedStyle(wall).borderWidth;
+const border = getComputedStyle(wall).borderWidth;
 
 spider.style.left =
-  (wallRect.width - spiderRect.width) / 2 - parseInt(bord) + 'px';
+  (wallRect.width - spiderRect.width) / 2 - parseInt(border) + 'px';
 
 spider.style.top =
-  (wallRect.height - spiderRect.height) / 2 - parseInt(bord) + 'px';
+  (wallRect.height - spiderRect.height) / 2 - parseInt(border) + 'px';
