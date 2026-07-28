ea8ac3e5847073752804cfb3d433a94731953620
w11ue <https://github.com/Vlad99M>
2026-07-28 23:27:57 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+spider.style.position = 'absolute';
+
+const wallRect = wall.getBoundingClientRect();
+const spiderRect = spider.getBoundingClientRect();
+
+const x = (wallRect.width - spiderRect.width) / 2;
+const y = (wallRect.height - spiderRect.height) / 2;
+
+spider.style.left = x + 'px';
+spider.style.top = y + 'px';
