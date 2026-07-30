68105408c9e9217bb8029ed44fec920a97692166
w11ue <https://github.com/Vlad99M>
2026-07-30 23:53:53 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,12 +4,6 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-const wallRect = wall.getBoundingClientRect();
-const spiderRect = spider.getBoundingClientRect();
-
-const x = (wallRect.width - spiderRect.width) / 2;
-const y = (wallRect.height - spiderRect.height) / 2;
-
 spider.style.position = 'absolute';
-spider.style.left = x + 'px';
-spider.style.top = y + 'px';
+spider.style.left = `${(wall.clientWidth - spider.offsetWidth) / 2}px`;
+spider.style.top = `${(wall.clientHeight - spider.offsetHeight) / 2}px`;
