4532eabbe7fc4a8a609a433e8c71d41978d6a036
Alexander Okhotnychenko <shashaohota@gmail.com>
2026-07-30 16:07:49 +0300

    solution: center spider DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+function positionSpider(selection) {
+  const spider = document.querySelector(selection);
+
+  const data = {
+    type: 'absolute',
+    coords: {
+      top: '50%',
+      left: '50%',
+    },
+    transform: '-50% -50%',
+  };
+
+  spider.style.position = data.type;
+  spider.style.top = data.coords.top;
+  spider.style.left = data.coords.left;
+  spider.style.translate = data.transform;
+}
+
+positionSpider('.spider');
