700359e3938db18054f1d7bf72b519c3c19a79c0
Nik200309 <nikitapidlypnij@gmail.com>
2026-08-03 15:57:36 +0300

    Third sttempt

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,10 +4,9 @@
 const spider = document.querySelector('.spider');
 const wall = document.querySelector('.wall');
 
+const leftPoint = wall.clientHeight / 2 - spider.clientHeight / 2;
+const topPoint = wall.clientWidth / 2 - spider.clientHeight / 2;
 
-const leftPoint = (wall.offsetWidth - spider.offsetWidth) / 2;
-const topPoint = (wall.offsetHeight - spider.offsetHeight) / 2;
-
+spider.style.position = 'absolute';
 spider.style.top = topPoint + 'px';
 spider.style.left = leftPoint + 'px';
-
