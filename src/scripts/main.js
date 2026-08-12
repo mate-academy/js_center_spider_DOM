df383d3b64d552ec29504480be5e5b63445af74b
Yevheniia Biliaieva <biliaieva99@gmail.com>
2026-08-12 20:25:20 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+window.addEventListener('load', () => {
+  const spider = document.querySelector('.spider');
+  const field = document.querySelector('.wall');
+
+  const spiderWidth = spider.offsetWidth;
+  const fieldInnerWidth = field.clientWidth;
+  const spiderHeight = spider.offsetHeight;
+  const fieldInnerHeight = field.clientHeight;
+
+  const spiderTop = (fieldInnerHeight - spiderHeight) / 2;
+  const spiderLeft = (fieldInnerWidth - spiderWidth) / 2;
+
+  spider.style.top = spiderTop + 'px';
+  spider.style.left = spiderLeft + 'px';
+});
