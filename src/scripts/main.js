450bc26dba478689e5e2a202af15e3210ddc3041
Vitalii <vitaliy.doronin4@gmail.com>
2026-08-24 13:40:40 +0300

    Add a solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const spidey = document.querySelector('.spider');
+const walle = document.querySelector('.wall');
+
+function centreASpider(spider, wall) {
+  const spiderRect = spider.getBoundingClientRect();
+  const wallRect = wall.getBoundingClientRect();
+
+  const spiderCenterX = spiderRect.left + spiderRect.width / 2;
+  const spiderCenterY = spiderRect.top + spiderRect.height / 2;
+  const wallCenterX = wallRect.left + wallRect.width / 2;
+  const wallCenterY = wallRect.top + wallRect.height / 2;
+
+  const deltaX = wallCenterX - spiderCenterX;
+  const deltaY = wallCenterY - spiderCenterY;
+
+  spider.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
+}
+
+centreASpider(spidey, walle);
