7a90a43d93332e1eee0b1b2cf6eecccd9fbc11dc
Bohdan Kulbaka <bkulbaka1990@gmail.com>
2026-08-29 09:57:53 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,12 @@
 const parentElement = document.querySelector('.wall');
 const spiderElement = document.querySelector('.spider');
 
-parentElement.style.display = 'flex';
-parentElement.style.justifyContent = 'center';
-parentElement.style.alignItems = 'center';
+const parentWidth = parentElement.clientWidth;
+const spiderWidth = spiderElement.offsetWidth;
+const parentHeight = parentElement.clientHeight;
+const spiderHeight = spiderElement.offsetHeight;
+const leftPos = (parentWidth - spiderWidth) / 2;
+const topPos = (parentHeight - spiderHeight) / 2;
 
-spiderElement.style.position = 'relative';
+spiderElement.style.top = topPos + 'px';
+spiderElement.style.left = leftPos + 'px';
