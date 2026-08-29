32de1c81f38dd141d06f1efd1286e3b5ef0056b8
Anastasi Bielikova <anastasiabelikova39@gmail.com>
2026-08-30 01:11:42 +0300

    add

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const posLeft = (wall.clientWidth - spider.offsetWidth) / 2;
+const posTop = (wall.clientHeight - spider.offsetHeight) / 2;
+
+spider.style.left = `${posLeft}px`;
+spider.style.top = `${posTop}px`;
