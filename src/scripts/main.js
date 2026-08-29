2c67d20426f9ec3968d25960dcb2e1114e230f8a
Bohdan Kulbaka <bkulbaka1990@gmail.com>
2026-08-29 09:20:56 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const parentElement = document.querySelector('.wall');
+const spiderElement = document.querySelector('.spider');
+
+parentElement.style.display = 'flex';
+parentElement.style.justifyContent = 'center';
+parentElement.style.alignItems = 'center';
+
+spiderElement.style.position = 'relative';
