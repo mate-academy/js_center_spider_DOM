553fbeee90d88f96c1c1b27d655ff298f8ae8852
AchekcSs_ <achekcss@gmail.com>
2026-08-30 20:04:29 +0300

    feat: center spider with JavaScript

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,7 @@
 'use strict';
 
-// write code here
+const spiderElement = document.querySelector('.spider');
+const wallElement = document.querySelector('.wall');
+
+spiderElement.style.top = `${(wallElement.clientHeight - spiderElement.clientHeight) / 2}px`;
+spiderElement.style.left = `${(wallElement.clientWidth - spiderElement.clientWidth) / 2}px`;
