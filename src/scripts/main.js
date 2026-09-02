55b7ca134af67197a5d6b95126bf4604bac5d344
Your Name <you@example.com>
2026-09-02 22:01:24 +0300

    Solution_v2.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,12 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-spider.style.top = '50%';
-spider.style.left = '50%';
-spider.style.transform = 'translate(-50%, -50%)';
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
+
+spider.style.top = `${wallHeight / 2 - spiderHeight / 2}px`;
+spider.style.left = `${wallWidth / 2 - spiderWidth / 2}px`;
