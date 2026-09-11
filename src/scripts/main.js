af167d576154793f6c4bc1d8a42a060cccd5f4d5
jkumorek01 <jkumorek01@gmail.com>
2026-09-11 19:12:07 +0200

    Prepared spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const wall = spider.parentElement;
+
+function centerSpider() {
+  spider.style.left = `${(wall.offsetWidth - spider.offsetWidth) / 2}px`;
+  spider.style.top = `${(wall.offsetHeight - spider.offsetHeight) / 2}px`;
+}
+
+centerSpider();
