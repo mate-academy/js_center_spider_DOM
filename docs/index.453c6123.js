72d170b4182c2aed46837a01d9c397f44fd1ed74
Mykola Averchenko <kolya.averchenko@gmail.com>
2026-09-12 21:28:33 +0300

    add solution

---
+++ docs/index.453c6123.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".spider"),e=t.offsetWidth,c=t.offsetHeight,o=t.offsetParent;t.style.left="".concat((o.clientWidth-e)/2,"px"),t.style.top="".concat((o.clientHeight-c)/2,"px");
+//# sourceMappingURL=index.453c6123.js.map
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
 // write code here
+const spider = document.querySelector('.spider');
+
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
+const wall = spider.offsetParent;
+
+spider.style.left = `${(wall.clientWidth - spiderWidth) / 2}px`;
+spider.style.top = `${(wall.clientHeight - spiderHeight) / 2}px`;
